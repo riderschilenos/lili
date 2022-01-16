94e36aeccff3e900803f1b6096909a7ee7c706b0
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-01-16 13:03:14 -0300

    remove routes

--- routes/filmmaker.php
+++ routes/filmmaker.php
@@ -9,9 +9,9 @@ use App\Http\Livewire\Filmmaker\SeriesVideos;
 
 Route::resource('serie', SerieController::class)->middleware('can:Actualizar series')->names('series');
 
-Route::get('serie/{serie}/videos',App\Http\Livewire\Filmmaker\SeriesVideos::class)->name('series.videos');
+//Route::get('serie/{serie}/videos',App\Http\Livewire\Filmmaker\SeriesVideos::class)->name('series.videos');
 
-Route::get('serie/{serie}/sponsors',App\Http\Livewire\Filmmaker\SeriesSponsors::class)->middleware('can:Actualizar series')->name('series.sponsors');
+//Route::get('serie/{serie}/sponsors',App\Http\Livewire\Filmmaker\SeriesSponsors::class)->middleware('can:Actualizar series')->name('series.sponsors');
 
 Route::post('serie/{serie}/status',[SerieController::class, 'status'])->name('series.status');
 

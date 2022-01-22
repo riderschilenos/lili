0e1a82da0d7f010dda7fcee5ae01978c0184a35e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-01-22 00:02:43 -0300

    route socio auth

--- routes/socio.php
+++ routes/socio.php
@@ -7,6 +7,6 @@ Route::get('socios', [HomeController::class,'index'])->name('index');
 
 Route::get('{socio}', [HomeController::class,'show'])->name('show');
 
-Route::get('socio/create', [HomeController::class,'create'])->name('create');
+Route::get('socio/create', [HomeController::class,'create'])->middleware('auth')->name('create');
 
 Route::post('socio/store', [HomeController::class,'store'])->name('store');

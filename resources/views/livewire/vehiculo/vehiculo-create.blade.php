b61b904445d19d82a5859e3bd0cb5713bbce9e57
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-01-24 14:58:27 -0300

    Cols vehiculo

--- resources/views/livewire/vehiculo/vehiculo-create.blade.php
+++ resources/views/livewire/vehiculo/vehiculo-create.blade.php
@@ -22,7 +22,7 @@
             @if(!is_null($marcas))
             
             
-            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-x-6 gap-y-8 mt-6">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-6 gap-y-8 mt-6">
 
 
                 <div class="flex items-center mt-4">

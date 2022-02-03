0c00710553b9ff70ac29f8cd4da5af1e6370f132
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-02-02 22:29:03 -0300

    welcome update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
         <h1 class="text-center text-3xl text-gray-600">Compra y Venta Rider</h1>
         <p class="text-center text-gray-500 text-sm mb-6">Bicicletas, Motos y Otros.</p>
         
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8">
 
             @foreach ($vehiculos as $vehiculo)
 

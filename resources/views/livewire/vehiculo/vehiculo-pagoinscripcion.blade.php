19331490b64798f1fa9201a74ea14d7de832a11e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-02-09 00:02:28 -0300

    inscripcion update

--- resources/views/livewire/vehiculo/vehiculo-pagoinscripcion.blade.php
+++ resources/views/livewire/vehiculo/vehiculo-pagoinscripcion.blade.php
@@ -1,4 +1,37 @@
 <div>
+    @php
+    // SDK de Mercado Pago
+    require base_path('/vendor/autoload.php');
+    // Agrega credenciales
+    MercadoPago\SDK::setAccessToken(config('services.mercadopago.token'));
+
+  
+    // Crea un objeto de preferencia
+    $preference = new MercadoPago\Preference();
+
+    // Crea un ítem en la preferencia
+    $item = new MercadoPago\Item();
+    $item->title = 'Inscripcion:';
+    $item->quantity = 1;
+    $item->unit_price = 10000;
+
+    
+
+    $preference = new MercadoPago\Preference();
+    //...
+    
+    $preference->back_urls = array(
+        "success" => "",
+        "failure" => "http://www.tu-sitio/failure",
+        "pending" => "http://www.tu-sitio/pending"
+    );
+    $preference->auto_return = "approved";
+
+    $preference->items = array($item);
+    $preference->save();
+
+    @endphp
+
     @if ($vehiculo->status==2)
         <h1 class="text-lg ml-2 font-bold text-center">La inscripción del primer vehiculo es un gentil auspicio de RIDERS CHILENOS.</h1>
 
@@ -28,13 +61,25 @@
                     
                 @else
 
-                    PAGO
+
+                        <h1 class="text-lg ml-2 font-bold">Periodo de inscripcion:</h1>
+                        <h1 class="text-lg ml-2"> 1 año</h1>
+                        
+                        
+
+           
+                        <p class="text-xl font-bold ml-auto">$10.000</p>
+                    </article>
+
+                    <div class="cho-container flex justify-center mt-2 mb-4">
+                        <!-- Esto es <a href="" class="btn btn-primary">Pagar</a> un comentario -->
+                    </div>
                     
                 @endif
                     
                     
                 
-            </article>
+            
             
             
             
@@ -63,4 +108,24 @@
         Como mantener o eliminar la siscripcion
 
     @endif
+
+    <script src="https://sdk.mercadopago.com/js/v2"></script>
+  
+        <script>
+        // Agrega credenciales de SDK
+          const mp = new MercadoPago("{{config('services.mercadopago.key')}}", {
+                locale: 'es-AR'
+          });
+        
+          // Inicializa el checkout
+          mp.checkout({
+              preference: {
+                  id: '{{ $preference->id }}'
+              },
+              render: {
+                    container: '.cho-container', // Indica el nombre de la clase donde se mostrará el botón de pago
+                    label: 'Pagar', // Cambia el texto del botón de pago (opcional)
+              }
+        });
+        </script>
 </div>

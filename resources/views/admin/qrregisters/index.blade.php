2304343767edd569290f128e269bc2e1a453f2d9
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-02-20 02:09:28 -0300

    update

--- resources/views/admin/qrregisters/index.blade.php
+++ resources/views/admin/qrregisters/index.blade.php
@@ -40,7 +40,13 @@
                                 {{$qrregister->pass}}
                             </td>
                             <td>
-                                {{'https://riderschilenos.cl/garage/'.$qrregister->slug}}
+                                 @if($qrregister->active_date==NULL)
+                                 {{'https://riderschilenos.cl/garage/'.$qrregister->slug}}
+                                @else
+                            
+                                    <a href="{{'https://riderschilenos.cl/garage/'.$qrregister->slug}}" target="_blank">{{'https://riderschilenos.cl/garage/'.$qrregister->slug}}</a>
+                                @endif
+                                
                             </td>
                             <td>
                                 {{$qrregister->value}}

3bb1efebd1c7f7509bbce552a3320df17ac6aeb5
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-02-24 01:41:13 -0300

    update

--- resources/views/livewire/admin/qrregister-proceso.blade.php
+++ resources/views/livewire/admin/qrregister-proceso.blade.php
@@ -3,27 +3,27 @@
                                         
         @case(1)
             
-            <a class="btn btn-primary btn-sm" href="">BORRADOR</a>
-            <a class="btn btn-primary btn-sm" href="" wire:click="procesomas({{$qrregister}})">+</a>
+            <a class="btn btn-light btn-sm" href="">BORRADOR</a>
+            <a class="btn btn-secondary btn-sm" href="" wire:click="procesomas({{$qrregister}})">+</a>
             @break
         @case(2)
-            <a class="btn btn-primary btn-sm" href="" wire:click="procesomenos({{$qrregister}})">-</a>
-            <a class="btn btn-primary btn-sm" href="">DISEÑADO</a>
-            <a class="btn btn-primary btn-sm" href="" wire:click="procesomas({{$qrregister}})">+</a>
+            <a class="btn btn-secondary btn-sm" href="" wire:click="procesomenos({{$qrregister}})">-</a>
+            <a class="btn btn-warning btn-sm" href="">DISEÑADO</a>
+            <a class="btn btn-secondary btn-sm" href="" wire:click="procesomas({{$qrregister}})">+</a>
             @break
         @case(3)
-            <a class="btn btn-primary btn-sm" href="" wire:click="procesomenos({{$qrregister}})">-</a>
+            <a class="btn btn-secondary btn-sm" href="" wire:click="procesomenos({{$qrregister}})">-</a>
             <a class="btn btn-primary btn-sm" href="">IMPRESO</a>
-            <a class="btn btn-primary btn-sm" href="" wire:click="procesomas({{$qrregister}})">+</a>
+            <a class="btn btn-secondary btn-sm" href="" wire:click="procesomas({{$qrregister}})">+</a>
             @break
         @case(4)
-        <a class="btn btn-primary btn-sm" href="" wire:click="procesomenos({{$qrregister}})">-</a>
-        <a class="btn btn-primary btn-sm" href="">CONSIGNACION</a>
-        <a class="btn btn-primary btn-sm" href="" wire:click="procesomas({{$qrregister}})">+</a>
+        <a class="btn btn-secondary btn-sm" href="" wire:click="procesomenos({{$qrregister}})">-</a>
+        <a class="btn btn-danger btn-sm" href="">CONSIGNACION</a>
+        <a class="btn btn-secondary btn-sm" href="" wire:click="procesomas({{$qrregister}})">+</a>
           @break
         @case(5)
-        <a class="btn btn-primary btn-sm" href="" wire:click="procesomenos({{$qrregister}})">-</a>
-        <a class="btn btn-primary btn-sm" href="">VENDIDO</a>
+        <a class="btn btn-secondary btn-sm" href="" wire:click="procesomenos({{$qrregister}})">-</a>
+        <a class="btn btn-success btn-sm" href="">VENDIDO</a>
         
           @break
                                           

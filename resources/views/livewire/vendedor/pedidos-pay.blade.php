d472ac2ed0cd4b7253f9ac952c3a39b1160b94c3
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-03-05 23:07:50 -0300

    update

--- resources/views/livewire/vendedor/pedidos-pay.blade.php
+++ resources/views/livewire/vendedor/pedidos-pay.blade.php
@@ -642,7 +642,7 @@
                                                     @switch($pago->estado)
                                                         @case(1)
                                                             <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
-                                                                Pendiente
+                                                                Pendiente de Aprobación
                                                             </span>
                                                             @break
                                                         @case(2)

94153728da266b2267af7b4c5aeb92c714819f03
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-03-07 01:37:16 -0300

    UPDATE

--- resources/views/admin/disenos/index.blade.php
+++ resources/views/admin/disenos/index.blade.php
@@ -17,16 +17,17 @@
                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Cliente
                     </th>
-                    <th>
-
-                    </th>
                     <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Fono
+                        Fono
+                    </th>
+                    <th class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider" >
+                        Vendedor
                     </th>
                     
+                    
                   
-                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      
+                    <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Fono
                         
                     </th>
                     
@@ -124,9 +125,7 @@
                                     </div>
                               </td>
   
-                              <td>
-                                  
-                            </td>
+                              
 
                               <td class="text-center">
                                 <div class="text-sm font-medium text-gray-900">
@@ -136,7 +135,7 @@
                                                 
                                                 @if($socio->id == $pedido->pedidoable_id)
                                                 <a href="https://api.whatsapp.com/send?phone=569{{substr($socio->fono, -8)}}&text=Hola%20que%20tal" target="_blank">
-                                                    {{$socio->fono}}
+                                                    {{$socio->fono}}<br>Cliente
                                                 </a>
                                                 @endif
                                         @endforeach
@@ -146,7 +145,7 @@
                                                 
                                                 @if($invitado->id == $pedido->pedidoable_id)
                                                 <a href="https://api.whatsapp.com/send?phone=569{{substr($invitado->fono, -8)}}&text=Hola%20que%20tal" target="_blank">
-                                                    {{$invitado->fono}} 
+                                                    {{$invitado->fono}} <br>Cliente
                                                 </a> 
                                                 @endif
                                         @endforeach
@@ -155,11 +154,12 @@
 
                                   </div>
                               </td>
-                             
-                              <td>
-
-                              </td>
-                              <td>
+                              <td class="text-center text-sm">{{$pedido->vendedor->name}}</td>
+                                
+                                <td class="text-center text-sm">+56966996699 <br>Vendedor</td>
+                                
+                            
+                              <td class="text-center">
                                 <div class="text-sm font-medium text-gray-900">
                                           
                                     @if($pedido->pedidoable_type=='App\Models\Socio')
@@ -187,6 +187,8 @@
                                   </div>
 
                               </td>
+                             
+
                               <td class="px-6 py-4 whitespace-nowrap">
                                   
                               </td>
@@ -247,8 +249,10 @@
                                 @endphp
 
                                     <tr>
-                                            <td>
-
+                                            <td class="text-center">
+                                                <label>
+                                                    <input type="checkbox" wire:model="selected" value="{{$pedido->id}}" class="mr-4 mt-2">
+                                                </label>
                                             </td>
                                             <td class="px-6 py-4 whitespace-nowrap text-right bg-yellow-200">
                                                 <label class="mx-4">{{$counter+1}}</label>
@@ -334,6 +338,97 @@
         @endif 
         
   </x-table-responsive>
+  <div class="justify-between mt-4 grid grid-cols-2 lg:grid-cols-2 gap-4">
+
+    
+                
+                <div class="bg-white h-54 w-full rounded-xl p-6 shadow-lg flex items-center justify-around col-span-2">
+                   
+                    <div class="text-center">
+                    
+                   
+                          
+                         
+
+                                    
+                                
+                                        <div class="form-group">
+                                    
+                                                <p class="px-12 pb-4">Selecciona el método de pago:</p>
+                                                <div class="form-group flex justify-center">
+                                                    <div class="flex form-check">
+                                                    <input type="radio" name="type" id="propio" value="" class="mr-2 mt-4" checked >
+                                                    <label for="propio" class="text-xl md:text-3xl font-bold text-gray-800">
+                                                        <img class="h-14 w-38 object-contain" src="{{asset('img/home/transferencia.png')}}" alt="">
+                                                    </label>
+                                                    </div>
+                                                    <div class="flex ml-4 form-check">
+                                                    <input type="radio" name="type" id="propio" value="" class="mr-2 mt-4" >
+                                                    <label for="propio" class="text-xl md:text-3xl font-bold text-gray-800" >
+                                                            <img class="h-14 w-38 object-contain" src="{{asset('img/home/mercadopago.png')}}" alt="">
+                                                    </label>
+                                                    </div>
+                                                    
+                        
+                        
+                                                </div>
+                                        
+                        
+                                        </div>
+                                        {!! Form::open(['route'=>'vendedor.pagos.store','files'=>true , 'autocomplete'=>'off', 'method'=> 'POST' ]) !!}
+                                        @csrf
+
+                                            <div class="h-32">
+                                                <h1 class="text-xl font-bold text-center py-2 mt-4">Adjunte los archivos diseñados</h1>
+                                                <hr class="w-full">
+                                                {!! Form::file('comprobante', ['class'=>'form-input w-full mt-6'.($errors->has('comprobante')?' border-red-600':''), 'id'=>'comprobante','accept'=>'image/*']) !!}
+                                                @error('foto')
+                                                    <strong class="text-xs text-red-600">{{$message}}</strong>
+                                                @enderror
+
+                                                
+                                            </div>
+
+                                            {!! Form::hidden('user_id', Auth()->user()->id ) !!}
+                
+                                            {!! Form::hidden('metodo', 'TRANSFERENCIA' ) !!}
+
+                                    
+
+                                           
+                                            
+
+                                            {!! Form::hidden('estado', '1' ) !!}
+
+                                            
+
+                                            <div class="flex justify-center">
+                                                {!! Form::submit('Enviar', ['class'=>'btn btn-primary cursor-pointer mt-4']) !!}
+                                            </div>
+                                        
+                                        {!! Form::close() !!}
+                            
+                               
+                            
+                               
+
+                                
+                                
+                                    
+                                    
+                                       
+
+
+                                   
+                                
+                    
+                    </div>
+                </div>
+    
+         
+            </div>
+
+
   
 </div>
  

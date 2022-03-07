0c5e24455c563a8fab21c19a850eaf87d58cde13
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-03-06 22:17:59 -0300

    update

--- resources/views/serie/index.blade.php
+++ resources/views/serie/index.blade.php
@@ -4,7 +4,7 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-8 lg:px-8 py-16">
             <div class="w-full md:w-3/4 lg:w-1/2">
             <h1 class="text-white font-fold text-4xl">RidesChilenos</h1>
-            <p class="text-white text-lg mt-2 mb-4">Bienvenido al portar rider mas grande del país</p>
+            <p class="text-white text-lg mt-2 mb-4">Busca las mejores series riders del país.</p>
                 <!-- component -->
                 <!-- This is an example component -->
                 

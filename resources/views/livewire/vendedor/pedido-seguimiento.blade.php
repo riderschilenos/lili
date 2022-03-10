6b6ffb82dbd6ccbb5dc94fec05bf16c126ef6107
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-03-10 18:05:38 -0300

    update

--- resources/views/livewire/vendedor/pedido-seguimiento.blade.php
+++ resources/views/livewire/vendedor/pedido-seguimiento.blade.php
@@ -92,8 +92,8 @@ window.load = setTimeout("document.body.removeChild(aviso)", 2000);
                 </div>
               </div>
       
-              <div class="w-10 h-10 mx-auto bg-white border-2 border-gray-200 rounded-full text-lg text-white flex items-center">
-                <span class="text-center text-gray-600 w-full">
+              <div class="w-10 h-10 mx-auto @if($pedido->status < 7 ) bg-white @else bg-green-500 @endif border-2 border-gray-200 rounded-full text-lg text-white flex items-center">
+                <span class="text-center @if($pedido->status < 7) text-gray-600 @else text-white @endif w-full">
                   <svg class="w-full fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
                     <path class="heroicon-ui" d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-2.3-8.7l1.3 1.29 3.3-3.3a1 1 0 0 1 1.4 1.42l-4 4a1 1 0 0 1-1.4 0l-2-2a1 1 0 0 1 1.4-1.42z"/>
                   </svg>

d46edbaa89bbbf5cc1fb35c94b5bae5d2c11e440
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-05-18 12:17:26 -0400

    upd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,32 +15,7 @@
 
     </section>
 
-    <section class="my-4  py-12">
-        <h1 class="text-center text-3xl text-gray-600 font-bold">Compra y Venta Rider</h1>
-        <p class="text-center text-gray-500 text-sm mb-6">Bicicletas, Motos y Otros.</p>
-        
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8">
-
-            @foreach ($vehiculos as $vehiculo)
-
-                <x-vehiculo-card :vehiculo="$vehiculo" />
-                
-            @endforeach
-
-        </div>
-
-        <div class="flex justify-center mt-4 pt-4">
-            <div class="grid grid-cols-2 gap-2">
-            <a href="{{route('garage.vehiculo.vender')}}" class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded text-center">
-                Publicar
-            </a>
-            <a href="{{route('garage.usados')}}" class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ">
-                Ver todos
-            </a>
-
-            </div>
     
-    </section>
 
     <section class="mt-16">
         
@@ -100,6 +75,33 @@
                 Ver Todos
             </a>
         </div>
+    
+    </section>
+
+    <section class="my-4  py-12">
+        <h1 class="text-center text-3xl text-gray-600 font-bold">Compra y Venta Rider</h1>
+        <p class="text-center text-gray-500 text-sm mb-6">Bicicletas, Motos y Otros.</p>
+        
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8">
+
+            @foreach ($vehiculos as $vehiculo)
+
+                <x-vehiculo-card :vehiculo="$vehiculo" />
+                
+            @endforeach
+
+        </div>
+
+        <div class="flex justify-center mt-4 pt-4">
+            <div class="grid grid-cols-2 gap-2">
+            <a href="{{route('garage.vehiculo.vender')}}" class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded text-center">
+                Publicar
+            </a>
+            <a href="{{route('garage.usados')}}" class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ">
+                Ver todos
+            </a>
+
+            </div>
     
     </section>
 

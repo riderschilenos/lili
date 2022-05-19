5c6ead47ec19fe73f55431b4ba074bb991195ef0
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-05-19 02:40:28 -0400

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,8 @@
         @stack('modals')
 
         @livewireScripts
+        <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js" data-turbolinks-eval="false" data-turbo-eval="false"></script>
+
 
         @isset($js)
 

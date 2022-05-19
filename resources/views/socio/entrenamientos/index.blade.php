bb2c25bde99f860e2db4c61dee23531c7eafa7fd
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-05-18 21:04:54 -0400

    up

--- resources/views/socio/entrenamientos/index.blade.php
+++ resources/views/socio/entrenamientos/index.blade.php
@@ -130,7 +130,7 @@
                                         d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                 </svg>
                             </span>
-                            <p class="tracking-wide">Información General 
+                            <p class="tracking-wide">Ficha Deportiva
 
                                 @can('perfil_propio', $socio)
 

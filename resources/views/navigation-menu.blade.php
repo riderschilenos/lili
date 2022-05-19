1771e49f903dddcbfd40638c1355ac5545637d12
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-05-19 00:55:09 -0400

    up

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -301,6 +301,16 @@
                         
                     </button>
                 </div>
+                <!-- Portal Vendedores -->
+                <div class="-mr-2 flex items-center sm:hidden">
+                    <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition">
+                        
+                            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                <a href="{{ route('vendedores.index') }}"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></a>
+                            </svg>
+                        
+                    </button>
+                </div>
                 <!-- Perfil -->
                 
                         <div class="-mr-2 flex items-center sm:hidden">

4000fe0ba60eeff98dd68d4f91ffad14191696bb
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-05-20 02:22:56 -0400

    u

--- resources/views/socio/show.blade.php
+++ resources/views/socio/show.blade.php
@@ -191,7 +191,7 @@
                                     <span class="text-red-500">
                                         <i class="fas fa-car text-white-800"></i>
                                     </span>
-                                    <span>Mi Garage</span>
+                                    <span>Garage</span>
                                        
                                                     @can('perfil_propio', $socio)
                                                     <a href="{{route('garage.vehiculo.create')}}"><span class="text-blue-600 font-bold text-sm ml-12 align-middle"> (Inscribir Vehiculo)</span></a>
@@ -305,7 +305,7 @@
                                     <span class="text-red-500">
                                         <i class="fas fa-car text-white-800"></i>
                                     </span>
-                                    <span>Mis Entrenamientos</span>
+                                    <span>Entrenamientos</span>
                                        
                                                     
                                                     <a href="{{route('socio.entrenamiento',$socio)}}"><span class="text-blue-600 font-bold text-sm ml-12 align-middle"> (Ver más)</span></a>

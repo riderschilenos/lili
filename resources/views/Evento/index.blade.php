58601d4c6373aac0f428757634826d588fe2dd05
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-05-24 19:01:03 -0400

    up

--- resources/views/Evento/index.blade.php
+++ resources/views/Evento/index.blade.php
@@ -1,15 +1,14 @@
 <x-app-layout>
-    <section class="bg-cover bg-center" style="background-image: url({{asset('img/home/compress.jpg')}})">
+    <section class="bg-cover bg-center" style="background-image: url({{asset('img/home/riders.jpg')}})">
 
-        <div class="max-w-7xl mx-auto px-4 sm:px-8 lg:px-8 py-16">
+        <div class="max-w-7xl mx-auto px-4 sm:px-8 lg:px-8 py-24">
             <div class="w-full md:w-3/4 lg:w-1/2">
-            <h1 class="text-white font-fold text-4xl">Carreras, Clinicas y Entrenamientos</h1>
+            <h1 class="text-white font-bold text-4xl">Carreras, Clinicas y Entrenamientos</h1>
             <p class="text-white text-lg mt-2 mb-4">Inscribete y reserva tu cupo ahora!!</p>
                 <!-- component -->
                 <!-- This is an example component -->
                 
                 @livewire('search')
-
                 
             </div>
         </div>

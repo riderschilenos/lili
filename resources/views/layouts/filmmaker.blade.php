10d8a1f0f42c1dd7e1aa04b0c23f26d97e3e400e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-05-26 15:41:07 -0400

    update

--- resources/views/layouts/filmmaker.blade.php
+++ resources/views/layouts/filmmaker.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="facebook-domain-verification" content="et4ybouboiv1kxdkkqknc1zjtsz9qw" />
         <link rel="shortcut icon" href="{{asset('img/logo.png')}}">
         
         <title>{{ config('app.name', 'Laravel') }}</title>

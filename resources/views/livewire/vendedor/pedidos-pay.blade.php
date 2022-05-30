855613da73370af21c06218d1d2e2b2a5aa0e877
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-05-30 16:48:20 -0400

    Revert "arreglate please"
    This reverts commit edb7152217e049c22d028f0b3f760ef2b198c191.

--- resources/views/livewire/vendedor/pedidos-pay.blade.php
+++ resources/views/livewire/vendedor/pedidos-pay.blade.php
@@ -581,7 +581,7 @@
                             <table class="min-w-full divide-y divide-gray-200 mt-4">
                                 <thead class="bg-gray-50">
                                 <tr>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider text-center">
                                     Nro
                                     </th>
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">

c86d9248e221d475499ca1e78856ba9ca46a3ba8
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-05-31 17:48:38 -0400

    update

--- resources/views/vehiculo/garage/show.blade.php
+++ resources/views/vehiculo/garage/show.blade.php
@@ -87,9 +87,9 @@
                 
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8">
 
-                    @foreach ($autos as $vehiculo)
+                    @foreach ($autos as $auto)
 
-                        <x-vehiculo-card :vehiculo="$vehiculo" />
+                        <x-vehiculo-card :vehiculo="$auto" />
                         
                     @endforeach
 

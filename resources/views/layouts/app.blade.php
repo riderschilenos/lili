268ec05c884c83009a68c6b971a858850530d83a
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-05-30 22:37:05 -0400

    text spa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,13 +25,14 @@
     <body class="font-sans antialiased">
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-gray-100"  x-data="{home: true}">
             @livewire('navigation-menu')
 
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
+            
         </div>
 
         @stack('modals')

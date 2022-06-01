66143b7c3bc3916688c3c0d71f506b16bb9fdce3
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-01 01:54:42 -0400

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,8 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
         <script src='https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.js'></script>
         <link href='https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css' rel='stylesheet' />
+        
+
         @yield('css')
     </head>
     <body class="font-sans antialiased">
@@ -44,6 +46,6 @@
             {{$js}}
 
         @endisset
-        
+        <script type="text/javascript" src='mapa.js'></script>
     </body>
 </html>
--- resources/views/socio/entrenamientos/index.blade.php
+++ resources/views/socio/entrenamientos/index.blade.php
@@ -409,6 +409,20 @@
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11'
         });
+
+        
+        // Add geolocate control to the map.
+        map.addControl(
+        new mapboxgl.GeolocateControl({
+        positionOptions: {
+        enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+        })
+        );
         </script>
 
 </x-app-layout>

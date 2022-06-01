2fc88cefb6286de98b9c98e676ca37ac213236ac
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-01 16:45:54 -0400

    update series menu

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -2,15 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Disciplina;
 use App\Models\Filmmaker;
 use Illuminate\Http\Request;
 use App\Models\Serie;
+use App\Models\Socio;
+use App\Models\Vehiculo;
 use App\Models\Video;
 
 class SerieController extends Controller
 {
     public function index(){
-        return view('serie.index');
+        $autos = Vehiculo::where('status',4)
+        ->orwhere('status',5)
+        ->orwhere('status',7)
+        ->latest('id')->get()->take(3);
+
+        $series = Serie::where('status',3)->latest('id')->get()->take(8);
+
+        $riders = Socio::where('status',1)->latest('id')->get()->take(4);
+
+        if(auth()->user())
+        {
+        if(auth()->user()->socio)
+        {
+        $socio2 = Socio::where('user_id',auth()->user()->id)->first();
+        }else{
+        $socio2=null;
+        }
+
+        }
+        else{
+        $socio2=null;
+        }
+
+        $disciplinas= Disciplina::pluck('name','id');
+
+        return view('serie.index',compact('series','riders','autos','socio2','disciplinas'));
     }
 
     public function show(Serie $serie){
--- resources/views/serie/index.blade.php
+++ resources/views/serie/index.blade.php
@@ -1,22 +1,27 @@
 <x-app-layout>
-    <section class="bg-cover bg-center" style="background-image: url({{asset('img/home/video.jpg')}})">
 
-        <div class="max-w-7xl mx-auto px-4 sm:px-8 lg:px-8 py-16">
-            <div class="w-full md:w-3/4 lg:w-1/2">
-            <h1 class="text-white font-fold text-4xl">RidesChilenos</h1>
-            <p class="text-white text-lg mt-2 mb-4">Busca las mejores series riders del país.</p>
-                <!-- component -->
-                <!-- This is an example component -->
-                
-                @livewire('search')
+    <x-fast-view :riders="$riders" :autos="$autos" :series="$series" :socio2="$socio2">
+       
+    
+        <section class="bg-cover bg-center" style="background-image: url({{asset('img/home/video.jpg')}})">
 
-                
+            <div class="max-w-7xl mx-auto px-4 sm:px-8 lg:px-8 py-16">
+                <div class="w-full md:w-3/4 lg:w-1/2">
+                <h1 class="text-white font-fold text-4xl">RidesChilenos</h1>
+                <p class="text-white text-lg mt-2 mb-4">Busca las mejores series riders del país.</p>
+                    <!-- component -->
+                    <!-- This is an example component -->
+                    
+                    @livewire('search')
+
+                    
+                </div>
             </div>
-        </div>
 
-    </section>
+        </section>
 
-    @livewire('series-index')
+        @livewire('series-index')
 
+    </x-fast-view>
 
 </x-app-layout>

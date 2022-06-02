16ab82b2b9324009d8df2ab753a8502a53e4edbc
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-02 03:37:22 -0400

    update

--- resources/views/socio/show.blade.php
+++ resources/views/socio/show.blade.php
@@ -42,7 +42,7 @@
                 </div>
                 <!-- End of Navbar -->
 
-                <div class="container mx-auto my-5 p-5">
+                <div class="container mx-auto mb-5 p-5">
                     <div class="md:flex no-wrap md:-mx-2 ">
                         <!-- Left Side -->
                         <div class="w-full md:w-3/12 md:mx-2">

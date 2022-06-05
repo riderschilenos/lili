6339000963c895da5931bd419ab0b82878270efb
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-05 05:23:06 -0400

    update prepay

--- resources/views/vendedor/pedidos/prepay.blade.php
+++ resources/views/vendedor/pedidos/prepay.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
 
     <x-fast-view :riders="$riders" :autos="$autos" :series="$series" :socio2="$socio2"  :disciplinas="$disciplinas">
-        
+        <div class="my-12">
             @livewire('vendedor.pedidos-pay')
-
+        </div>
     </x-fast-view>
     
     

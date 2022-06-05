ee5f54520627337946cf42c2ff00ad08e11c09ce
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-05 14:44:34 -0400

    update

--- resources/views/vehiculo/garage/show.blade.php
+++ resources/views/vehiculo/garage/show.blade.php
@@ -19,7 +19,7 @@
                         {{-- comment <img class="h-80 w-full object-cover object-center" src="{{Storage::url($vehiculo->image->first()->url)}}" alt="">--}}
                         <ul class="slider snap-mandatory flex overflow-x-auto gap-0 snap-x before:shrink-0 before:w-[30vw] after:shrink-0 after:w-[30vw]" style='z-index: 1 ; '>
                             @foreach ($vehiculo->image as $image)  
-                            <li class="shrink-0 snap-center w-full snap-mandatory">       
+                            <li class="shrink-0 snap-center w-full h-screen snap-mandatory">       
                                 <img class="" src="{{Storage::url($image->url)}}" alt="" style="scroll-snap-align: center;">
                             </li>
                             @endforeach

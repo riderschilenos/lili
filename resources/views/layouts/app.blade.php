6b3f99a3f95d1d163c4e7da26dcc75a3c55e812f
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-05 03:13:34 -0400

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,6 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-        <link rel="stylesheet" href="{{asset('css/slider.css')}}">
         <link rel="stylesheet" href="{{asset('vendor/fontawesome-free/css/all.min.css')}}">
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,12 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     //mode: 'jit',
-    purge: [
+    content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/css/*.css'
     ],
 
     theme: {

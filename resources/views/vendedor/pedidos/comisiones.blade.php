0927d0b01995d5dc7919b9cec39eb5b7475b5c22
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-05 17:50:12 -0400

    update

--- resources/views/vendedor/pedidos/comisiones.blade.php
+++ resources/views/vendedor/pedidos/comisiones.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     
     <x-fast-view :riders="$riders" :autos="$autos" :series="$series" :socio2="$socio2"  :disciplinas="$disciplinas">
-        <div class="my-12">
+        <div class="my-12 mb-10">
             @livewire('vendedor.retiro-comisiones')
         </div>
     </x-fast-view>

b223402340016072dc80423f1ba7d33f9ed997fc
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-05 03:06:21 -0400

    update config purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     //mode: 'jit',
-    content: [
+    purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',

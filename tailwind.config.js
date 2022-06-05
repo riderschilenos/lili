8779d1e25059316a9fcc81f1f98f4c5b4b386776
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-05 02:58:21 -0400

    update tailwing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors')
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    //mode: 'jit',
+    mode: 'jit',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',

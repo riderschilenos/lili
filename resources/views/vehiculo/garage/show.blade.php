b05a7b7f5e44cb46cf6e670e3771932a94dc2dce
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-04 23:55:41 -0400

    update

--- resources/views/vehiculo/garage/show.blade.php
+++ resources/views/vehiculo/garage/show.blade.php
@@ -23,7 +23,7 @@
                             <div
                             class="fb-like"
                             data-share="true"
-                            data-width="250"
+                            data-width="280"
                             data-show-faces="true">
                             </div>
                         </div>

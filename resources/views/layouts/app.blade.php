abe903e8140ae3c0466ba11f6f6374cda1b09f69
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-06 15:16:41 -0400

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="facebook-domain-verification" content="et4ybouboiv1kxdkkqknc1zjtsz9qw" />
-        <meta name="description" content="Te invitamos a visualizar el contenido del portal rider más importante del Pais, registrate y disfruta los beneficios que tenemos para ti y la comunidad.">
+        <meta name="description" content="Te invitamos a visualizar el contenido del portal rider más importante del Pais, haz click y sorprendete con lo que tenemos para ti y la comunidad.">
         <link rel="shortcut icon" href="{{asset('img/logo.png')}}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>

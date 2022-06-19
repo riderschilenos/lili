705559a31fba179b0e277ba9e2e10d0c83e7b6e0
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-19 14:45:51 -0400

    update

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,6 +1,6 @@
 <div>
 
-            <form class="pt-2 relative text-gray-600" autocomplete="off">
+            <form class="ml-4 pt-2 relative text-gray-600" autocomplete="off">
                 <input wire:model="search" class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
                 type="search" name="search" placeholder="Buscar">
 

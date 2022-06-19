22fcd154924fbbb7e7b5d105e28e06f7c9d7958c
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-19 14:43:43 -0400

    up

--- resources/views/components/fast-view.blade.php
+++ resources/views/components/fast-view.blade.php
@@ -2,7 +2,7 @@
 <div>
     <div class="w-full text-white bg-main-color block sm:hidden">
         <div x-data="{ open: false }"
-            class="flex flex-col max-w-screen-xl pt-4 pb-6 sm:py-0 px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+            class="flex flex-col max-w-screen-xl py-4 sm:py-0 px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
             <div class="flex flex-row pl-6 items-center justify-center">
                 @livewire('search')
             </div>

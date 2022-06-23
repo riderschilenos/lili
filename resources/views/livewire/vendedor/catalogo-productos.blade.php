fe4da6e2c8122aa00dc21af088e08b5bc305e4aa
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-06-23 09:55:22 -0400

    update

--- resources/views/livewire/vendedor/catalogo-productos.blade.php
+++ resources/views/livewire/vendedor/catalogo-productos.blade.php
@@ -1,4 +1,4 @@
-<div class="mb-12">
+<div class="mb-12 hidden">
     <div class="bg-white">
         <div class="py-16 sm:py-8 lg:max-w-7xl lg:mx-auto lg:px-8">
 
@@ -10,19 +10,19 @@
                     <h1 class="text-center mb-12"> ¿Que Tipo de producto buscas?</h1>
                     <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-x-6 gap-y-8">
                       
-                        <article wire:click="category(1)">
+                        <article class="cursor-pointer" wire:click="category(1)">
                             <figure>
                                     <img class="rounded-xl h-38 mx-auto w-44 object-contain" src="{{asset('img/home/carcasas.jpg')}}" alt="">
                             </figure>
                         </article>
 
-                        <article wire:click="category(2)">
+                        <article class="cursor-pointer" wire:click="category(2)">
                             <figure>
                                     <img class="rounded-xl h-38 mx-auto w-44 object-contain" src="{{asset('img/home/accesorios.jpg')}}" alt="">
                             </figure>
                         </article>
 
-                        <article wire:click="category(3)">
+                        <article class="cursor-pointer" wire:click="category(3)">
                             <figure>
                                     <img class="rounded-xl h-38 mx-auto w-44 object-contain" src="{{asset('img/home/poleras.jpeg')}}" alt="">
                             </figure>               
@@ -37,17 +37,17 @@
                     <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-4 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-x-6 gap-y-8">
                         @foreach ($marcas as $marca)    
                                 @if ($marca->image)
-                                    <article>
-                                        <figure>
-                                                <img class="rounded-xl h-38 mx-auto w-44 object-contain" src="{{Storage::url($marca->image->url)}}" alt="">
-                                        </figure>
-                                    </article>
+                                    <div class="flex h-screen cursor-pointer">
+                                        <div class="m-auto">
+                                            <img class="rounded-xl h-38 mx-auto w-44 object-contain" src="{{Storage::url($marca->image->url)}}" alt="">
+                                        </div>
+                                    </div>
                                 @else
-                                    <article>
-                                        <figure>
-                                                <img class="rounded-xl h-38 mx-auto w-44 object-contain" src="{{asset('img/home/poleras.jpeg')}}" alt="">
-                                        </figure>
-                                    </article>
+                                <div class="flex h-screen bg-gray-800 cursor-pointer">
+                                    <div class="m-auto">
+                                      <h3 class="text-center text-white my-4">{{$marca->name}}</h3>
+                                    </div>
+                                  </div>
                                 @endif
                         @endforeach
 
@@ -57,13 +57,13 @@
                     <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-4 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-x-6 gap-y-8">
                         @foreach ($products as $product)    
                                 @if ($product->image)
-                                    <article wire:click="producto({{$product->id}})">
+                                    <article class="cursor-pointer" wire:click="producto({{$product->id}})">
                                         <figure>
                                                 <img class="rounded-xl h-38 mx-auto w-44 object-contain" src="{{Storage::url($product->image)}}" alt="">
                                         </figure>
                                     </article>
                                 @else
-                                    <article wire:click="producto({{$product->id}})">
+                                    <article class="cursor-pointer" wire:click="producto({{$product->id}})">
                                         <figure>
                                                 <img class="rounded-xl h-38 mx-auto w-44 object-contain" src="{{asset('img/home/poleras.jpeg')}}" alt="">
                                         </figure>

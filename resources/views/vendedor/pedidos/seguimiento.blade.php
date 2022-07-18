0ef34c19e9bd22d7bbc580fd1fabe7d3466708b2
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-18 15:03:06 -0400

    up

--- resources/views/vendedor/pedidos/seguimiento.blade.php
+++ resources/views/vendedor/pedidos/seguimiento.blade.php
@@ -3,6 +3,25 @@
     <x-slot name="pedido">
         {{$pedido->id}}
     </x-slot>
+    
+    <style>
+        :root {
+            --main-color: #4a76a8;
+        }
+    
+        .bg-main-color {
+            background-color: var(--main-color);
+        }
+    
+        .text-main-color {
+            color: var(--main-color);
+        }
+    
+        .border-main-color {
+            border-color: var(--main-color);
+        }
+    </style>
+
 
     @livewire('vendedor.pedido-seguimiento', ['pedido' => $pedido], key('pedido-seguimiento.'.$pedido->id))
 

13f1976d39410a68feadce8b5f21628d01bf64f7
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-18 14:34:38 -0400

    up

--- resources/views/livewire/vendedor/pedido-seguimiento.blade.php
+++ resources/views/livewire/vendedor/pedido-seguimiento.blade.php
@@ -1,22 +1,5 @@
 <div>
-  
-  <style>
-    :root {
-        --main-color: #4a76a8;
-    }
-
-    .bg-main-color {
-        background-color: var(--main-color);
-    }
-
-    .text-main-color {
-        color: var(--main-color);
-    }
-
-    .border-main-color {
-        border-color: var(--main-color);
-    }
-</style>
+    
 
 <script>//<![CDATA[
 function getlink() {
--- resources/views/vendedor/pedidos/seguimientopublic.blade.php
+++ resources/views/vendedor/pedidos/seguimientopublic.blade.php
@@ -4,10 +4,28 @@
         {{$pedido->id}}
     </x-slot>
 
-    <div class="container pt-8" >
+    <style>
+        :root {
+            --main-color: #4a76a8;
+        }
+    
+        .bg-main-color {
+            background-color: var(--main-color);
+        }
+    
+        .text-main-color {
+            color: var(--main-color);
+        }
+    
+        .border-main-color {
+            border-color: var(--main-color);
+        }
+    </style>
+
+
 
         @livewire('vendedor.pedido-seguimiento', ['pedido' => $pedido], key('pedido-seguimiento.'.$pedido->id))
 
-    </div>
+
 
 </x-app-layout> 

6295fa5b891319e646c84169ce7c4c1f2aaca236
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-22 13:32:00 -0400

    up

--- app/Http/Controllers/Admin/SerieController.php
+++ app/Http/Controllers/Admin/SerieController.php
@@ -65,7 +65,7 @@ class SerieController extends Controller
         $serie->save();
 
         $mail = new RejectSerie($serie); 
-        Mail::to($serie->productor->first()->email)->queue($mail);
+        Mail::to($serie->user->email)->send($mail);
 
         return redirect()->route('admin.series.index')->with('info','La serie se ha rechazado con éxito');
 

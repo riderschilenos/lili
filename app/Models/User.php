d6febdbf17c09da85d3626e39cb3c78b9232fc76
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-07-22 15:10:07 -0400

    repair

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,10 +21,6 @@ class User extends Authenticatable
     use TwoFactorAuthenticatable;
     use HasRoles;
     
-    public function sendPasswordResetNotification($token)
-    {
-        $this->notify(new ResetPasswordNotification($token));
-    }
     /**
      * The attributes that are mass assignable.
      *

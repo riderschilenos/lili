c94da74565fce263c75d40d223351d44bd95407d
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-08-15 01:02:50 -0400

    u

--- resources/views/livewire/eventos-index.blade.php
+++ resources/views/livewire/eventos-index.blade.php
@@ -80,7 +80,7 @@
     </div>  
     
   
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
 
         @foreach ($eventos as $evento)
 

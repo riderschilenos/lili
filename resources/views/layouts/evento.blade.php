d33e92ccf6cf8c04f7f973dd22b097b13e19a1d1
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-08-16 00:57:13 -0400

    update

--- resources/views/layouts/evento.blade.php
+++ resources/views/layouts/evento.blade.php
@@ -12,7 +12,8 @@
         <meta property="og:image" content="{{Storage::url($evento->image->url)}}">
         <meta property="og:title" content="{{$evento->titulo}}"/>  
         <meta property="og:description" content="Te invitamos a visualizar el contenido del portal rider más importante del Pais, haz click y revisa lo que hay detras de este link."/>  
-        <link rel="shortcut icon" href="{{Storage::url($evento->image->url)}}">
+
+        <link rel="shortcut icon" href="{{asset('img/logo.png')}}">
 
         <meta name="description" content="Te invitamos a visualizar el contenido del portal rider más importante del Pais, haz click y revisa lo que hay detras de este link.">
        

8e06eac9a386363f158692b8abf247bae088dfed
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-09-11 17:34:28 -0300

    u

--- resources/views/vendedor/catalogo/carcasas.blade.php
+++ resources/views/vendedor/catalogo/carcasas.blade.php
@@ -9,20 +9,20 @@
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-x-6 gap-y-8">
                 <article>
                     <figure>
-                        <a href="catalogos/poleronesmx.pdf"><img class="rounded-xl h-35 w-55 object-cover" src="{{asset('catalogos/mx.jpg')}}" alt=""></a>
+                        <a href="catalogos/poleronesmx.pdf"><img class="rounded-xl h-35 w-55 object-cover" src="{{asset('catalogos/mx-min.jpg')}}" alt=""></a>
                     </figure>
         
                 
                 </article>
                 <article>
                     <figure>
-                        <a href="{{route('catalogo.carcasas')}}"><img class="rounded-xl h-35 w-55 object-cover" src="{{asset('catalogos/mtb.jpg')}}" alt=""></a>
+                        <a href="{{route('catalogo.carcasas')}}"><img class="rounded-xl h-35 w-55 object-cover" src="{{asset('catalogos/mtb-min.jpg')}}" alt=""></a>
                     </figure>
                 
                 </article>
                 <article>
                     <figure>
-                        <a href=""><img class="rounded-xl h-35 w-55 object-cover" src="{{asset('catalogos/multimarca.jpg')}}" alt=""></a>
+                        <a href=""><img class="rounded-xl h-35 w-55 object-cover" src="{{asset('catalogos/multimarca-min.jpg')}}" alt=""></a>
                     </figure>
                 
                 </article>

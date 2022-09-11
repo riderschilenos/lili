375d63a3df022b9176ac383ef9a37837fbabd0f8
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-09-11 18:24:23 -0300

    up

--- resources/views/socio/create.blade.php
+++ resources/views/socio/create.blade.php
@@ -1,6 +1,27 @@
 <x-app-layout>
    
-
+    <style>
+        :root {
+            --main-color: #4a76a8;
+            --rider-color: #314780;
+        }
+    
+        .bg-main-color {
+            background-color: var(--main-color);
+        }
+    
+        .bg-rider-color {
+            background-color: var(--rider-color);
+        }
+    
+        .text-main-color {
+            color: var(--main-color);
+        }
+    
+        .border-main-color {
+            border-color: var(--main-color);
+        }
+    </style>
     <div class="max-w-7xl mx-auto px-4 py-8">
 
         <div class="card pb-8">

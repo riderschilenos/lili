8c76ff542893400882d0da8da9ada08c7d5f03ad
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-09-11 14:29:38 -0300

    update

--- resources/views/vendedor/catalogo/carcasas.blade.php
+++ resources/views/vendedor/catalogo/carcasas.blade.php
@@ -1,4 +1,25 @@
 <x-app-layout>
-
+    <style>
+        :root {
+            --main-color: #4a76a8;
+            --rider-color: #314780;
+        }
+    
+        .bg-main-color {
+            background-color: var(--main-color);
+        }
+    
+        .bg-rider-color {
+            background-color: var(--rider-color);
+        }
+    
+        .text-main-color {
+            color: var(--main-color);
+        }
+    
+        .border-main-color {
+            border-color: var(--main-color);
+        }
+    </style>
 
 </x-app-layout>

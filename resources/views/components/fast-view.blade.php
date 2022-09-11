8469cc185e29b634de32ab2ab1147a6be9010b26
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-09-11 14:18:07 -0300

    up

--- resources/views/components/fast-view.blade.php
+++ resources/views/components/fast-view.blade.php
@@ -487,7 +487,7 @@
             </button>
         </div>
 comment --}}
-<section class="mt-12 sm:mt-16">
+<section class="hidden md:block mt-12 sm:mt-16">
             
 
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-x-6 gap-y-8">

96e2d747e8dfe456ea63af850c4f08f89f58365f
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2022-09-29 18:48:22 -0300

    update

---
+++ app/Http/Livewire/Ticket/EventoShow.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Livewire\Ticket;
+
+use App\Models\Evento;
+use App\Models\Fecha;
+use App\Models\Ticket;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class EventoShow extends Component
+{   
+    use WithPagination;
+
+    use AuthorizesRequests;
+
+    public $evento;
+
+    public function mount(Evento $evento){
+        
+        $this->evento=$evento;
+
+    }
+
+    public function render()
+    {    $fechas= Fecha::where('evento_id',$this->evento->id)->paginate();
+        
+        $similares = Evento::where('disciplina_id',$this->evento->disciplina_id)
+                            ->where('id','!=',$this->evento->id)
+                            ->where('status',1)
+                            ->latest('id')
+                            ->take(5)
+                            ->get();
+          
+        if(auth()->user())
+        {   
+                if(Ticket::where('evento_id',$this->evento->id)->where('user_id',auth()->user()->id)){    
+                    $ticket = Ticket::where('evento_id',$this->evento->id)->where('user_id',auth()->user()->id)->first();
+                }else{
+                    $ticket =null;
+                }    
+                
+        }
+        else{
+            $ticket =null;
+        }                   
+
+        return view('livewire.ticket.evento-show',compact('fechas','similares','ticket'));
+    }
+}

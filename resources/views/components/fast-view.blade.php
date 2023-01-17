4f40e1aa996035958635174c05697524938e197d
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-01-17 02:54:25 -0300

    update

--- resources/views/components/fast-view.blade.php
+++ resources/views/components/fast-view.blade.php
@@ -680,10 +680,7 @@
                         <div>
     
                         </div>
-                        <div>
-                            <h1 class="text-2xl font-bold pb-4 text-center">CLUB RIDERS CHILENOS</h1>
-                            
-                        </div>
+                       
                         <div class="hidden sm:block">
                             <div class="flex justify-end mr-4 ">
     
@@ -738,6 +735,42 @@
     
                             </div>
                         </div>
+                    </div>
+
+                    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-x-6 gap-y-8 mb-6 mt-2">
+                        <article>
+                            <figure>
+                                <a href="{{route('socio.index')}}"><img class="rounded-xl h-35 w-55 object-cover" src="{{asset('img/home/registroriders.png')}}" alt=""></a>
+                            </figure>
+        
+                        
+                        </article>
+
+                    
+                    </div>
+
+                    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-x-6 gap-y-8">
+                        <article>
+                            <figure>
+                                <a href="{{route('socio.index')}}"><img class="rounded-xl h-35 w-55 object-cover" src="{{asset('img/home/RIDERS-min.png')}}" alt=""></a>
+                            </figure>
+        
+                        
+                        </article>
+                        <article>
+                            <figure>
+                                <a href="{{route('garage.vehiculos.registerindex')}}"><img class="rounded-xl h-35 w-55 object-cover" src="{{asset('img/home/REGISTRO2-min.png')}}" alt=""></a>
+                            </figure>
+                        
+                        </article>
+                        <article>
+                            <figure>
+                                <a href="{{route('garage.usados')}}"><img class="rounded-xl h-35 w-55 object-cover" src="{{asset('img/home/USADOS2-min.png')}}" alt=""></a>
+                            </figure>
+                        
+                        </article>
+                        
+                    
                     </div>
     
             

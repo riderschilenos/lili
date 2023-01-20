06daa237a0c5cb221ce603cda922aea5b02e9447
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-01-20 15:10:50 -0300

    u

--- resources/views/livewire/vendedor/catalogo-productos.blade.php
+++ resources/views/livewire/vendedor/catalogo-productos.blade.php
@@ -1,6 +1,6 @@
 <div class="mb-12">
     <div class="bg-white">
-        <div class="py-10 sm:py-4 lg:max-w-7xl lg:mx-auto lg:px-8">
+        <div class="pt-4 pb-6 sm:py-4 lg:max-w-7xl lg:mx-auto lg:px-8">
 
            
 
@@ -107,7 +107,7 @@
             
 
                 @if(is_null($selectedcategory))
-                    <h1 class="text-center mb-12"> ¿Qué Tipo de producto buscas?</h1>
+                    <h1 class="text-center mb-4"> ¿Qué Tipo de producto buscas?</h1>
                     <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-x-6 gap-y-8">
                       
                         <article class="cursor-pointer" wire:click="category(1)">

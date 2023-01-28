5f3f3a1b476d6662d1bc7330e1b1adf13f1153c0
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-01-28 00:28:10 -0300

    u

--- resources/views/components/fast-view.blade.php
+++ resources/views/components/fast-view.blade.php
@@ -615,7 +615,7 @@
         <section class="mt-6 sm:mt-16">
             
 
-            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-x-6 gap-y-8">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-x-2 gap-y-2">
                 <article>
                     <figure>
                         <a href="{{route('socio.index')}}"><img class="rounded-xl h-35 w-55 object-cover" src="{{asset('img/home/RIDERS-min.png')}}" alt=""></a>

f56f4349127f01c98875609bde96c41a35007fb8
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-01-28 00:08:20 -0300

    update

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,4 +1,26 @@
 <x-app-layout>
+    <style>
+        :root {
+            --main-color: #4a76a8;
+            --rider-color: #314780;
+        }
+    
+        .bg-main-color {
+            background-color: var(--main-color);
+        }
+    
+        .bg-rider-color {
+            background-color: var(--rider-color);
+        }
+    
+        .text-main-color {
+            color: var(--main-color);
+        }
+    
+        .border-main-color {
+            border-color: var(--main-color);
+        }
+    </style>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Profile') }}

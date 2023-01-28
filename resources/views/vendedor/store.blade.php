875f68b592765429b495768fd2067c944f165d81
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-01-28 15:58:59 -0300

    up

--- resources/views/vendedor/store.blade.php
+++ resources/views/vendedor/store.blade.php
@@ -70,15 +70,17 @@
                                 </div>
                                 <div class="col-spam-3 px-4 w-full">
                                     <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8 justify-between">
-                                        <span clas="text-green-500">
-                                            <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                                            </svg>
-                                        </span>
-                                        <p class="tracking-wide">{{ $socio->name." ".$socio->second_name }} {{ $socio->last_name }}
-    
+                                        <div class="flex">
+                                            <span clas="text-green-500">
+                                                <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                    stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                                </svg>
+                                            </span>
+                                            <p class="tracking-wide">{{ $socio->name." ".$socio->second_name }} {{ $socio->last_name }}
+                                            
+                                        </div>
                                             @can('perfil_propio', $socio)
     
                                             

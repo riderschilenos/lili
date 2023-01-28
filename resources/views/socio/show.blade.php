a0864d305b58eea4a7064795558141cf3e3e4432
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-01-28 02:13:41 -0300

    u

--- resources/views/socio/show.blade.php
+++ resources/views/socio/show.blade.php
@@ -175,7 +175,7 @@
                                         </div>
                                     </div>
                                 
-                                    <button x-on:click="open=false" x-show="open" class="block w-full text-blue-800 text-sm font-semibold rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4">Contácto</button>
+                                    <button x-on:click="open=false" x-show="open" class="block w-full text-blue-800 text-sm font-semibold rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4">Info de Contácto</button>
                                     <button x-on:click="open=true" x-show="!open" class="block w-full text-blue-800 text-sm font-semibold rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4">Resume</button>
                                 </div>
                             </div>

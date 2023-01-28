40259868fc17a0606e0a18739e90075f87894865
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-01-28 00:05:22 -0300

    up

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,6 +18,28 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body>
+        <style>
+            :root {
+                --main-color: #4a76a8;
+                --rider-color: #314780;
+            }
+        
+            .bg-main-color {
+                background-color: var(--main-color);
+            }
+        
+            .bg-rider-color {
+                background-color: var(--rider-color);
+            }
+        
+            .text-main-color {
+                color: var(--main-color);
+            }
+        
+            .border-main-color {
+                border-color: var(--main-color);
+            }
+        </style>
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>

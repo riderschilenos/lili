fac71768dd7f4a5822c8d7e340c9a9f0eff27c2a
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-02-20 09:46:43 -0300

    u

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,29 @@
         @yield('css')
     </head>
     <body class="font-sans antialiased">
+        <style>
+            :root {
+                --main-color: #4a76a8;
+                --rider-color: #314780;
+            }
+        
+            .bg-main-color {
+                background-color: var(--main-color);
+            }
+        
+            .bg-rider-color {
+                background-color: var(--rider-color);
+            }
+        
+            .text-main-color {
+                color: var(--main-color);
+            }
+        
+            .border-main-color {
+                border-color: var(--main-color);
+            }
+        </style>
+        
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100"  x-data="{@routeIs('home') home: true @else home: false @endif, base: true, socio: false, registro: false, user: false, vendedor: false}">

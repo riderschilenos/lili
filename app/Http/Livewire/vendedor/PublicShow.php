40931009a792f1c28cd4eeff090e0c19eb9ecd15
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-03-01 22:49:51 -0300

    u

--- app/Http/Livewire/vendedor/PublicShow.php
+++ app/Http/Livewire/vendedor/PublicShow.php
@@ -4,11 +4,23 @@ namespace App\Http\Livewire\Vendedor;
 
 use App\Models\Producto;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class PublicShow extends Component
-{
+{   use WithPagination;
+
+    public $search;
+
     public function render()
-    {   $productos=Producto::all();
+    {   $productos=Producto::where('name','LIKE','%'. $this->search .'%')
+                ->latest('id')
+                ->paginate(16);
+       
         return view('livewire.vendedor.public-show',compact('productos'));
+    }
+
+    
+    public function limpiar_page(){
+        $this->resetPage();
     }
 }

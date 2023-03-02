ae48be84933f8b83a723ba06e072afd04c2bc7cb
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-03-01 23:21:30 -0300

    u

--- resources/views/livewire/vendedor/public-show.blade.php
+++ resources/views/livewire/vendedor/public-show.blade.php
@@ -7,7 +7,7 @@
   
   
   <!-- Title -->
-  <div class="flex flex-wrap items-center  overflow-x-auto overflow-y-hidden py-10 justify-center   bg-white text-gray-800">
+  <div class="hidden flex flex-wrap items-center  overflow-x-auto overflow-y-hidden py-10 justify-center   bg-white text-gray-800">
     <a rel="noopener noreferrer" href="#" class="flex items-center flex-shrink-0 px-5 py-3 space-x-2text-gray-600">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">
             <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>

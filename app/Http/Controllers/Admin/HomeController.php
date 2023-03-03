8dc1fec924d35544c25696069d6a6c4fe788d682
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-03-03 09:10:23 -0300

    u

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Gasto;
+use App\Models\Pago;
 use App\Models\Retiro;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -16,6 +17,8 @@ class HomeController extends Controller
 
       $retiros = Retiro::where('estado',1)->where('created_at', '<=', Carbon::now()."+ 1 month")->get();
 
-      return view('admin.index',compact('gastos','retiros'));
+      $pagos = Pago::where('estado',1)->paginate(80);
+
+      return view('admin.index',compact('gastos','retiros','pagos'));
    }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,6 +8,108 @@
 
 @section('content')
 
+@if (session('info'))
+        <div class="alert alert-success">
+            {{session('info')}}
+        </div>
+    @endif
+
+    @if ($pagos)
+        <div class="card">
+            <div class="card-body">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Vendedor</th>
+                            <th>Pedidos</th>
+                            <th>Metodo</th>
+                            <th>Cantidad</th>
+                            <th>Comprobante</th>
+                            <th></th>
+                            <th></th>
+
+
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach ($pagos as $pago)
+                            <tr>
+                                <td>{{$pago->id}}</td>
+                                <td> 
+                                    @foreach ($pago->pedidos as $pedido)
+                                        {{$pedido->vendedor->name}} <br>
+                                    @endforeach
+                                    
+                                </td>
+                                <td> 
+                                    @foreach ($pago->pedidos as $pedido)
+                                        @php
+                                        $subtotal=0;
+                                        @endphp
+            
+                                        @if($pedido->pedidoable_type=="App\Models\Socio")
+                                        @foreach ($pedido->ordens as $orden)
+                                        @php
+                                            
+                                            $subtotal+=$orden->producto->precio-$orden->producto->descuento_socio;
+            
+                                        @endphp    
+                                        @endforeach
+            
+                                        @endif
+                                        @if($pedido->pedidoable_type=="App\Models\Invitado")
+                                        @foreach ($pedido->ordens as $orden)
+                                        @php
+                                            
+                                            $subtotal+=$orden->producto->precio;
+            
+                                        @endphp    
+                                        @endforeach
+            
+                                        @endif
+
+                                        Pedido {{$pedido->id}} - ${{$subtotal}} <br>
+                                    @endforeach
+                                    
+                                </td>
+                                <td>{{$pago->metodo}}</td>
+                                <td>{{$pago->cantidad}}</td>
+                                <td>
+                                    <img class="object-cover object-center" width="60px" src="{{Storage::url($pago->comprobante)}}" alt="">
+                                
+                                </td>
+                                <td>{{$pago->created_at->format('d-m-Y H:i:s')}}</td>
+                                <td>
+                                    <form action="{{route('admin.pagos.destroy',$pago)}}" method="POST">
+                                        @csrf
+                                        @method('delete')
+
+                                        <button class="btn btn-danger btn-sm" type="submit"> RECHAZAR </button>
+                                    </form>
+                                    <form action="{{route('admin.pago.approved',$pago)}}" method="POST">
+                                        @csrf
+                
+                                        <button class="btn btn-primary" type="submit">Aprobar</button>
+                                    </form>   
+                                </td>
+                                
+                            </tr>
+                            
+                        @endforeach
+
+                    </tbody>
+                </table>
+                
+            </div>
+
+            <div class="card-footer">
+                {{$pagos->links('vendor.pagination.bootstrap-4')}}
+            </div>
+        </div>
+    @endif
+
     @if ($gastos->count())
         @livewire('admin.comisiones-pay')
     @endif

7b51e589f6eb100295eecff1f940f2391f4b07cf
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-04-01 23:27:38 -0400

    u

--- resources/views/payment/pagocliente.blade.php
+++ resources/views/payment/pagocliente.blade.php
@@ -5,15 +5,28 @@
         </div>
     </div>
     <script>
+        // This method will trigger user permissions
+        Html5Qrcode.getCameras().then(devices => {
+        /**
+         * devices would be an array of objects of type:
+         * { id: "id", label: "label" }
+         */
+        if (devices && devices.length) {
+            var cameraId = devices[0].id;
+            // .. use this to start scanning.
+        }
+        }).catch(err => {
+        // handle err
+        });
         function onScanSuccess(decodedText, decodedResult) {
         console.log(`Code scanned = ${decodedText}`, decodedResult);
         }
 
-
-       
         var html5QrcodeScanner = new Html5QrcodeScanner(
             "qr-reader", { fps: 10, qrbox: 250 });
         html5QrcodeScanner.render(onScanSuccess);
+        // If you want to prefer back camera
+        html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
     </script>
 
 </x-app-layout>

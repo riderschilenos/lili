c417bdd0ee6c4897ef74cbb34d67aa70934d951f
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-04-01 23:08:51 -0400

    u

--- resources/views/payment/pagocliente.blade.php
+++ resources/views/payment/pagocliente.blade.php
@@ -8,6 +8,18 @@
         function onScanSuccess(decodedText, decodedResult) {
         console.log(`Code scanned = ${decodedText}`, decodedResult);
         }
+        Html5Qrcode.getCameras().then(devices => {
+        /**
+         * devices would be an array of objects of type:
+         * { id: "id", label: "label" }
+         */
+        if (devices && devices.length) {
+            var cameraId = devices[0].id;
+            // .. use this to start scanning.
+        }
+        }).catch(err => {
+        // handle err
+        });
         var html5QrcodeScanner = new Html5QrcodeScanner(
             "qr-reader", { fps: 10, qrbox: 250 });
         html5QrcodeScanner.render(onScanSuccess);

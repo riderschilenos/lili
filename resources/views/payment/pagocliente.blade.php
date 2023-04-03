110d0e084a141767ee572fd9e268a7b830ec6ae7
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-04-03 13:31:36 -0400

    u

--- resources/views/payment/pagocliente.blade.php
+++ resources/views/payment/pagocliente.blade.php
@@ -27,6 +27,8 @@
           function onScanSuccess(decodedText, decodedResult) {
               // Handle the scanned code as you like, for example:
               console.log(`Code matched = ${decodedText}`, decodedResult);
+              var audio = new Audio('https://freesound.org/data/previews/262/262453_4416883-lq.mp3');
+              audio.play();
               window.location.replace(decodedText);
             }
     

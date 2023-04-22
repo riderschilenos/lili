6d0195f554dcfe0bb17c3b6fc0627ab2946eff63
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-04-22 00:02:52 -0400

    u

--- config/app.php
+++ config/app.php
@@ -162,7 +162,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
-        
+        Barryvdh\DomPDF\ServiceProvider::class,
+
         /*
          * Package Service Providers...
          */

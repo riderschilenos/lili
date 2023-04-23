d1735f858b7a53d25f46800d62efd79f85bcfc85
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-04-23 15:50:28 -0400

    u

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -9,7 +9,7 @@ class WhatsappController extends Controller
 {
     public function invitacion(Request $request){
 
-        $fono='569'.substr(str_replace(' ', '', $request->phone), -8);
+        //$fono='569'.substr(str_replace(' ', '', $request->phone), -8);
         //TOKEN QUE NOS DA FACEBOOK
         $token = env('WS_TOKEN');
         $phoneid= env('WS_PHONEID');

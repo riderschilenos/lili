c6145fc2667cef8c295c94ba7aa5870459b8c9b5
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-04 22:55:18 -0400

    u

--- resources/views/admin/estadisticas.blade.php
+++ resources/views/admin/estadisticas.blade.php
@@ -4,7 +4,9 @@
     <x-fast-view :riders="$riders" :autos="$autos" :series="$series" :socio2="$socio2" :disciplinas="$disciplinas">
      
         <div class="flex justify-center mb-2 mt-6 max-w-7xl mx-auto sm:px-6 lg:px-8">
-               
+            <div class="max-w-4xl mx-auto px-2 sm:px-6 mt-2 lg:px-8">
+                @livewire('admin.pedidos-count')
+            </div>
             <div class="max-w-7xl w-full bg-white shadow rounded-lg my-2 mx-4">
                @livewire('admin.contabilidad')
             </div>

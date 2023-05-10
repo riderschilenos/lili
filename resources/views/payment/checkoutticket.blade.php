eade28b17163d543f0684762011655bdefa1c7ba
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-10 18:24:38 -0400

    u

--- resources/views/payment/checkoutticket.blade.php
+++ resources/views/payment/checkoutticket.blade.php
@@ -350,7 +350,12 @@
                                             Fecha
                                         </th>
                                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Categoria
+                                        
+                                        @if ($evento->type=='pista')
+                                            Cilindrada
+                                        @else
+                                            Categoria
+                                        @endif   
                                         </th>
                                         <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Número

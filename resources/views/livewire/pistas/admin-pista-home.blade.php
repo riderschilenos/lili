dcefa94cb2e8a45d7735c6ee76e9ab77c292a3ca
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-10 23:05:25 -0400

    u

--- resources/views/livewire/pistas/admin-pista-home.blade.php
+++ resources/views/livewire/pistas/admin-pista-home.blade.php
@@ -64,29 +64,31 @@
         <div class="col-span-2 bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 my-2 mx-1" class="col-span-2">
             <h1 class="text-center mb-6 "> Administracion Pista <b>{{$pista->titulo}}</b></h1>
             <a href="{{route('organizador.eventos.retiros.fast',$pista)}}" >
-            <div class="flex items-center">
-                <div class="grid grid-cols-2 w-full">
-                    <div >
-                        <span class="text-4xl sm:text-8xl text-center leading-none font-bold text-gray-900">${{number_format($total-$total*0.072-$retiroacumulado)}}-.</span>
-                        <h3 class="sm:hidden text-base font-normal text-gray-500">Pend. Cobrar</h3>
-                        <h3 class="hidden sm:block text-base font-normal text-gray-500">Pendiente Cobrar</h3>
+                <div class="flex items-center">
+
+                    <div class="grid grid-cols-2 w-full">
+                        <div >
+                            <span class="text-4xl sm:text-8xl text-center leading-none font-bold text-gray-900">${{number_format($total-$total*0.072-$retiroacumulado)}}-.</span>
+                            <h3 class="sm:hidden text-base font-normal text-gray-500">Pend. Cobrar</h3>
+                            <h3 class="hidden sm:block text-base font-normal text-gray-500">Pendiente Cobrar</h3>
+                        </div>
+                        <div >
+                            <span class="text-4xl sm:text-8xl text-center leading-none font-bold text-gray-900">${{number_format($retiroacumulado)}}-.</span>
+                            <h3 class="sm:hidden text-base font-normal text-gray-500">Cobradas</h3>
+                            <h3 class="hidden sm:block text-base font-normal text-gray-500">Cobradas</h3>
+                        </div>
                     </div>
-                    <div >
-                        <span class="text-4xl sm:text-8xl text-center leading-none font-bold text-gray-900">${{number_format($retiroacumulado)}}-.</span>
-                        <h3 class="sm:hidden text-base font-normal text-gray-500">Cobradas</h3>
-                        <h3 class="hidden sm:block text-base font-normal text-gray-500">Cobradas</h3>
+                
+                    <div class="hidden sm:flex ml-5 w-10 items-center justify-end flex-1 text-red-500 text-base font-bold cursor-pointer">
+                        
+                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                        </svg>
                     </div>
+                
+            
                 </div>
-              
-                <div class="hidden sm:flex ml-5 w-10 items-center justify-end flex-1 text-red-500 text-base font-bold cursor-pointer">
-                    
-                    <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
-                    </svg>
-                </div>
-               
             </a>
-            </div>
             <div class="flex justify-center"> 
                 <a href="{{route('organizador.eventos.fechas',$pista)}}">
                     <button class="btn btn-danger ml-2 text-center text-xl mt-4">Entrenamientos</button>

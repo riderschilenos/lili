0066f7ef4e3c3badf091e869f25ff930d186282c
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-12 15:15:01 -0400

    u

--- resources/views/livewire/pistas/admin-pista-home.blade.php
+++ resources/views/livewire/pistas/admin-pista-home.blade.php
@@ -23,24 +23,31 @@
     @endforeach
 
     
-    <div class="mt-2 sm:mt-4 mb-4 w-full grid grid-cols-3 md:grid-cols-3 xl:grid-cols-3 gap-x-2 gap-y-2 items-center content-center">
+    <div class="bg-white shadow mt-2 sm:mt-4 mb-4 w-full grid grid-cols-3 md:grid-cols-3 xl:grid-cols-3 gap-x-2 gap-y-2 items-center content-center">
     
 
        
-            <div class="max-w-xl  bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 my-2 mx-1">
+            <div class="max-w-xl  bg-gray-100 shadow rounded-lg p-4 sm:p-6 xl:p-8 my-4 mx-4">
                 <a href="{{route('organizador.eventos.inscritos.fast',$pista)}}" class="col-span-1">
-                    <div class="flex items-center">
-                        <div class="flex-shrink-0">
-                        
+                    <div class="flex justify-center">
                             @isset($pista->image)
-                                <img class="h-12 w-12 rounded-full mb-4" src=" {{Storage::url($pista->image->url)}}" alt="">
+                                <img class="h-14 w-14 mb-4" src=" {{Storage::url($pista->image->url)}}" alt="">
                             @else
                                 <img loading="lazy" class="h-80 w-full object-cover" src="https://raindance.org/wp-content/uploads/2019/10/filmmaking-1080x675-1.jpg" alt="">
                 
-                        @endisset
-                        <span class="text-4xl sm:text-8xl leading-none text ml-4 font-bold text-gray-900">{{number_format($inscripciones->count())}}</span>
-                            <h3 class="sm:hidden text-base font-normal text-gray-500">Entradas Compr.<br>/Mes</h3>
-                            <h3 class="hidden sm:block text-base font-normal text-gray-500">Entradas Compradas/Mes</h3>
+                            @endisset
+                        </div>
+                    <div class="flex items-center">
+
+                        <div class="flex-shrink-0">
+                        
+                        <div class="flex justify-center">     
+                            <span class="text-4xl sm:text-8xl leading-none ml-6 font-bold text-gray-900 text-center">
+                                {{number_format($inscripciones->count())}} / 0
+                            </span>
+                        </div>
+                            <h3 class="sm:hidden text-base font-normal text-gray-500">Vendidas/Cobradas<br>/Mes</h3>
+                            <h3 class="hidden sm:block text-base font-normal text-gray-500">Vendidas/Cobradas/Mes</h3>
                         </div>
                         <div class="hidden sm:flex w-10 items-center justify-end flex-1 text-red-500 text-base font-bold cursor-pointer">
                             
@@ -62,7 +69,7 @@
 
       
         
-        <div class="col-span-2 bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 my-2 mx-1" class="col-span-2">
+        <div class="col-span-2 rounded-lg p-4 sm:p-6 xl:p-8 my-2 mx-1" class="col-span-2">
             <h1 class="text-center mb-6 "> Administracion Pista <b>{{$pista->titulo}}</b></h1>
             <a href="{{route('organizador.eventos.retiros.fast',$pista)}}" >
                 <div class="flex items-center">

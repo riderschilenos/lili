541f6baa856b958b771553e4532f904b83d07e06
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-12 21:46:21 -0400

    u

--- resources/views/livewire/evento-view.blade.php
+++ resources/views/livewire/evento-view.blade.php
@@ -1,6 +1,6 @@
-<div class="w-full bg-blue-900 p-20 flex justify-center">
+<div class="w-full bg-blue-900 p-20 flex justify-center h-screen">
   <div class="max-w-4xl my-12"> 
-      <div class="grid grid-cols-2 md:grid-cols-4 gap-x-4 gap-y-2 "> 
+      <div class="grid grid-cols-2 md:grid-cols-4 gap-x-4 gap-y-2 mx-2 md:mx-4"> 
           @foreach ($tickets->reverse() as $ticket)
 
                           <div class="bg-white shadow-lg rounded-xl p-4">

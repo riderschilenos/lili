754bcbebe2903464724e232c6ceb3cb767832e5b
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-13 02:06:16 -0400

    u

--- resources/views/organizador/eventos/categorias.blade.php
+++ resources/views/organizador/eventos/categorias.blade.php
@@ -1,20 +1,44 @@
-<x-ticket-layout :evento="$evento">
+<x-app-layout>
 
     
 
-
-    <h1 class="text-2xl font-bold"> 
-        @if ($evento->type=='carrera')
-        Fecha y Categorias
-        @else
-        Fechas y Categorias
-        @endif
-    </h1>
-    <hr class="mt-2 mb-6">
-
-        @livewire('organizador.fecha-categorias', ['fecha' => $fecha], key($fecha->id))
+    <style>
+        :root {
+            --main-color: #4a76a8;
+        }
     
+        .bg-main-color {
+            background-color: var(--main-color);
+        }
+    
+        .text-main-color {
+            color: var(--main-color);
+        }
+    
+        .border-main-color {
+            border-color: var(--main-color);
+        }
+    </style>
 
-</x-ticket-layout>
+<div class="flex justify-center">
+    <div class="mb-2 mt-6 max-w-7xl sm:px-6 lg:px-8 bg-white rounded-xl shadow-lg py-10 px-6 mx-4">
+                
+        <div class="max-w-7xl w-full rounded-lg my-2 mx-4 ">
+                <h1 class="text-2xl font-bold"> 
+                    @if ($evento->type=='carrera')
+                    Fecha y Categorias
+                    @else
+                    Fechas y Categorias
+                    @endif
+                </h1>
+                <hr class="mt-2 mb-6">
+
+                <div class="flex justify-center mx-auto">
+                    @livewire('organizador.fecha-categorias', ['fecha' => $fecha], key($fecha->id))
+                </div>
+        </div>
+    </div>
+</div>
+</x-app-layout>
 
 
--- resources/views/organizador/eventos/fechasfast.blade.php
+++ resources/views/organizador/eventos/fechasfast.blade.php
@@ -17,26 +17,26 @@
         }
     </style>
 
+<div class="flex justify-center">
+    <div class="mb-2 mt-6 max-w-7xl sm:px-6 lg:px-8 bg-white rounded-xl shadow-lg py-10 px-6 mx-4">
+                
+        <div class="max-w-7xl w-full rounded-lg my-2 mx-4 ">
 
-<div class="flex justify-center mb-2 mt-6 max-w-7xl sm:px-6 lg:px-8 bg-white rounded-xl shadow-lg py-10 px-6 mx-4">
-            
-    <div class="max-w-7xl w-full rounded-lg my-2 mx-4 ">
+            <h1 class="text-2xl font-bold text-center"> 
+                @if ($evento->type=='pista')
+                    Entrenamientos y Precios
+                @else
+                    Fechas y Categorias
+                @endif
+            </h1>
+            <hr class="mt-2 mb-6">
+        
 
-        <h1 class="text-2xl font-bold text-center"> 
-            @if ($evento->type=='pista')
-                Entrenamientos y Precios
-            @else
-                Fechas y Categorias
-            @endif
-        </h1>
-        <hr class="mt-2 mb-6">
+
     
+            @livewire('organizador.eventos-fechas', ['evento' => $evento], key($evento->id))
 
-
-  
-        @livewire('organizador.eventos-fechas', ['evento' => $evento], key($evento->id))
-
+        </div>
     </div>
 </div>
-
 </x-app-layout>

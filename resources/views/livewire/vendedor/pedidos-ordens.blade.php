2edf1d276ed53da34a3dd6ef47708d8c938f40bd
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-17 11:05:24 -0400

    u

--- resources/views/livewire/vendedor/pedidos-ordens.blade.php
+++ resources/views/livewire/vendedor/pedidos-ordens.blade.php
@@ -29,21 +29,25 @@
 
     <div x-data="{open: false, referencia: false}" class="pb-12">
         @if($pedido->status==1)
-            <div class="grid grid-cols-3 md:grid-cols-5 justify-center ">
-                        <button wire:click="select_product(1)" x-on:click="open=true" class="btn btn-danger max-w-xs items-center justify-items-center mt-2">CARCASA MX</button>
-                        <button wire:click="select_product(2)" x-on:click="open=true" class="btn btn-danger max-w-xs items-center justify-items-center mt-2 ml-2">CARCASA MTB</button>
-                        <button wire:click="select_product(7)" x-on:click="open=true" class="btn btn-danger max-w-xs items-center justify-items-center mt-2 ml-2">CARCASA MULTIMARCA</button>
-                        <button wire:click="select_product(11)" x-on:click="open=true" class="btn btn-danger max-w-xs items-center justify-items-center mt-2 ml-2">POLERON 29.990</button>
-                        <button wire:click="select_product(28)" x-on:click="open=true" class="btn btn-danger max-w-xs items-center justify-items-center mt-2 ml-2">ROMPECABEZAS PERSONALIZADO</button>             
-            </div>
+            @if (auth()->user())
+                <div class="grid grid-cols-3 md:grid-cols-5 justify-center ">
+                            <button wire:click="select_product(1)" x-on:click="open=true" class="btn btn-danger max-w-xs items-center justify-items-center mt-2">CARCASA MX</button>
+                            <button wire:click="select_product(2)" x-on:click="open=true" class="btn btn-danger max-w-xs items-center justify-items-center mt-2 ml-2">CARCASA MTB</button>
+                            <button wire:click="select_product(7)" x-on:click="open=true" class="btn btn-danger max-w-xs items-center justify-items-center mt-2 ml-2">CARCASA MULTIMARCA</button>
+                            <button wire:click="select_product(11)" x-on:click="open=true" class="btn btn-danger max-w-xs items-center justify-items-center mt-2 ml-2">POLERON 29.990</button>
+                            <button wire:click="select_product(28)" x-on:click="open=true" class="btn btn-danger max-w-xs items-center justify-items-center mt-2 ml-2">ROMPECABEZAS PERSONALIZADO</button>             
+                </div>
+            @endif
         @endif
         <div class="flex">
             <h1 class="text-xl font-bold mt-6">Subtotal: ${{number_format($subtotal)}}-.</h1>
             @if($pedido->status==1)
-                <a x-show="!open" x-on:click="open=true" class="flex items-center cursor-pointer justify-end ml-auto">
-                    <i class="far fa-plus-square text-2xl text-green-500 mr-2"></i>
-                    Agregar Producto
-                </a>
+                @if (auth()->user())
+                    <a x-show="!open" x-on:click="open=true" class="flex items-center cursor-pointer justify-end ml-auto">
+                        <i class="far fa-plus-square text-2xl text-green-500 mr-2"></i>
+                        Agregar Producto
+                    </a>
+                @endif
             @endif
         </div>
         <hr class="mt-2 mb-6 max-w-sm">
@@ -390,13 +394,13 @@
         @if($pedido->ordens->count()>0)
             @if($pedido->status==1)
                 <div class="flex justify-center pb-8">
-                                    
-                    <form action="{{route('vendedor.pedido.close',$pedido)}}" method="POST">
-                        @csrf
+                    @if(auth()->user())           
+                        <form action="{{route('vendedor.pedido.close',$pedido)}}" method="POST">
+                            @csrf
 
-                        <button class="btn btn-success justify-center my-4" type="submit">Finalizar->Pagar</button>
-                    </form>
-
+                            <button class="btn btn-success justify-center my-4" type="submit">Finalizar->Pagar</button>
+                        </form>
+                    @endif
                 </div>
 
             @elseif($pedido->status==2)

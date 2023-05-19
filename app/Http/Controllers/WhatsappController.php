3f63cdd3e2294c4b66460e738255dd15b879bbaf
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-18 23:09:55 -0400

    u

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -14,28 +14,35 @@ class WhatsappController extends Controller
         
         $fono='569'.substr(str_replace(' ', '', $num), -8);
         //TOKEN QUE NOS DA FACEBOOK
-        $token = env('WS_TOKEN');
-        $phoneid= env('WS_PHONEID');
-        $version='v16.0';
-        $url="https://riderschilenos.cl/";
-        $payload=[
-            'messaging_product' => 'whatsapp',
-            "preview_url"=> false,
-            'to'=>$fono,
+
+        try {
+            $token = env('WS_TOKEN');
+            $phoneid= env('WS_PHONEID');
+            $version='v16.0';
+            $url="https://riderschilenos.cl/";
+            $payload=[
+                'messaging_product' => 'whatsapp',
+                "preview_url"=> false,
+                'to'=>$fono,
+                
+                'type'=>'template',
+                    'template'=>[
+                        'name'=>'invitacion_de_registro',
+                        'language'=>[
+                            'code'=>'es'],
+                    
+                    ]
+                
+            ];
             
-            'type'=>'template',
-                'template'=>[
-                    'name'=>'invitacion_de_registro',
-                    'language'=>[
-                        'code'=>'es'],
-                 
-                ]
+            Http::withToken($token)->post('https://graph.facebook.com/'.$version.'/'.$phoneid.'/messages',$payload)->throw()->json();
             
-        ];
-        
-        Http::withToken($token)->post('https://graph.facebook.com/'.$version.'/'.$phoneid.'/messages',$payload)->throw()->json();
-        
-        return redirect()->back();
+            return redirect()->back();
+
+        } catch (\Throwable $th) {
+             return redirect()->back();
+        }
+       
     }
 
     public function webhook(){
@@ -67,7 +74,7 @@ class WhatsappController extends Controller
         //EXTRAEMOS EL TELEFONO DEL ARRAY
         //GUARDAMOS EL MENSAJE Y LA RESPUESTA EN EL ARCHIVO text.txt
         WhatsappMensaje::create(['numero'=> $respuesta['entry'][0]['changes'][0]['value']['messages'][0]['from'],
-                'mensaje'=>$respuesta['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']]);
+                                'mensaje'=>$respuesta['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']]);
       }
     
     

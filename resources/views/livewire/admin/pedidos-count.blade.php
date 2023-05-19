77fb8e086c329be4b521be23b4fda5321f186ed8
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-19 00:56:54 -0400

    u

--- resources/views/livewire/admin/pedidos-count.blade.php
+++ resources/views/livewire/admin/pedidos-count.blade.php
@@ -13,9 +13,9 @@
                </div>
                <div class="flex flex-col leading-tight">
                   <div class="text-2xl mt-1 flex items-center">
-                     <span class="text-gray-700 mr-3">Anderson Vanhron</span>
+                     <span class="text-gray-700 mr-3">{{$cliente->name}}</span>
                   </div>
-                  <span class="text-lg text-gray-600">Junior Developer</span>
+                  <span class="text-lg text-gray-600">{{$cliente->fono}}</span>
                </div>
             </div>
             <div class="flex items-center space-x-2">

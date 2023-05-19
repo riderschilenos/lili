06e453bf999e2b671b93ab4382968784d94f5d71
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-19 16:42:37 -0400

    u

--- resources/views/livewire/admin/pedidos-count.blade.php
+++ resources/views/livewire/admin/pedidos-count.blade.php
@@ -53,7 +53,7 @@
                      <div class="chat-message">
                         <div class="flex items-end justify-end">
                            <div class="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-                              <div><span class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-gray-300 text-gray-600 ">Your error message says permission denied, npm global installs must be given root privileges.</span></div>
+                              <div><span class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-gray-300 text-gray-600 ">{{$mensaje->mensaje}}</span></div>
                            </div>
                            <img src="https://riderschilenos.cl/img/logo.png" alt="My profile" class="w-6 h-6 rounded-full order-2">
                         </div>

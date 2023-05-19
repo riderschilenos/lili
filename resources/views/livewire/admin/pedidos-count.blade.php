df22122b0fae7248ccf3ad6c65655e8ec945a738
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-19 17:12:35 -0400

    u

--- resources/views/livewire/admin/pedidos-count.blade.php
+++ resources/views/livewire/admin/pedidos-count.blade.php
@@ -133,7 +133,7 @@
                         @endif
                      </span>
                   </div>
-                  <span class="whitespace-nowrap px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+                  <span class="whitespace-nowrap mt-1 px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
                      Pendiente de diseño
                  </span>
                </div>
@@ -171,7 +171,7 @@
                         @endif
                      </span>
                   </div>
-                  <span class="whitespace-nowrap px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800">
+                  <span class="whitespace-nowrap mt-1 px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800">
                      Pendiente de producción
                  </span>
                </div>
@@ -209,7 +209,7 @@
                         @endif
                      </span>
                   </div>
-                  <span class="whitespace-nowrap px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+                  <span class="whitespace-nowrap mt-1 px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
                      Pendiente de despacho
                  </span>
                </div>

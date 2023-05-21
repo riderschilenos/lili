921245deeb3ec4a3c043256256240afde68b6f54
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-21 15:59:45 -0400

    U

--- resources/views/components/fast-view.blade.php
+++ resources/views/components/fast-view.blade.php
@@ -698,13 +698,13 @@
 
                     @livewire('pistas-home')
 
-                    <a class="sm:hidden" href="https://riderschilenos.cl/eventos/mariocross">
+                    <a class="hidden" href="https://riderschilenos.cl/eventos/mariocross">
                         <img class="h-full w-full object-cover object-center" src="{{asset('img/home/mariocross2.png')}}" alt="">
                     </a>
 
                 @else
-
-                    <a class="sm:hidden" href="https://riderschilenos.cl/eventos/mariocross">
+                    @livewire('pistas-home')
+                    <a class="hidden" href="https://riderschilenos.cl/eventos/mariocross">
                         <img class="h-full w-full object-cover object-center mt-4" src="{{asset('img/home/mariocross2.png')}}" alt="">
                     </a>
                     <div class="flex justify-center">

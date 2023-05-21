ac033bd871ce50fde5c3ba32f47d3cc8ce3a113d
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-21 19:48:35 -0400

    u

--- resources/views/livewire/pistas-home.blade.php
+++ resources/views/livewire/pistas-home.blade.php
@@ -26,7 +26,7 @@
                         <a href="{{route('ticket.pista.show', $pista)}}">
                                     <p class="text-gray-500 text-sm mt-auto">Disciplina: {{$pista->disciplina->name}}</p> 
                                     <p class="text-gray-500 text-sm mb-2">Organizador: {{$pista->organizador->first()->name}}</p>
-                                    <p class="text-gray-500 text-sm mb-2 "><b>{{$pista->fechas_count}}</b> Entrenamientos </p> 
+                                    <p class="text-gray-500 text-sm mb-2 "><b>+{{$pista->fechas_count}}</b> Entrenamiento Realizado </p> 
                                     
 
                                     </a>

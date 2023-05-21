05abfef6199548758df26f9f754c7190848c3d64
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-21 16:47:39 -0400

    u

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,26 @@
 <x-guest-layout>
+   
+
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
         </x-slot>
-
+        <div class="flex justify-center">
+            <div class="max-w-4xl px-6 pt-2 mb-4 rounded-xl">
+    
+                <div class="flex justify-center mt-4 ">
+    
+                    <a href="https://riderschilenos.cl/login-google">
+                        <button class="btn bg-blue-500 text-white w-full items-center justify-items-center mr-2 mt-2"><svg class="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>Registrarme Con Google<div></div></button>
+                    </a>
+                
+                </div>
+             
+            </div>
+        </div>
         <x-jet-validation-errors class="mb-4" />
 
+        
         <form method="POST" action="{{ route('register') }}">
             @csrf
 

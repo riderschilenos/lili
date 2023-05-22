c48d420a2842c68a0761a861e9cf0fcbf329f63e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-05-21 20:30:37 -0400

    u

--- resources/views/Evento/show.blade.php
+++ resources/views/Evento/show.blade.php
@@ -382,7 +382,7 @@
                                         </div>
                                         
                                     </div>
-                                    <a href= "" class="btn bg-gray-100 my-2 btn-block">
+                                    <a href= "" class="hidden btn bg-gray-100 my-2 btn-block">
                                         ${{number_format($min)}} - ${{number_format($max)}}
                                     </a>
 

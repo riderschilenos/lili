3614d34fa2575fa4e3db9fe56e7d76415338f042
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-07-11 10:46:44 -0400

    u

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -6,43 +6,43 @@
 
 
                 @if ($search)
-                <ul class="absolute z-1 left-0 w-full bg-white mt-1 rounded-lg overflow-hidden px-4">
-                    @forelse ($this->riders as $rider)
-                        <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300">
-                            <a href="{{route('socio.show',$rider)}}">{{$rider->name}}</a>
-                        </li>
-                        @empty
-                        <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300">
-                            No se encontraron riders...
-                        </li>
-                            
-                    
-                    @endforelse
-                    @forelse ($this->series as $serie)
-                        <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300">
-                            <a href="{{route('series.show',$serie)}}">{{$serie->titulo}}</a>
-                        </li>
-                        @empty
-                        <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300">
-                            No se encontraron series...
-                        </li>
-                            
+                    <ul class="absolute z-1 left-0 w-full bg-white mt-1 rounded-lg overflow-hidden px-4">
+                        @forelse ($this->riders as $rider)
+                            <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300">
+                                <a href="{{route('socio.show',$rider)}}">{{$rider->name}}</a>
+                            </li>
+                            @empty
+                            <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300">
+                                No se encontraron riders...
+                            </li>
+                                
                         
-                    @endforelse
-                    @forelse ($this->eventos as $evento)
-                        <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300">
-                            <a href="{{route('ticket.evento.show',$evento)}}">{{$evento->titulo}}</a>
-                        </li>
-                        @empty
-                        <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300">
-                            No se encontraron eventos...
-                        </li>
+                        @endforelse
+                        @forelse ($this->series as $serie)
+                            <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300">
+                                <a href="{{route('series.show',$serie)}}">{{$serie->titulo}}</a>
+                            </li>
+                            @empty
+                            <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300">
+                                No se encontraron series...
+                            </li>
+                                
                             
-                        
-                    @endforelse
-                   
+                        @endforelse
+                        @forelse ($this->eventos as $evento)
+                            <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300">
+                                <a href="{{route('ticket.evento.show',$evento)}}">{{$evento->titulo}}</a>
+                            </li>
+                            @empty
+                            <li class="leading-10 px-5 text-sm cursor-pointer hover:bg-gray-300">
+                                No se encontraron eventos...
+                            </li>
+                                
+                            
+                        @endforelse
                     
-                </ul>
+                        
+                    </ul>
             
                     
                 @else

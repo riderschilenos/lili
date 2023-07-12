9a864101ec12db729da966f14ab579ae763e8def
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-07-11 23:46:42 -0400

    u

--- resources/views/socio/show.blade.php
+++ resources/views/socio/show.blade.php
@@ -151,9 +151,12 @@
                                             <div class="grid grid-cols-2">
                                                 <div class="px-4 py-2 font-semibold">Nro. Contacto</div>
                                                 
-                                                <a class="btn btn-success" href="https://api.whatsapp.com/send?phone=569{{substr(str_replace(' ', '', $socio->fono), -8)}}&text=Hola" target="_blank">
-                                                    <div class="px-4 py-2">{{ $socio->fono }}</div>
-                                                </a>
+                                                @can('Ver dashboard')
+                                                    <a class="btn btn-success" href="https://api.whatsapp.com/send?phone=569{{substr(str_replace(' ', '', $socio->fono), -8)}}&text=Hola" target="_blank">
+                                                        <div class="px-4 py-2">{{ $socio->fono }}</div>
+                                                    </a> 
+                                                @endcan
+                                                
 
                                             </div>
                                         @endif

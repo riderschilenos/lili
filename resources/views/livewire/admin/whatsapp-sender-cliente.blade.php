53f06fbc404de8181552baa8707fad4323aaa328
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-07-12 13:27:28 -0400

    u

--- resources/views/livewire/admin/whatsapp-sender-cliente.blade.php
+++ resources/views/livewire/admin/whatsapp-sender-cliente.blade.php
@@ -1,18 +1,20 @@
 <div>
-    <div class="block">
+    <div class="block bg-gray-800 p-3 mt-4 rounded-lg">
                                 
         <div class="mb-4">
             
-            <h1 class="text-center font-bold mt-6 mb-2 text-white">Nro de Whatsapp:</h1>
+            <h1 class="text-center font-bold  mb-2 text-white">Nro de Whatsapp:</h1>
             <input wire:model="nro" class="form-input w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none">
         </div>
      
 
-        <div class="flex justify-center mb-4">                 
+        <div class="grid grid-cols-3 gap-y-2 justify-center mb-4">                 
             <button class="btn btn-success mx-2" wire:click="invitacion">Invitación Registro</button>
             <button class="btn btn-success mx-2" wire:click="carcasas">Catálogo Carcasas</button>
             <button class="btn btn-success mx-2" wire:click="accesorios">Catálogo Accesorios</button>
             <button class="btn btn-success mx-2" wire:click="polerones">Catálogo Polerones</button>
         </div>
+
+        
     </div> 
 </div>

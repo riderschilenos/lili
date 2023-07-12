c287138d43a03add4ec8fe9623f320c287376e5d
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-07-12 12:25:23 -0400

    u

--- resources/views/components/fast-view.blade.php
+++ resources/views/components/fast-view.blade.php
@@ -724,14 +724,19 @@
             
                                 <div class="max-w-4xl mx-auto px-2 sm:px-6 mt-2 lg:px-8 pb-4" x-data="{whatsap: true}">
                                     <div class="flex justify-between">
+                                        
                                         <button class="btn btn-success ml-2 text-center text-xl" x-on:click="whatsap=!whatsap">Whatsapp RCH</button>
+                                        
                                         <a href="{{route('contabilidad')}}">
                                             <button class="btn btn-danger ml-2 text-center text-xl">Gráficos y Estadisticas</button>
                                         </a>
+
                                         <a href="{{route('vendedor.pedidos.create')}}">
                                             <button class="btn btn-success ml-2 text-center text-xl">Nuevo Pedido</button>
                                         </a>
+
                                     </div>
+
                                     <div x-show="!whatsap">
             
                                     @livewire('admin.whatsapp-sender-cliente')

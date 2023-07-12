6e9119d25a45dd33bbeefe07894d944cf9aa28a5
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-07-12 13:52:37 -0400

    u

--- resources/views/admin/estadisticas.blade.php
+++ resources/views/admin/estadisticas.blade.php
@@ -2,7 +2,7 @@
 <x-app-layout>
 
     <x-fast-view :riders="$riders" :autos="$autos" :series="$series" :socio2="$socio2" :disciplinas="$disciplinas">
-        <div class="max-w-7xl mx-auto px-2 sm:px-6 mt-2 lg:px-8">
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 mt-6 lg:px-8">
             @livewire('admin.money-info')
         </div>
         <div class="flex justify-center mb-2 mt-6 max-w-7xl mx-auto sm:px-6 lg:px-8">

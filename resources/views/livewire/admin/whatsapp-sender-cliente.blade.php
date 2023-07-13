f7af9f4c30685656817ebca707982bf03bce70b2
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-07-13 18:27:48 -0400

    u

--- resources/views/livewire/admin/whatsapp-sender-cliente.blade.php
+++ resources/views/livewire/admin/whatsapp-sender-cliente.blade.php
@@ -15,58 +15,58 @@
             <button class="btn btn-success mx-2" wire:click="polerones">Catálogo Polerones</button>
         </div>
     <div class="bg-white ">
+        <div class="px-6 py-4 flex justify-center max-w-3xl mx-6">
+            <div class="max-w-xl  bg-white shadow rounded-lg p-2 sm:p-6 xl:p-8 my-2">
+                <div class="flex items-center">
+                   <div class="flex-shrink-0">
+                      <span class="text-2xl sm:text-8xl leading-none font-bold text-gray-900">{{number_format($guess_all->count())}}</span>
+                      <h3 class="sm:hidden text-base font-normal text-gray-500">Invitados</h3>
+                      <h3 class="hidden sm:block text-base font-normal text-gray-500">Invitados</h3>
+                   </div>
+                   <div class="hidden sm:flex w-10 items-center justify-end flex-1 text-red-500 text-base font-bold cursor-pointer">
+                      
+                      <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                         <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                      </svg>
+                   </div>
+                </div>
+             </div>
+             <input wire:keydown="limpiar_page" wire:model="search"  class="form-input my-auto items-center mx-4 shadow-sm justify-center text-center border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg focus:outline-none" placeholder="Ingrese el nombre, rut, fono o email del invitado para saber si a comprado antes">
+             <div class="max-w-xl  bg-white shadow rounded-lg p-2 sm:p-6 xl:p-8 my-2">
+                <div class="flex items-center">
+                   <div class="flex-shrink-0">
+                      <span class="text-2xl sm:text-8xl leading-none font-bold text-gray-900">{{number_format($socios_all->count())}}</span>
+                      <h3 class="sm:hidden text-base font-normal text-gray-500">Socios</h3>
+                      <h3 class="hidden sm:block text-base font-normal text-gray-500">Socios</h3>
+                   </div>
+                   <div class="hidden sm:flex w-10 items-center justify-end flex-1 text-red-500 text-base font-bold cursor-pointer">
+                      
+                      <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                         <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                      </svg>
+                   </div>
+                </div>
+             </div>
+        </div>
         <x-table-responsive>
-            <div class="px-6 py-4 flex justify-center max-w-3xl">
-                <div class="max-w-xl  bg-white shadow rounded-lg p-2 sm:p-6 xl:p-8 my-2">
-                    <div class="flex items-center">
-                       <div class="flex-shrink-0">
-                          <span class="text-2xl sm:text-8xl leading-none font-bold text-gray-900">{{number_format($guess_all->count())}}</span>
-                          <h3 class="sm:hidden text-base font-normal text-gray-500">Invitados</h3>
-                          <h3 class="hidden sm:block text-base font-normal text-gray-500">Invitados</h3>
-                       </div>
-                       <div class="hidden sm:flex w-10 items-center justify-end flex-1 text-red-500 text-base font-bold cursor-pointer">
-                          
-                          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                             <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
-                          </svg>
-                       </div>
-                    </div>
-                 </div>
-                 <input wire:keydown="limpiar_page" wire:model="search"  class="form-input my-auto items-center mx-4 shadow-sm justify-center text-center border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg focus:outline-none" placeholder="Ingrese el nombre, rut, fono o email del invitado para saber si a comprado antes">
-                 <div class="max-w-xl  bg-white shadow rounded-lg p-2 sm:p-6 xl:p-8 my-2">
-                    <div class="flex items-center">
-                       <div class="flex-shrink-0">
-                          <span class="text-2xl sm:text-8xl leading-none font-bold text-gray-900">{{number_format($socios_all->count())}}</span>
-                          <h3 class="sm:hidden text-base font-normal text-gray-500">Socios</h3>
-                          <h3 class="hidden sm:block text-base font-normal text-gray-500">Socios</h3>
-                       </div>
-                       <div class="hidden sm:flex w-10 items-center justify-end flex-1 text-red-500 text-base font-bold cursor-pointer">
-                          
-                          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                             <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
-                          </svg>
-                       </div>
-                    </div>
-                 </div>
-            </div>
+          
     
             @if ($guess->count())
     
-                <table class="min-w-full divide-y divide-gray-200">
-                    <thead class="bg-gray-50">
+            <table class="divide-y divide-gray-200">
+                <thead class="bg-gray-50">
                     <tr>
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase ">
                         Nombre
                         </th>
-                 
-                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                    
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase ">
                             Fono
                         </th>
                     
-                      
                     </tr>
-                    </thead>
-                    <tbody class="bg-white divide-y divide-gray-200">
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
     
                         @foreach ($guess as $invitado)
          

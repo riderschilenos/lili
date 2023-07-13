2a239c9d2c989bb0c399dd011516fc6af13fcf22
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-07-13 18:09:25 -0400

    u

--- resources/views/livewire/admin/whatsapp-sender-cliente.blade.php
+++ resources/views/livewire/admin/whatsapp-sender-cliente.blade.php
@@ -58,9 +58,7 @@
                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                         Nombre
                         </th>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Email
-                        </th>
+                 
                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                             Fono
                         </th>
@@ -141,11 +139,9 @@
                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                         Nombre
                         </th>
+                       
                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Email
-                        </th>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Rut
+                            Fono
                         </th>
                     
                         <th scope="col" class="relative px-6 py-3">

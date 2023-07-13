52f6ccc5dccbbda064280dd19d0dda114a22bd92
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-07-13 18:22:39 -0400

    u

--- resources/views/livewire/admin/whatsapp-sender-cliente.blade.php
+++ resources/views/livewire/admin/whatsapp-sender-cliente.blade.php
@@ -55,11 +55,11 @@
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead class="bg-gray-50">
                     <tr>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
                         Nombre
                         </th>
                  
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
                             Fono
                         </th>
                     
@@ -72,7 +72,7 @@
          
                                 
                                 <tr wire:click="updateinvitado_id({{$invitado->id}})">
-                                    <td class="px-6 py-4 whitespace-nowrap">
+                                    <td class="px-6 py-4 ">
                                         <div class="flex items-center">
                                             <div class="hidden md:flex flex-shrink-0 h-10 w-10">
                                                     
@@ -127,14 +127,14 @@
 
             @if ($socios->count())
 
-                <table class="min-w-full divide-y divide-gray-200">
+                <table class="divide-y divide-gray-200">
                     <thead class="bg-gray-50">
                     <tr>
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase ">
                         Nombre
                         </th>
                        
-                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase ">
                             Fono
                         </th>
                     
@@ -146,7 +146,7 @@
                           
                                 
                                 <tr wire:click="updatesocio_id({{$socio->id}})" >
-                                    <td class="px-6 py-4 whitespace-nowrap">
+                                    <td class="px-6 py-4">
                                         <div class="flex items-center">
                                             <div class="hidden md:flex flex-shrink-0 h-10 w-10">
                                                     

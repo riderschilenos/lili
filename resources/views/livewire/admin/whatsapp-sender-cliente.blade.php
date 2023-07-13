56f0390ea099f265783d9d93da8386f6297b7682
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-07-13 19:15:17 -0400

    u

--- resources/views/livewire/admin/whatsapp-sender-cliente.blade.php
+++ resources/views/livewire/admin/whatsapp-sender-cliente.blade.php
@@ -32,6 +32,21 @@
                    </div>
                 </div>
              </div>
+             <div class="md:hidden max-w-xl mx-4 bg-white shadow rounded-lg p-2 sm:p-6 xl:p-8 my-2">
+                <div class="flex items-center">
+                   <div class="flex-shrink-0">
+                      <span class="text-2xl sm:text-8xl leading-none font-bold text-gray-900">{{number_format($socios_all->count()+$guess_all->count())}}</span>
+                      <h3 class="sm:hidden text-base font-normal text-gray-500">Total</h3>
+                      <h3 class="hidden sm:block text-base font-normal text-gray-500">Total</h3>
+                   </div>
+                   <div class="hidden sm:flex w-10 items-center justify-end flex-1 text-red-500 text-base font-bold cursor-pointer">
+                      
+                      <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                         <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                      </svg>
+                   </div>
+                </div>
+             </div>
         </div>
         <div class="mb-4">
             

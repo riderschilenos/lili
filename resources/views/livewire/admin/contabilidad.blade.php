20f1948403230fdee393ba9ce163bbeeca51900d
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-07-22 01:53:06 -0400

    u

--- resources/views/livewire/admin/contabilidad.blade.php
+++ resources/views/livewire/admin/contabilidad.blade.php
@@ -499,6 +499,21 @@ if($orden->producto->id==1 || $orden->producto->id==2 || $orden->producto->id==3
                  <div class="max-w-xl  bg-white shadow rounded-lg p-2 sm:p-6 xl:p-8 my-2">
                     <div class="flex items-center">
                        <div class="flex-shrink-0">
+                          <span class="text-4xl sm:text-8xl leading-none font-bold text-gray-900">{{number_format($polerones)}}</span>
+                          <h3 class="sm:hidden text-base font-normal text-gray-500">Polerones</h3>
+                          <h3 class="hidden sm:block text-base font-normal text-gray-500">Polerones</h3>
+                       </div>
+                       <div class="hidden sm:flex w-10 items-center justify-end flex-1 text-red-500 text-base font-bold cursor-pointer">
+                          
+                          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                             <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                          </svg>
+                       </div>
+                    </div>
+                 </div>
+                 <div class="max-w-xl  bg-white shadow rounded-lg p-2 sm:p-6 xl:p-8 my-2">
+                    <div class="flex items-center">
+                       <div class="flex-shrink-0">
                           <span class="text-4xl sm:text-8xl leading-none font-bold text-gray-900">{{number_format($llaveros)}}</span>
                           <h3 class="sm:hidden text-base font-normal text-gray-500">Llaveros</h3>
                           <h3 class="hidden sm:block text-base font-normal text-gray-500">Llaveros</h3>
@@ -556,21 +571,7 @@ if($orden->producto->id==1 || $orden->producto->id==2 || $orden->producto->id==3
                        </div>
                     </div>
                  </div>
-                 <div class="max-w-xl  bg-white shadow rounded-lg p-2 sm:p-6 xl:p-8 my-2">
-                    <div class="flex items-center">
-                       <div class="flex-shrink-0">
-                          <span class="text-4xl sm:text-8xl leading-none font-bold text-gray-900">{{number_format($polerones)}}</span>
-                          <h3 class="sm:hidden text-base font-normal text-gray-500">Polerones</h3>
-                          <h3 class="hidden sm:block text-base font-normal text-gray-500">Polerones</h3>
-                       </div>
-                       <div class="hidden sm:flex w-10 items-center justify-end flex-1 text-red-500 text-base font-bold cursor-pointer">
-                          
-                          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                             <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
-                          </svg>
-                       </div>
-                    </div>
-                 </div>
+                
                  <div class="max-w-xl  bg-white shadow rounded-lg p-2 sm:p-6 xl:p-8 my-2">
                     <div class="flex items-center">
                        <div class="flex-shrink-0">

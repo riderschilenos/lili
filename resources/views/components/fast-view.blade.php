82260fb010ee0b5cb387ba264b4a7fe09c721d58
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-07-27 15:39:35 -0400

    u

--- resources/views/components/fast-view.blade.php
+++ resources/views/components/fast-view.blade.php
@@ -751,12 +751,13 @@
                             </div>
                         @endcan    
                 
-                
+                        @livewire('vendedor.catalogo-productos')
+                        
                         <div class="max-w-4xl mx-auto px-2 sm:px-6 mt-2 lg:px-8">
                             @livewire('pistas.admin-pista-home')
                         </div>
            
-                    @livewire('vendedor.catalogo-productos')
+                   
                                                   
                     @livewire('pistas-home')
 

9b907b13774a50bc9d88d98e559970dcb6f89694
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-07-28 21:37:03 -0400

    u

--- resources/views/livewire/vendedor/catalogo-productos.blade.php
+++ resources/views/livewire/vendedor/catalogo-productos.blade.php
@@ -219,7 +219,7 @@
        
                 <h1 class="text-center my-4 font-bold">Nuestros Trabajos Anteriores </h1>
                 <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-x-2 gap-y-2">
-                    @foreach ($ordens as $orden)    
+                    @foreach ($ordens->reverse() as $orden)    
                             @if ($orden->images)
                                 @foreach ($orden->images as $image)
 

5faea72011b343411f0248a139d8176016c29963
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-01 22:41:19 -0400

    u

--- resources/views/socio/show.blade.php
+++ resources/views/socio/show.blade.php
@@ -343,8 +343,8 @@
                                             </ol>
                                             <div class="flex justify-end"> 
                                                 <a href="#"
-                                                class="inline-flex justify-end ml-auto items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Learn
-                                                more <svg class="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20"
+                                                class="inline-flex justify-end ml-auto items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Ver
+                                                más <svg class="w-3 h-3 ml-2" fill="currentColor" viewBox="0 0 20 20"
                                                     xmlns="http://www.w3.org/2000/svg">
                                                     <path fill-rule="evenodd"
                                                         d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"

07c7743f990ba532a8e6c8f9dfb076f2a104f562
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-01 23:10:56 -0400

    u

--- resources/views/socio/show.blade.php
+++ resources/views/socio/show.blade.php
@@ -303,18 +303,18 @@
                                         </div>
 
                                         <!-- This is an example component -->
-                                        <div class="max-w-5xl mx-auto">
+                                        <div class="max-w-5xl mx-auto hidden">
 
 
                                             <ol class="relative border-l border-gray-200 dark:border-gray-700">
-                                                <li class="mb-10 ml-4">
+                                                <li class="mb-10 ml-2">
                                                     <div
                                                         class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                                                     </div>
-                                                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time>
+                                                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 ml-2">February 2022</time>
                                                     <div class="flex items-center">
-                                                        <img class="h-14 object-contain mr-2 cursor-pointer items-center" src="{{asset('img/pdf_icon2.png')}}" title="Descargar" alt="">
-                                                        <div>
+                                                        <img class="h-16 object-contain mr-4 cursor-pointer items-center" src="{{asset('img/pdf_icon2.png')}}" title="Descargar" alt="">
+                                                        <div class="ml-2">
                                                             <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Application UI code in Tailwind CSS</h3>
                                                             <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages
                                                                 including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce &amp; Marketing
@@ -323,20 +323,20 @@
                                                     </div>
                                                   
                                                 </li>
-                                                <li class="mb-10 ml-4">
+                                                <li class="mb-10 ml-2">
                                                     <div
                                                         class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                                                     </div>
-                                                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">March 2022</time>
+                                                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 ml-2">March 2022</time>
                                                     <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Marketing UI design in Figma</h3>
                                                     <p class="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first
                                                         designed in Figma and we keep a parity between the two versions even as we update the project.</p>
                                                 </li>
-                                                <li class="ml-4">
+                                                <li class="ml-2">
                                                     <div
                                                         class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                                                     </div>
-                                                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">April 2022</time>
+                                                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 ml-2">April 2022</time>
                                                     <h3 class="text-lg font-semibold text-gray-900 dark:text-white">E-Commerce UI code in Tailwind CSS</h3>
                                                     <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components
                                                         and interactive elements built on top of Tailwind CSS.</p>

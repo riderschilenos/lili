6ef6e6053831d582683bbab15243ac033def06fd
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-01 23:23:58 -0400

    u

--- resources/views/components/socio-card.blade.php
+++ resources/views/components/socio-card.blade.php
@@ -29,36 +29,40 @@
                         </div> --}}
                         @if(!is_null($socio->direccion))
                         <div class="flex  text-sm justify-between px-2">
-                            <div class="px-2 py-2 text-red-500 font-semibold">
-                                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                            </div>
-                            <div class="px-2 py-2">{{Str::limit($socio->direccion->comuna.', '.$socio->direccion->region,20)}}</div>
+                            <a href="{{route('socio.show', $socio)}}">
+                                <div class="px-2 py-2 text-red-500 font-semibold">
+                                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                </div>
+                                <div class="px-2 py-2">{{Str::limit($socio->direccion->comuna.', '.$socio->direccion->region,20)}}</div>
+                            </a>
                         </div>
                         @endif
-                        <table class="text-xs mt-auto">
-                            <tbody>
+                        <a href="{{route('socio.show', $socio)}}">
+                            <table class="text-xs mt-auto">
+                                <tbody>
 
-                            <tr>
-                                <td class="px-2 py-2 text-gray-500 font-semibold">Disciplina</td>
-                                <td class="px-2 py-2">{{$socio->disciplina->name}}</td>
-                            </tr>
-                            
-                        </tbody></table>
-                        
-                        <div class="flex justify-center">
-                            @switch($socio->status)
-                                @case(1)
-                                    <span class="mx-auto"><span class="bg-green-500 py-1 px-2 rounded text-white text-sm">{{$socio->disciplina->name}}</span></span>
-                                    @break
-                                @case(2)
-                                    <span class="mx-auto"><span class="bg-red-500 py-1 px-2 rounded text-white text-sm">{{$socio->disciplina->name}}</span></span>
-                                    @break
-                                @default
+                                <tr>
+                                    <td class="px-2 py-2 text-gray-500 font-semibold">Disciplina</td>
+                                    <td class="px-2 py-2">{{$socio->disciplina->name}}</td>
+                                </tr>
                                 
-                            @endswitch
-                        </div>
-
-                        <div class="text-center my-3">
+                            </tbody></table>
+                        </a>
+                        <a href="{{route('socio.show', $socio)}}">
+                            <div class="flex justify-center">
+                                @switch($socio->status)
+                                    @case(1)
+                                        <span class="mx-auto"><span class="bg-green-500 py-1 px-2 rounded text-white text-sm">{{$socio->disciplina->name}}</span></span>
+                                        @break
+                                    @case(2)
+                                        <span class="mx-auto"><span class="bg-red-500 py-1 px-2 rounded text-white text-sm">{{$socio->disciplina->name}}</span></span>
+                                        @break
+                                    @default
+                                    
+                                @endswitch
+                            </div>
+                        </a>
+                        <div class="text-center my-3 hidden">
                             <a href= "{{route('socio.show', $socio)}}" class="text-sm text-red-500 italic hover:underline hover:text-red-600 font-medium" href="#">Ver Perfil</a>
                         </div>
             

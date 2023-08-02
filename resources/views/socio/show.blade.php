d037b036b9d095b4f4a7aab99f10389a7ed68e7a
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-01 23:02:20 -0400

    u

--- resources/views/socio/show.blade.php
+++ resources/views/socio/show.blade.php
@@ -312,10 +312,15 @@
                                                         class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                                                     </div>
                                                     <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time>
-                                                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Application UI code in Tailwind CSS</h3>
-                                                    <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages
-                                                        including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce &amp; Marketing
-                                                        pages.</p>
+                                                    <div class="flex items-center">
+                                                        <img class="h-14 object-contain mr-2 cursor-pointer items-center" src="{{asset('img/pdf_icon2.png')}}" title="Descargar" alt="">
+                                                        <div>
+                                                            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Application UI code in Tailwind CSS</h3>
+                                                            <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages
+                                                                including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce &amp; Marketing
+                                                                pages.</p>
+                                                        </div>
+                                                    </div>
                                                   
                                                 </li>
                                                 <li class="mb-10 ml-4">

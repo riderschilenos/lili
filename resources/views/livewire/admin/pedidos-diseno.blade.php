4e055cda8835c9b4d5fc6f2443693a641ff28aae
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-11 11:23:28 -0400

    u

--- resources/views/livewire/admin/pedidos-diseno.blade.php
+++ resources/views/livewire/admin/pedidos-diseno.blade.php
@@ -189,7 +189,7 @@
                                             ORDENES
                                         </th>
                                       
-                                        <th class="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase max-w-2xl w-full">
+                                        <th class="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase">
                                         Producto
                                         </th>
                                         <th scope="col" class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">

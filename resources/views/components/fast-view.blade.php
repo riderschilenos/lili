309e85cbe2421bdf0bbee8f7fe7940f25f53b349
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-12 15:22:14 -0400

    u

--- resources/views/components/fast-view.blade.php
+++ resources/views/components/fast-view.blade.php
@@ -1045,7 +1045,29 @@
     </div>
 
     <div :class="{'block': registro, 'hidden': ! registro}" class="hidden">
+        <h1 class="text-xs font-bold text-center my-1">Registro Nacional de Motos y Bicicletas</h1>
+        <div class="max-w-7xl mx-auto pb-8 ">      
+            <div class="card">
+                <div class="card-body">
+                    
+                    
+                        
 
+                         
+
+                    
+                        
+                    @livewire('vehiculo.vehiculo-search')
+                
+                </div>
+            </div>
+
+        </div>
+
+        <x-slot name="js">
+            <script src="https://cdn.ckeditor.com/ckeditor5/31.0.0/classic/ckeditor.js"></script>
+            <script src="{{asset('js/filmmaker/series/form.js')}}"></script>
+        </x-slot>
     </div>
 
     <div :class="{'block': vendedor, 'hidden': ! vendedor}" class="hidden">

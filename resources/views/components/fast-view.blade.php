115c78a0f4010fd117338511ecfdf7360b10d6a5
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-12 15:34:28 -0400

    u

--- resources/views/components/fast-view.blade.php
+++ resources/views/components/fast-view.blade.php
@@ -1031,7 +1031,19 @@
     
                    
 
-                
+                <div  class="mt-4 text-2xl mb-4 sm:text-xl mx-4 leading-none font-bold text-gray-900 flex justify-between">
+                    <div class="flex justify-center">
+                       
+                        <button class="btn btn-danger ml-6 text-center text-base" @click="user = false; home = false; socio = true; registro = false; vendedor = false; base = false" >RIDERS</button>
+                               
+                    </div>
+                    <div class="flex justify-center">
+                      
+                                
+                                    <button class="btn btn-danger mr-6 text-center text-base" @click="user = false; home = false; socio = false; registro = true; vendedor = false; base = false" >BIKES</button>
+                               
+                    </div>
+                </div>
     
             
     
@@ -1045,6 +1057,19 @@
     </div>
 
     <div :class="{'block': registro, 'hidden': ! registro}" class="hidden">
+        <div  class="mt-4 text-2xl mb-4 sm:text-xl mx-4 leading-none font-bold text-gray-900 flex justify-between">
+            <div class="flex justify-center">
+               
+                <button class="btn btn-danger ml-6 text-center text-base" @click="user = false; home = false; socio = true; registro = false; vendedor = false; base = false" >RIDERS</button>
+                       
+            </div>
+            <div class="flex justify-center">
+              
+                        
+                            <button class="btn btn-danger mr-6 text-center text-base" @click="user = false; home = false; socio = false; registro = true; vendedor = false; base = false" >BIKES</button>
+                       
+            </div>
+        </div>
         <h1 class="text-xs font-bold text-center my-1">Registro Nacional de Motos y Bicicletas</h1>
         <div class="max-w-7xl mx-auto pb-8 ">      
             <div class="card">

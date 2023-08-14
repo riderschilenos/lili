945580a8af5522d90b1f8dcaee51746cc3700ff1
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-14 15:39:57 -0400

    i

--- resources/views/socio/show.blade.php
+++ resources/views/socio/show.blade.php
@@ -196,7 +196,7 @@
                                     
                                         <div>
                                             <div class="mt-6">
-                                                <div class="font-semibold text-center text-sm mb-2">AUSPICIA A {{Str::limit(strtoupper($socio->name),10)}} AHORA!!</div>
+                                                <div class="font-semibold text-center text-sm mb-2">AU$PICIA A {{Str::limit(strtoupper($socio->name),10)}} AHORA!!</div>
                                                 <p class="text-center text-xs mb-2">Al realizar tu colaboración monetaria, apareceras en el perfil del rider como un auspiciador, manteniendo la cantidad donada de forma anonima.</p>
                                          
                                                 <div><input class="hidden mt-1 w-full rounded-[4px] border border-[#A0ABBB] p-2" value="1.000" type="text" placeholder="1.000" /></div>

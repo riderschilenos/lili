0a695236dbbe6fe6e5a12ede7c42a73f5ca62a5a
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-15 19:00:25 -0400

    u

--- resources/views/vehiculo/garage/show.blade.php
+++ resources/views/vehiculo/garage/show.blade.php
@@ -1,4 +1,10 @@
 <x-app-layout>
+    <x-slot name="tl">
+            
+        <title>{{$vehiculo->marca->name.' '.strtoupper($vehiculo->modelo).$vehiculo->cilindrada.' '.$vehiculo->año}}</title>
+        
+        
+    </x-slot>
 
     <x-fast-view :riders="$riders" :autos="$autos" :series="$series" :socio2="$socio2" :disciplinas="$disciplinas">
 

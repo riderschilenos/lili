36b7e1965d84cd97f86b29acdb61a2850d0b1448
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-16 01:09:40 -0400

    u

--- app/Http/Controllers/Ticket/EventoController.php
+++ app/Http/Controllers/Ticket/EventoController.php
@@ -7,9 +7,14 @@ use App\Models\Disciplina;
 use App\Models\Evento;
 use App\Models\Fecha;
 use App\Models\Precio;
+use App\Models\Serie;
+use App\Models\Socio;
 use App\Models\Ticket;
 use App\Models\User;
+use App\Models\Vehiculo;
 use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Cache;
 
 class EventoController extends Controller
 {
@@ -19,8 +24,48 @@ class EventoController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        return view('Evento.index');
+    {    if(Cache::has('autos')){
+            $autos = Cache::get('autos');
+        }else{
+            $autos = Vehiculo::where('status',4)
+                            ->orwhere('status',5)
+                            ->orwhere('status',7)
+                            ->latest('id')->get()->take(3);
+            Cache::put('autos',$autos);
+        }
+
+        if(Cache::has('series')){
+            $series = Cache::get('series');
+        }else{
+            $series = Serie::where('status',3)->where('content','serie')->latest('id')->get()->take(8);
+            Cache::put('series',$series);
+        }
+
+
+        if(Cache::has('riders')){
+            $riders = Cache::get('riders');
+        }else{
+            $riders = Socio::where('status',1)->orwhere('status',2)->latest('id')->get()->take(4);
+            Cache::put('riders',$riders);
+        }
+
+        if(auth()->user())
+        {
+        if(auth()->user()->socio)
+        {
+        $socio2 = Socio::where('user_id',auth()->user()->id)->first();
+        }else{
+        $socio2=null;
+        }
+
+        }
+        else{
+        $socio2=null;
+        }
+
+        $disciplinas= Disciplina::pluck('name','id');
+
+        return view('Evento.index',compact('series','riders','autos','socio2','disciplinas'));
     }
 
     /**
--- resources/views/Evento/index.blade.php
+++ resources/views/Evento/index.blade.php
@@ -5,41 +5,42 @@
         
         
     </x-slot>
-    <style>
-        :root {
-            --main-color: #4a76a8;
-        }
-    
-        .bg-main-color {
-            background-color: var(--main-color);
-        }
-    
-        .text-main-color {
-            color: var(--main-color);
-        }
-    
-        .border-main-color {
-            border-color: var(--main-color);
-        }
-    </style>
-    <section class="bg-cover bg-center" style="background-image: url({{asset('img/home/riders.jpg')}})">
+    <x-fast-view :riders="$riders" :autos="$autos" :series="$series" :socio2="$socio2" :disciplinas="$disciplinas">
+        <style>
+            :root {
+                --main-color: #4a76a8;
+            }
+        
+            .bg-main-color {
+                background-color: var(--main-color);
+            }
+        
+            .text-main-color {
+                color: var(--main-color);
+            }
+        
+            .border-main-color {
+                border-color: var(--main-color);
+            }
+        </style>
+        <section class="bg-cover bg-center" style="background-image: url({{asset('img/home/riders.jpg')}})">
 
-        <div class="max-w-7xl mx-auto px-4 sm:px-8 lg:px-8 py-24">
-            <div class="w-full md:w-3/4 lg:w-1/2">
-            <h1 class="text-white font-bold text-4xl">Carreras, Clinicas y Entrenamientos</h1>
-            <p class="text-white text-lg mt-2 mb-4">Inscribete y reserva tu cupo ahora!!</p>
-                <!-- component -->
-                <!-- This is an example component 
+            <div class="max-w-7xl mx-auto px-4 sm:px-8 lg:px-8 py-24">
+                <div class="w-full md:w-3/4 lg:w-1/2">
+                <h1 class="text-white font-bold text-4xl">Carreras, Clinicas y Entrenamientos</h1>
+                <p class="text-white text-lg mt-2 mb-4">Inscribete y reserva tu cupo ahora!!</p>
+                    <!-- component -->
+                    <!-- This is an example component 
+                    
                 
-              
-                -->
+                    -->
+                </div>
             </div>
-        </div>
 
-    </section>
-    
-    @livewire('eventos-index')
-
+        </section>
+        
+        @livewire('eventos-index')
+    </x-fast-view>
 
 
 </x-app-layout>

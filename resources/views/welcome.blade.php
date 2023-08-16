31a3c54ee08d9daa324396bd7050642b0d356b66
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-16 00:15:30 -0400

    u

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,12 @@
 <x-app-layout>
 
+    <x-slot name="tl">
+            
+        <title>Portal Riders Chilenos</title>
+        
+        
+    </x-slot>
+
     <x-fast-view :riders="$riders" :autos="$autos" :series="$series" :socio2="$socio2" :disciplinas="$disciplinas">
        
     </x-fast-view>

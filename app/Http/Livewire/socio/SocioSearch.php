686b045c3952d6e29f8ea65f154a3474cf56c2d8
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-16 01:25:54 -0400

    u

--- app/Http/Livewire/socio/SocioSearch.php
+++ app/Http/Livewire/socio/SocioSearch.php
@@ -27,7 +27,7 @@ class SocioSearch extends Component
                     ->orwhere('users.name','LIKE','%'. $this->search .'%')
                     ->orwhere('socios.slug','LIKE','%'. $this->search .'%')
                     ->orderby('users.profile_photo_path','DESC')
-                    ->orderby('socios.updated_at','DESC')
+                    ->orderby('users.updated_at','DESC')
                     ->paginate(50);
 
         

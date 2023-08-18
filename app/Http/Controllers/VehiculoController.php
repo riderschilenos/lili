1f1c2f1828059c78e3d27cb26ed8e421f07bdd62
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-18 12:34:38 -0400

    u

--- app/Http/Controllers/VehiculoController.php
+++ app/Http/Controllers/VehiculoController.php
@@ -378,6 +378,60 @@ class VehiculoController extends Controller
         return view('vehiculo.garage.show',compact('vehiculo','qr','socio2','disciplinas','riders','series','autos'));
     }
 
+    public function qrlink(Qrregister $qr){
+        $vehiculo=Vehiculo::where('slug',$qr->vehiculo->slug)->first();
+        if(Cache::has('autos')){
+             $autos = Cache::get('autos');
+         }else{
+             $autos = Vehiculo::where('status',4)
+                             ->orwhere('status',5)
+                             ->orwhere('status',7)
+                             ->latest('id')->get()->take(3);
+             Cache::put('autos',$autos);
+          }
+ 
+         if(Cache::has('series')){
+             $series = Cache::get('series');
+         }else{
+             $series = Serie::where('status',3)->where('content','serie')->latest('id')->get()->take(8);
+             Cache::put('series',$series);
+          }
+ 
+ 
+         if(Cache::has('riders')){
+             $riders = Cache::get('riders');
+         }else{
+            $riders = Socio::join('users','socios.user_id','=','users.id')
+             ->select('socios.*','users.name','users.email','users.updated_at')->where('status',1)
+             ->orwhere('status',2)
+             ->orderByRaw("CASE WHEN users.profile_photo_path IS NOT NULL THEN 0 ELSE 1 END, 
+             CASE WHEN socios.created_at >= CURDATE() THEN 0 ELSE 1 END, 
+             CASE WHEN socios.updated_at >= CURDATE() THEN 0 ELSE 1 END, 
+             id DESC")->get()->take(4);
+             Cache::put('riders',$riders);
+          }
+         
+         if(auth()->user())
+         {
+             if(auth()->user()->socio)
+             {
+                 $socio2 = Socio::where('user_id',auth()->user()->id)->first();
+             }else{
+                 $socio2=null;
+             }
+             
+         }
+         else{
+             $socio2=null;
+         }
+ 
+        $disciplinas= Disciplina::pluck('name','id');
+ 
+         $qr=Qrregister::where('vehiculo_slug', $vehiculo->slug)->first();
+ 
+         return view('vehiculo.garage.show',compact('vehiculo','qr','socio2','disciplinas','riders','series','autos'));
+     }
+
     public function store(Request $request)
     {   
         if($request->status==1){

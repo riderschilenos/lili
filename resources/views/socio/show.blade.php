9ac819cacb9b23e9ea220acfce7c6f304b91916d
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-18 00:16:56 -0400

    u

--- resources/views/socio/show.blade.php
+++ resources/views/socio/show.blade.php
@@ -319,7 +319,7 @@
                                                     @if($vehiculo->status==5 || $vehiculo->status==6)
                                                         <div class="text-center p-2 m-2 bg-main-color rounded-xl">
                                                             <a href="{{route('garage.vehiculo.show', $vehiculo)}}" class="text-main-color">
-                                                                <img class="h-44 w-42" src="{{Storage::url($vehiculo->image->first()->url)}}" alt="">
+                                                                <img class="h-32 w-30" src="{{Storage::url($vehiculo->image->first()->url)}}" alt="">
                                                                 <a href="{{route('garage.vehiculo.show', $vehiculo)}}">
                                                                     <h1 class="text-white mt-1 font-bold text-md">{{$vehiculo->marca->name}}<br>{{strtoupper($vehiculo->modelo).$vehiculo->cilindrada.' '.$vehiculo->año}}</h1>
                                                                 </a>

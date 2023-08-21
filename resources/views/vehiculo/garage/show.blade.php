4d23a29fd9a4cb083af6e461327d518cc98539da
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-20 20:24:49 -0400

    u

--- resources/views/vehiculo/garage/show.blade.php
+++ resources/views/vehiculo/garage/show.blade.php
@@ -172,8 +172,10 @@
                                     </div>
                                     @if($vehiculo->nro_serie)
                                         <div class="grid grid-cols-2">
-                                            <div class="px-4 py-2 font-semibold">Nro Serie:</div>
-                                            <div class="px-4 py-2">{{ $vehiculo->nro_serie}}</div>
+                                            <div class="px-4 py-2 font-semibold">Nro Serie / Nro Chasis:</div>
+                                            <div class="px-4 py-2" >
+                                               <h1 alt="{{ $vehiculo->nro_serie}}"> {{ $vehiculo->nro_serie}}</h1>
+                                            </div>
                                         </div>
                                     @endif
                                     

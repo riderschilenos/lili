fb1fbbf5ede43e69a58528a5312a0f4fd67b02ba
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-25 00:51:54 -0400

    u

--- app/Http/Controllers/StravaController.php
+++ app/Http/Controllers/StravaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AtletaStrava;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 
@@ -25,7 +26,17 @@ class StravaController extends Controller
         $data = json_decode($response->getBody(), true);
 
 
-        
+        $atletaId = $data['athlete']['id'];
+        $athleteName = $data['athlete']['username'];
+        $atletaStrava = new AtletaStrava([
+            'user_id' => auth()->user()->id, // Reemplaza con el ID del atleta
+            'atleta_id' => $atletaId, // Reemplaza con el ID del atleta
+            'access_token' => $data['access_token'],
+            'refresh_token' => $data['refresh_token'], // Si aplica
+            'token_expires_at' => now()->addSeconds($data['expires_in']), // Calcula la fecha de vencimiento
+            'scope' => $data['scope'],
+            'athlete_name' => $athleteName, // Reemplaza con el nombre del atleta
+        ]);
 
         // Aquí puedes guardar el token de acceso en la base de datos u otra acción
         // $data['access_token'] contiene el token de acceso que puedes utilizar para acceder a la API de Strava

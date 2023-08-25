6e8ae398593df0478a61536985a7df6389fce752
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-25 00:53:17 -0400

    u

--- app/Models/AtletaStrava.php
+++ app/Models/AtletaStrava.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class AtletaStrava extends Model
 {
     use HasFactory;
+    
+    protected $guarded = ['id'];
+
+     //relacion uno uno inversa
+
+     public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
 }

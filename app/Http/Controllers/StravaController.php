515a5f1f0d14efbb2c1e29efbee4c9ae695dad4d
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-25 00:56:25 -0400

    u

--- app/Http/Controllers/StravaController.php
+++ app/Http/Controllers/StravaController.php
@@ -34,7 +34,7 @@ class StravaController extends Controller
             'access_token' => $data['access_token'],
             'refresh_token' => $data['refresh_token'], // Si aplica
             'token_expires_at' => now()->addSeconds($data['expires_in']), // Calcula la fecha de vencimiento
-            'scope' => $data['scope'],
+            //'scope' => $data['scope'],
             'athlete_name' => $athleteName, // Reemplaza con el nombre del atleta
         ]);
 

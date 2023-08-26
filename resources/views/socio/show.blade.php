0a4f6528de13579694dbc3cd18e1f9e84af64b0c
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-26 16:53:51 -0400

    u

--- resources/views/socio/show.blade.php
+++ resources/views/socio/show.blade.php
@@ -447,7 +447,7 @@
                                             <div class="container mx-auto text-center">
                                                 <h1 class="text-white text-4xl font-semibold mb-4">¡Desafío Strava! 15 y 30Km Online!</h1>
                                                 <p class="text-white text-lg mb-8">Demuestra tu constancia montando a la bicicleta y participa de este desafio virtual.</p>
-                                                <a href="https://riderschilenos.cl/eventos/desaf-o-la-serena-473km-ft-strava" class="bg-white text-blue-500 hover:bg-blue-100 text-lg font-semibold py-2 px-6 rounded-full">Regístrate ahora</a>
+                                                <a href="https://riderschilenos.cl/eventos/desaf-o-riderschilenos-ft-strava" class="bg-white text-blue-500 hover:bg-blue-100 text-lg font-semibold py-2 px-6 rounded-full">Regístrate ahora</a>
                                             </div>
                                         </div>
                                         <ul class="list-inside space-y-2 ml-2 hidden">

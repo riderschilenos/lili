4493e0265571e8fb6d266f04234bc7a3deb3c3b9
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-27 02:04:33 -0400

    u

--- resources/views/payment/ticketview.blade.php
+++ resources/views/payment/ticketview.blade.php
@@ -35,8 +35,8 @@
                               <div class="bg-gray-100 p-4 rounded-lg shadow-lg text-center">
                                 <div class="text-4xl font-bold my-4" id="kilometers">1.58 Kms</div>
                                 <div class="text-2xl font-semibold mb-2">Recorridos con Strava</div>
-                    
-                                <div class="text-6xl font-bold mb-4" id="clock">Calculando...</div>
+                                <div id="clock" class="text-sm">Quedan {{ $hoursRemaining }} horas y {{ $minutesRemaining }} minutos</div>
+                                
                             </div>
                         @else
                           <a href="{{route('ticket.historial.view',$ticket->user)}}">

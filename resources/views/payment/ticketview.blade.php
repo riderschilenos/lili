f05c5b9e84c9ee35965765589d708e98d1b0e39b
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-27 02:01:35 -0400

    u

--- resources/views/payment/ticketview.blade.php
+++ resources/views/payment/ticketview.blade.php
@@ -35,7 +35,8 @@
                               <div class="bg-gray-100 p-4 rounded-lg shadow-lg text-center">
                                 <div class="text-4xl font-bold my-4" id="kilometers">1.58 Kms</div>
                                 <div class="text-2xl font-semibold mb-2">Recorridos con Strava</div>
-                                <div>Quedan {{ $hoursRemaining }} horas y {{ $minutesRemaining }} minutos</div>
+                    
+                                <div class="text-6xl font-bold mb-4" id="clock">Calculando...</div>
                             </div>
                         @else
                           <a href="{{route('ticket.historial.view',$ticket->user)}}">
@@ -192,6 +193,30 @@
   
     </div>
   </div>
-  
+  @php
+
+      $endTime = $ticket->updated_at->addHours(48);
+
+  @endphp
+  <script>
+    const clockDisplay = document.getElementById('clock');
+    
+    function updateClock() {
+      const endTime = new Date(<?php echo json_encode($endTime) ?>);
+      const currentTime =  new Date(); // Reemplaza con tu fecha de finalización
+
+        const timeRemaining = endTime.getTime() - currentTime.getTime();
+
+        const seconds = Math.floor((timeRemaining / 1000) % 60);
+        const minutes = Math.floor((timeRemaining / (1000 * 60)) % 60);
+        const hours = Math.floor(timeRemaining / (1000 * 60 * 60));
+
+        clockDisplay.innerText = `Quedan ${hours} horas, ${minutes} minutos y ${seconds} segundos`;
+    }
+
+    updateClock(); // Actualiza inicialmente
+
+    setInterval(updateClock, 1000); // Actualiza cada segundo
+</script>
   
 </x-app-layout > 

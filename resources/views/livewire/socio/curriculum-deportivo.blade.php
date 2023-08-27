18aac8db4d243d3ec7a76d48b7ca5d9a44df48fe
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-08-27 01:40:03 -0400

    u

--- resources/views/livewire/socio/curriculum-deportivo.blade.php
+++ resources/views/livewire/socio/curriculum-deportivo.blade.php
@@ -7,7 +7,7 @@
                 <div
                     class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                 </div>
-                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 ml-2">February 2022</time>
+                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 ml-2">Febrero 2023</time>
                 <div class="flex items-center">
                     <img class="h-16 object-contain mr-4 cursor-pointer items-center" src="{{asset('img/pdf_icon2.png')}}" title="Descargar" alt="">
                     <div class="ml-2">
@@ -32,7 +32,7 @@
                 <div
                     class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                 </div>
-                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 ml-2">February 2022</time>
+                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 ml-2">Febrero 2023</time>
                 <div class="flex items-center">
                     <img class="h-16 object-contain mr-4 cursor-pointer items-center" src="{{asset('img/pdf_icon2.png')}}" title="Descargar" alt="">
                     <div class="ml-2">
@@ -56,11 +56,11 @@
                 <div
                     class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                 </div>
-                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 ml-2">February 2022</time>
+                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 ml-2">Febrero 2023</time>
                 <div class="flex items-center">
                     <img class="h-16 object-contain mr-4 cursor-pointer items-center" src="{{asset('img/pdf_icon2.png')}}" title="Descargar" alt="">
                     <div class="ml-2">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Application UI code in Tailwind CSS</h3>
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Primer Lugar Motocross</h3>
                         <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages
                             including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce &amp; Marketing
                             pages.</p>
@@ -76,31 +76,7 @@
 
                 </div>
               
-            </li><li class=" ml-2">
-                <div
-                    class="absolute w-3 h-3 bg-gray-200 rounded-full -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
-                </div>
-                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 ml-2">February 2022</time>
-                <div class="flex items-center">
-                    <img class="h-16 object-contain mr-4 cursor-pointer items-center" src="{{asset('img/pdf_icon2.png')}}" title="Descargar" alt="">
-                    <div class="ml-2">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Application UI code in Tailwind CSS</h3>
-                        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages
-                            including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce &amp; Marketing
-                            pages.</p>
-                            <div class="hidden flex justify-end"> 
-                                <a href="#"
-                                class=" inline-flex justify-end ml-auto items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                               <p class="whitespace-nowrap "> Ver más </p>
-                                
-                               
-                            </a>
-                            </div>
-                    </div>
-
-                </div>
-              
-            </li>
+           
           
            
         </ol>

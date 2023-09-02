b0a546de5cccb4f2f8ab8917ed6a941f964b21bc
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-09-02 16:03:24 -0400

    u

--- resources/views/vehiculo/garage/show.blade.php
+++ resources/views/vehiculo/garage/show.blade.php
@@ -238,10 +238,10 @@
                                 <div class="flex items-center">
 
                                     @if (str_contains($vehiculo->user->profile_photo_url,'https://ui-'))
-                                        <img class="flex h-14 w-14 rounded-full shadow-lg object-cover" src="https://static.vecteezy.com/system/resources/previews/021/155/831/original/motocross-helmet-mascot-logo-racer-rider-cyclist-sport-concept-suitable-for-print-web-avatar-profile-and-more-vector.jpg" alt="">
+                                        <img class="flex h-16 w-16 rounded-full shadow-lg object-cover" src="https://static.vecteezy.com/system/resources/previews/021/155/831/original/motocross-helmet-mascot-logo-racer-rider-cyclist-sport-concept-suitable-for-print-web-avatar-profile-and-more-vector.jpg" alt="">
                                         
                                     @else
-                                        <img class="flex h-14 w-14 rounded-full shadow-lg object-cover" src="{{ $vehiculo->user->profile_photo_url }}" alt=""  />
+                                        <img class="flex h-16 w-16 rounded-full shadow-lg object-cover" src="{{ $vehiculo->user->profile_photo_url }}" alt=""  />
                                     
                                     @endif
                                       

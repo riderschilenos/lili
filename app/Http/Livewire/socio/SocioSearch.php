fbb72aa3ded0906f2a4d0ef333cf89fd829e43a6
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-09-02 14:39:12 -0400

    u

--- app/Http/Livewire/socio/SocioSearch.php
+++ app/Http/Livewire/socio/SocioSearch.php
@@ -30,7 +30,7 @@ class SocioSearch extends Component
                     CASE WHEN socios.created_at >= CURDATE() THEN 0 ELSE 1 END, 
                     CASE WHEN socios.updated_at >= CURDATE() THEN 0 ELSE 1 END, 
                     id DESC")
-                    ->paginate(50);
+                    ->paginate(16);
 
         
         return view('livewire.socio.socio-search',compact('socios','disciplinas','sociosfull'));

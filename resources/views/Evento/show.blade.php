59e49bda12a7bb1a4bfb3cb7990f9d97761500ca
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-09-19 23:20:43 -0300

    u

--- resources/views/Evento/show.blade.php
+++ resources/views/Evento/show.blade.php
@@ -179,7 +179,17 @@
                                                     <td class="px-6 py-4 whitespace-nowrap">
                                                         <div class="flex items-center">
                                                             <div class="flex-shrink-0 h-10 w-10">
-                                                                <a href="{{route('socio.show', $item->user->socio)}}">
+                                                                @if ($item->user->socio)
+                                                                    <a href="{{route('socio.show', $item->user->socio)}}">
+                                                                        @if (str_contains($item->user->profile_photo_url,'https://ui-'))
+                                                                            <img class="h-11 w-11 object-cover object-center rounded-full" src="https://static.vecteezy.com/system/resources/previews/021/155/831/original/motocross-helmet-mascot-logo-racer-rider-cyclist-sport-concept-suitable-for-print-web-avatar-profile-and-more-vector.jpg" alt="{{ $item->user->name }}"  >
+                                                                        
+                                                                        @else
+                                                                            <img class="h-11 w-11 object-cover object-center rounded-full" src="{{$item->user->profile_photo_url}}" alt="">
+                                                                        
+                                                                        @endif
+                                                                    </a>
+                                                                @else
                                                                     @if (str_contains($item->user->profile_photo_url,'https://ui-'))
                                                                         <img class="h-11 w-11 object-cover object-center rounded-full" src="https://static.vecteezy.com/system/resources/previews/021/155/831/original/motocross-helmet-mascot-logo-racer-rider-cyclist-sport-concept-suitable-for-print-web-avatar-profile-and-more-vector.jpg" alt="{{ $item->user->name }}"  >
                                                                     
@@ -187,15 +197,21 @@
                                                                         <img class="h-11 w-11 object-cover object-center rounded-full" src="{{$item->user->profile_photo_url}}" alt="">
                                                                     
                                                                     @endif
-                                                                </a>
+                                                                @endif
+                                                              
                                                                 
                                                             </div>
                                                             <div class="ml-4">
                                                                 <div class="text-sm font-medium text-gray-900">
-                                                                    <a href="{{route('socio.show', $item->user->socio)}}">
-                                                               
-                                                                        {{ Str::limit($item->user->name, 18) }}
-                                                                    </a>
+                                                                    @if ()
+                                                                        <a href="{{route('socio.show', $item->user->socio)}}">
+                                                                
+                                                                            {{ Str::limit($item->user->name, 18) }}
+                                                                        </a>
+                                                                    @else
+                                                                            {{ Str::limit($item->user->name, 18) }}
+                                                                    @endif
+                                                                   
                                                                 </div>
                                                                 
                                                             </div>
@@ -213,6 +229,7 @@
                                                                         @foreach ($item->inscripcions as $inscripcion)
                                                                                         
                                                                                               <div class="flex justify-center">
+                                                                                                @if ($item->user->socio)
                                                                                                     <a href="{{route('socio.show', $item->user->socio)}}">
                                                                                                         <div class="px-2 py-4 whitespace-nowrap">
                                                                                                     
@@ -220,6 +237,15 @@
                                                                                                         
                                                                                                         </div>
                                                                                                     </a>
+                                                                                                @else
+                                                                                                    <div class="px-2 py-4 whitespace-nowrap">
+                                                                                                        
+                                                                                                        {{-- comment   {{$fecha->name}} --}} {{$inscripcion->fecha_categoria->categoria->name}}
+                                                                                                        
+                                                                                                        </div>
+                                                                                                    
+                                                                                                @endif
+                                                                                                 
                                                                                                 
                                                                                                     
                                                                                                 </div> 

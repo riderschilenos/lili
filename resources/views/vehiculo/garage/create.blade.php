c597694d2b6e873e66633928486ac4b88c826e0c
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-02 21:36:58 -0300

    u

--- resources/views/vehiculo/garage/create.blade.php
+++ resources/views/vehiculo/garage/create.blade.php
@@ -14,10 +14,10 @@
                     <hr class="mt-2 mb-6">
                     <div class="bg-gray-100 border-t-4 mb-6 mx-3 border-gray-500 rounded-b text-gray-900 px-4 py-3 shadow-md" role="alert">
                   
-                        <p class="text-base ">
+                        <p class="text-base text-center">
                             Al registrar tu vehículo con nosotros, garantizamos que una vez registrado cualquiera que busque el número de chasis en Google podrá identificar rápidamente quién es el propietario legítimo de la moto o bicicleta. El proceso de indexación en google puede tardar hasta 96 horas.
                         </p>
-                  <div>
+                     <div>
                     
                     @livewire('vehiculo.vehiculo-inscripcion')
 

193cb6bf38509613265d9407716f147ff8b92a10
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-10 13:06:25 -0300

    u

--- app/Models/Activitie.php
+++ app/Models/Activitie.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Activitie extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+     //relacion uno uno inversa
+
+     public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
 }

463d86b6162374bd5cecbcd54856beb296e67094
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-10 15:06:55 -0300

    1.58

--- resources/views/payment/ticketview.blade.php
+++ resources/views/payment/ticketview.blade.php
@@ -43,9 +43,10 @@
                         @if ($evento->type=='desafio')
                               <div class="bg-gray-100 p-4 rounded-lg shadow-lg text-center">
                                 
-                                <div class="text-4xl font-bold my-4" id="kilometers">1.58 Kms</div>
+                                <div class="text-4xl font-bold my-4" id="kilometers">0.00 Kms</div>
                                 <div class="text-2xl font-semibold mb-2">Recorridos con Strava</div>
-                                <div id="clock" class="text-sm">Quedan {{ $hoursRemaining }} horas y {{ $minutesRemaining }} minutos</div>
+
+                                <div id="clock" class="text-sm hidden">Quedan {{ $hoursRemaining }} horas y {{ $minutesRemaining }} minutos</div>
                                 
                             </div>
                         @else

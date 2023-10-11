a35b77f07c400c3dd718b51e65cd5a5e6a61b726
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-11 00:59:35 -0300

    u

--- resources/views/livewire/admin/strava-count.blade.php
+++ resources/views/livewire/admin/strava-count.blade.php
@@ -1,10 +1,13 @@
 <div>
-   
+   @php
+       $hoursRemaining=0;
+       $minutesRemaining=0;
+   @endphp
     <div class="bg-gray-100 p-4 rounded-lg shadow-lg text-center">
                                 
         <div class="text-4xl font-bold my-4" id="kilometers">0.00 Kms</div>
         <div class="text-2xl font-semibold mb-2">Recorridos con Strava</div>
-
+        
         <div id="clock" class="text-sm hidden">Quedan {{ $hoursRemaining }} horas y {{ $minutesRemaining }} minutos</div>
         
     </div>

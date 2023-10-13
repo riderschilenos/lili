2b4d1d43af281446f4b9d4fbcaabe505d61f1b89
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-13 17:44:06 -0300

    u

--- resources/views/components/fast-view.blade.php
+++ resources/views/components/fast-view.blade.php
@@ -818,7 +818,7 @@
                         @else
                 
                             <div class="max-w-7xl mx-auto px-4 sm:px-8 lg:px-8 my-4 @routeIs('socio.create') hidden @endif">
-                                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded">
                                     <p class="font-bold text-center">¡Bienvenido!</p>
                                     <span class="block sm:inline">Ahora puedes crear el perfil de Rider que te servira para registrar tu moto o bicicleta, registrar tus logros deportivos, contratar cursos o clases, entre otras cosas.</span>
                                     <a href="{{route('socio.create')}}">

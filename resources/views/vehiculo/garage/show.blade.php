fe9a17c5f64892a2df20e3fb03d9c5410339fcf2
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-14 14:51:28 -0300

    u

--- resources/views/vehiculo/garage/show.blade.php
+++ resources/views/vehiculo/garage/show.blade.php
@@ -17,7 +17,7 @@
 </style>
 
 
-            <section class="bg-gray-700 pb-6 pt-2 mb-8 ">
+            <section class="bg-gray-700 pb-6 mb-8 ">
                 <figure class="block sm:hidden pt-6 pb-4">
     
                     @if($vehiculo->image->first())

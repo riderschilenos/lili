3ba3f2486ce2276eb0e6d832f01fc3002fbf59cf
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-18 17:57:00 -0300

    u

--- resources/views/components/fast-view.blade.php
+++ resources/views/components/fast-view.blade.php
@@ -1628,7 +1628,18 @@
                        
                 @else
                     
+                <div class="max-w-7xl mx-auto pt-2 pb-8">
+        
+                    <div class="bg-white shadow-lg rounded overflow-hidden pb-8 ">
+                  
+                            
+                          @livewire('vendedor.public-show')
+                   
+                    </div>
+               
+                </div>
 
+              
                         <div class="max-w-7xl mx-auto px-2 pt-2 pb-8">
                     
                             <div class="card pb-8 ">
@@ -1642,58 +1653,6 @@
                                         </div>
                     
                                
-                             
-                                    <div class="max-w-7xl sm:px-6 mx-2 lg:px-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-x-6 gap-y-8 mt-8 lg:mx-14">
-                                        <article class="hidden  md:block col-span-2 md:col-span-1">
-                                            <figure>
-                                                <img class="h-35 w-55 object-cover" src="{{asset('img/vendedores/second3.png')}}" alt="">
-                                            </figure>
-                                
-                                        
-                                        </article>
-                                        <div class="block  md:hidden col-span-2 md:col-span-1">
-                                            <article class="flex justify-center mt-2">
-                                                <figure>
-                                                    <img class="h-48 object-contain" src="{{asset('img/vendedores/second3.png')}}" alt="">
-                                                </figure>
-                                            </article>
-                                        </div>
-                                        <article class="col-span-2 sm:col-span-2">
-                                            <figure>
-                                                <img class="h-35 w-55 object-cover" src="{{asset('img/vendedores/tree3.png')}}" alt="">
-                                            </figure>
-                                        
-                                        </article>
-                                    
-                                    
-                                    </div>
-                                
-                           
-                                
-                                
-                                    <div class="justify-between mt-8 bg-gray-200">
-                    
-                                        <h1 class="text-2xl py-4 text-center font-bold">Vendedor Destacado</h1>
-                                        <div class="max-w-7xl px-4 sm:px-6 mx-2 lg:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-x-2 gap-y-2 lg:mx-14 pb-10">
-                                    
-                                            <article>
-                                                <figure>
-                                                    <a href="https://www.instagram.com/reel/CVyzsrhpZE3/?utm_source=ig_web_copy_link" target="_blank">
-                                                        <img class="h-35 w-55 object-cover" src="{{asset('img/vendedores/felipe2.png')}}" alt="">
-                                                    </a>
-                                                </figure>
-                                            
-                                            </article>
-                                            <div>
-                                                <h1 class="text-2xl py-4 text-center mt-6 md:ml-10">"...El poder ganar dinero desde su smartphone, de manera rapida y accesible es algo que todos pueden hacer..."</h1>
-                                                <h1 class="text-xl pb-4 pt-6 ml-4">Felipe Caerols<br>Santiago</h1>
-                                    
-                                            </div>
-                    
-                    
-                                        </div>
-                                
-                                    </div>
                                 
                                     <h1 class="text-3xl font-bold text-center my-8">Formulario de Inscripción</h1>
                             
@@ -1989,9 +1948,9 @@
             --}}
         
         
-            <div class="max-w-7xl mx-auto px-2 pt-2 pb-8">
+            <div class="max-w-7xl mx-auto pt-2 pb-8">
         
-                <div class="card pb-8 ">
+                <div class="bg-white shadow-lg rounded overflow-hidden pb-8 ">
               
                         
                       @livewire('vendedor.public-show')

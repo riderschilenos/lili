86067bc23afb6f78ae7f1677d96819bbd33eee7e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-18 21:07:37 -0300

    u

--- app/Http/Livewire/socio/SocioSearch.php
+++ app/Http/Livewire/socio/SocioSearch.php
@@ -6,8 +6,6 @@ use App\Models\Disciplina;
 use App\Models\Socio;
 use Livewire\Component;
 use Livewire\WithPagination;
-use Illuminate\Support\Facades\Cache;
-
 
 class SocioSearch extends Component
 {   
@@ -28,28 +26,22 @@ class SocioSearch extends Component
         $disciplinas = Disciplina::all();
 
         $sociosfull = Socio::all();
-            
-        // Nombre único para la clave de caché
-        $cacheKey = 'socios_query_' . md5(serialize($this->search));
 
-        $socios = Cache::remember($cacheKey,30, function () {
-            return Socio::join('users', 'socios.user_id', '=', 'users.id')
-                ->select('socios.*', 'users.name', 'users.email', 'users.updated_at')
-                ->where(function($query) {
-                    $search = $this->search;
-                    $query->where('rut', 'LIKE', '%' . $search . '%')
-                        ->orWhere('email', 'LIKE', '%' . $search . '%')
-                        ->orWhere('socios.name', 'LIKE', '%' . $search . '%')
-                        ->orWhere('users.name', 'LIKE', '%' . $search . '%')
-                        ->orWhere('socios.slug', 'LIKE', '%' . $search . '%');
-                })
-                ->orderByRaw("CASE WHEN users.profile_photo_path IS NOT NULL THEN 0 ELSE 1 END, 
-                CASE WHEN socios.created_at >= CURDATE() THEN 0 ELSE 1 END, 
-                CASE WHEN socios.updated_at >= CURDATE() THEN 0 ELSE 1 END, 
-                id DESC")
-                ->paginate($this->perPagesoc);
-        });
-            
+        $socios = Socio::join('users', 'socios.user_id', '=', 'users.id')
+                            ->select('socios.*', 'users.name', 'users.email', 'users.updated_at')
+                            ->where(function($query) {
+                                $search = $this->search;
+                                $query->where('rut', 'LIKE', '%' . $search . '%')
+                                    ->orWhere('email', 'LIKE', '%' . $search . '%')
+                                    ->orWhere('socios.name', 'LIKE', '%' . $search . '%')
+                                    ->orWhere('users.name', 'LIKE', '%' . $search . '%')
+                                    ->orWhere('socios.slug', 'LIKE', '%' . $search . '%');
+                            })
+                            ->orderByRaw("CASE WHEN users.profile_photo_path IS NOT NULL THEN 0 ELSE 1 END, 
+                            CASE WHEN socios.created_at >= CURDATE() THEN 0 ELSE 1 END, 
+                            CASE WHEN socios.updated_at >= CURDATE() THEN 0 ELSE 1 END, 
+                            id DESC")
+                            ->paginate($this->perPagesoc);
 
         
         return view('livewire.socio.socio-search',compact('socios','disciplinas','sociosfull'));

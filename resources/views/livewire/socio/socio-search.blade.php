f81defae6a7760235cb179af3b9337c2e47e5b38
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-18 22:30:25 -0300

    u

--- resources/views/livewire/socio/socio-search.blade.php
+++ resources/views/livewire/socio/socio-search.blade.php
@@ -25,7 +25,7 @@
     <div x-data="setup()">
                      
        
-            <div class="mt-4 grid grid-cols-1 lg:grid-cols-3 mx-2">
+            <div class="mt-4 grid grid-cols-1 lg:grid-cols-3">
                 
                 <div>
             

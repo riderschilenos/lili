b411f4b1bdaee096da7f5bf5481c281ecc1ee794
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-18 21:55:18 -0300

    u

--- resources/views/livewire/socio/socio-search.blade.php
+++ resources/views/livewire/socio/socio-search.blade.php
@@ -96,7 +96,7 @@
            
             </div>
    
-        <div class="px-6 pb-2">
+        <div class="px-6 py-2">
             <input wire:keydown="limpiar_page" wire:model="search"  class="form-input flex-1 w-full shadow-sm  border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg focus:outline-none" placeholder="Ingrese el nombre de un rider" autocomplete="off">
         </div>
 

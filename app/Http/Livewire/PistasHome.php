1301d0385736f537bffb90d219895e0b832fe67e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-19 23:12:01 -0300

    u

--- app/Http/Livewire/PistasHome.php
+++ app/Http/Livewire/PistasHome.php
@@ -11,6 +11,7 @@ class PistasHome extends Component
     {   $pistas=Evento::where('status',1)
         ->where('type','pista')
         ->orwhere('type','desafio')
+        ->orwhere('type','carrera')
         ->latest('id')
         ->paginate(4);
         return view('livewire.pistas-home',compact('pistas'));

3f7c7005777365061550e37447aaca0cec6a284e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-23 13:15:44 -0300

    u

--- resources/views/vehiculo/index.blade.php
+++ resources/views/vehiculo/index.blade.php
@@ -8,7 +8,16 @@
     </x-slot>
 
     <x-fast-view :riders="$riders" :autos="$autos" :series="$series" :socio2="$socio2" :disciplinas="$disciplinas">
-        <h1 class="text-xs font-bold text-center my-1">Registro Nacional de Motos y Bicicletas</h1>
+        <div class="words sm:hidden bg-white pt-2 overflow-x-auto whitespace-no-wrap border-b-2 font-bold text-gray-700">
+            <div class="flex justify-center">
+              <div class="px-4 py-2 cursor-pointer hover:underline" @click="user = false; home = false; socio = true; evento = false; registro = false; vendedor = false; base = false" >Riders</div>
+              <div class="px-4 py-2 cursor-pointer hover:underline" @click="evento = true; user = false; home = false; socio = false; registro = false; vendedor = false; base = false" >Eventos</div>
+              <div class="px-4 py-2 cursor-pointer underline text-gray-900" @click="user = false; home = false; socio = false; evento = false; registro = true; vendedor = false; base = false" >Bikes</div>
+              <div class="px-4 py-2 cursor-pointer hover:underline" @click="user = false; novedades = true; home = false; socio = false; evento = false; registro = false; vendedor = false; base = false" >Novedades</div>
+               
+              <!-- Agrega más categorías aquí -->
+            </div>
+          </div>
         <div class="max-w-7xl mx-auto pb-8 ">      
             <div class="">
                 <div class="bg-white">

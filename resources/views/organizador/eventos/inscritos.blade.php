eec4f7f38ae6e739c6b17d4ce774f96df21e2cd9
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-10-25 13:24:18 -0300

    u

--- resources/views/organizador/eventos/inscritos.blade.php
+++ resources/views/organizador/eventos/inscritos.blade.php
@@ -6,10 +6,10 @@
     <h1 class="text-2xl font-bold"> 
             Listado de Inscritos: {{$evento->titulo}}
     </h1>
-    <hr class="mt-2 mb-6">
-
+    <hr class="mt-2 mb-6 ">
+<div class="pb-6 mb-6">
    @livewire('organizador.evento-inscritos', ['evento' => $evento], key('evento.'.$evento->id))
-    
+</div>
 
 </x-ticket-layout>
 

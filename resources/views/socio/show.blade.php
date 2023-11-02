0808ecd6c7bb6b0e9412725ab441e11e04271623
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-11-02 00:10:32 -0300

    u

--- resources/views/socio/show.blade.php
+++ resources/views/socio/show.blade.php
@@ -24,7 +24,7 @@
                     
         <div x-data="{fullview: false}" >            
             <div x-show="fullview" x-on:click="fullview=false" class="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-white">
-                <div class="flex-shrink-0 flex items-center" x-on:click="fullview=false">
+                <div class="flex items-center" x-on:click="fullview=false">
                     @if (str_contains($socio->user->profile_photo_url,'https://ui-'))
                         <img class="w-full h-64 object-contain"
                         src="https://static.vecteezy.com/system/resources/previews/021/155/831/original/motocross-helmet-mascot-logo-racer-rider-cyclist-sport-concept-suitable-for-print-web-avatar-profile-and-more-vector.jpg"

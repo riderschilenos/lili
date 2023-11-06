c19ee5ae8c2d0a407827ae59b18c69c0e3fa4ad0
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-11-06 15:45:11 -0300

    u

--- resources/views/livewire/admin/pedidos-diseno.blade.php
+++ resources/views/livewire/admin/pedidos-diseno.blade.php
@@ -62,89 +62,89 @@
                                                 
                                                 <div class="ml-4">
                                                         <a target="_blank" href="{{route('vendedor.pedidos.edit',$pedido)}}">
-                                                        <div class="text-sm font-medium text-gray-900 whitespace-nowrap">
-                                                        
-                                                            @if($pedido->pedidoable_type=='App\Models\Socio')
-                                                                @foreach ($socios as $socio)
-                                                                        
-                                                                        @if($socio->id == $pedido->pedidoable_id)
-                                                                        
-                                                                            {{Str::limit($socio->user->name, 12)}}
-                                                                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                                                Socio
-                                                                            </span>
-                                                                        @endif
-                                                                @endforeach
-                                                            @endif
-                                                            @if($pedido->pedidoable_type=='App\Models\Invitado')
-                                                                @foreach ($invitados as $invitado)
-                                                                        
-                                                                        @if($invitado->id == $pedido->pedidoable_id)
-                                                                        
-                                                                            {{Str::limit($invitado->name, 12)}}
-                                                                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
-                                                                                Invitado
-                                                                            </span>
-                                                                        @endif
-                                                                @endforeach
-                                                            @endif
-                
-                
-                                                        </div>
-                                                        <div class="text-sm text-gray-500">
-                
-                                                            @if($pedido->pedidoable_type=='App\Models\Socio')
-                                                                
-                                                                @foreach ($socios as $socio)
-                                                                    @if(!is_null($socio->direccion))
-                                                                        @if($socio->id == $pedido->pedidoable_id)
-                                                                            {{$socio->direccion->comuna.", ".$socio->direccion->region}} 
-                                                                        @endif
-                                                                    @endif
-                                                                @endforeach
-                                                            @endif
-                
-                                                            @if($pedido->pedidoable_type=='App\Models\Invitado')
-                                                                @foreach ($invitados as $invitado)
-                                                                    
-                                                                        @if($invitado->id == $pedido->pedidoable_id)
-                                                                        
-                                                                            @if(!is_null($invitado->direccion))
-                                                                                {{$invitado->direccion->comuna.", ".$invitado->direccion->region}}
-                                                                            @else
-                                                                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
-                                                                                    FALTA DIRECCIÓN DE DESPACHO
+                                                            <div class="text-sm font-medium text-gray-900 whitespace-nowrap">
+                                                            
+                                                                @if($pedido->pedidoable_type=='App\Models\Socio')
+                                                                    @foreach ($socios as $socio)
+                                                                            
+                                                                            @if($socio->id == $pedido->pedidoable_id)
+                                                                            
+                                                                                {{Str::limit($socio->user->name, 12)}}
+                                                                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                                                                    Socio
                                                                                 </span>
                                                                             @endif
-                                                                        
+                                                                    @endforeach
+                                                                @endif
+                                                                @if($pedido->pedidoable_type=='App\Models\Invitado')
+                                                                    @foreach ($invitados as $invitado)
+                                                                            
+                                                                            @if($invitado->id == $pedido->pedidoable_id)
+                                                                            
+                                                                                {{Str::limit($invitado->name, 12)}}
+                                                                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+                                                                                    Invitado
+                                                                                </span>
+                                                                            @endif
+                                                                    @endforeach
+                                                                @endif
+                    
+                    
+                                                            </div>
+                                                            <div class="text-sm text-gray-500">
+                    
+                                                                @if($pedido->pedidoable_type=='App\Models\Socio')
+                                                                    
+                                                                    @foreach ($socios as $socio)
+                                                                        @if(!is_null($socio->direccion))
+                                                                            @if($socio->id == $pedido->pedidoable_id)
+                                                                                {{$socio->direccion->comuna.", ".$socio->direccion->region}} 
+                                                                            @endif
                                                                         @endif
+                                                                    @endforeach
+                                                                @endif
+                    
+                                                                @if($pedido->pedidoable_type=='App\Models\Invitado')
+                                                                    @foreach ($invitados as $invitado)
+                                                                        
+                                                                            @if($invitado->id == $pedido->pedidoable_id)
+                                                                            
+                                                                                @if(!is_null($invitado->direccion))
+                                                                                    {{$invitado->direccion->comuna.", ".$invitado->direccion->region}}
+                                                                                @else
+                                                                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                                                                                        FALTA DIRECCIÓN DE DESPACHO
+                                                                                    </span>
+                                                                                @endif
+                                                                            
+                                                                            @endif
+                                                                        
+                                                                    @endforeach
+                                                                @endif
+                                                                <br>
+                                                                @switch($pedido->transportista->id)
+                                                                    @case(1)
+                                                                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                                                            {{$pedido->transportista->name}}
+                                                                        </span>
+                                                                        @break
+                                                                    @case(2)
+                                                                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+                                                                            {{$pedido->transportista->name}}
+                                                                        </span>
+                                                                        @break
+                                                                        @case(3)
+                                                                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800">
+                                                                            {{$pedido->transportista->name}}
+                                                                        </span>
+                                                                        @break
                                                                     
-                                                                @endforeach
-                                                            @endif
-                                                            <br>
-                                                            @switch($pedido->transportista->id)
-                                                                @case(1)
-                                                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                                        {{$pedido->transportista->name}}
-                                                                    </span>
-                                                                    @break
-                                                                @case(2)
-                                                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
-                                                                        {{$pedido->transportista->name}}
-                                                                    </span>
-                                                                    @break
-                                                                    @case(3)
-                                                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-100 text-blue-800">
-                                                                        {{$pedido->transportista->name}}
-                                                                    </span>
-                                                                    @break
-                                                                
-                                                                @default
-                                                                    
-                                                            @endswitch
-                
-                                                        </div>
-                                                    </a>
+                                                                    @default
+                                                                        
+                                                                @endswitch
+                    
+                                                            </div>
+                                                        </a>
                                                 </div>
                                             
                                             </div>

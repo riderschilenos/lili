b8b7c4a0e09cd3fa0801a51888c55dc4a0cf544b
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-11-06 17:33:01 -0300

    u

--- app/Http/Livewire/vendedor/PedidosCreate.php
+++ app/Http/Livewire/vendedor/PedidosCreate.php
@@ -22,73 +22,81 @@ class PedidosCreate extends Component
     public $textoPortapapeles = '';
 
     use WithPagination;
-   
+
+    protected $listeners = ['actualizarTextoPortapapeles'];
+
+    public function actualizarTextoPortapapeles($text)
+    {
+        $this->textoPortapapeles = $text;
+    }
 
     public function completarDesdePortapapeles()
-{
-    // Expresiones regulares para extraer información del primer conjunto de datos
-    $patternNombres = '/NOMBRES: ([A-Z][A-ZA-ZA-Za-zÉéÍíÑñÓóÚúÁáÜü-]+(?: [A-Z][A-ZA-ZA-Za-zÉéÍíÑñÓóÚúÁáÜü-]+)?)\\s/';
-    $patternApellidos = '/APELLIDOS: ([A-Z][A-ZA-ZA-Za-zÉéÍíÑñÓóÚúÁáÜü-]+(?: [A-Z][A-ZA-ZA-Za-zÉéÍíÑñÓóÚúÁáÜü-]+)?)\\s/';
-    $patternRut = '/RUT: (\d{1,2}\.\d{3}\.\d{3}-[\dKk]|\d{7,8}-[\dKk])/'; // Modificamos la expresión regular del RUT
-    $patternTelefono = '/FONO: (\+569\d{8}|569\d{8}|\d{9}|\d{1} \d{8})/';
-    $patternEmail = '/MAIL: ([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})/';
+    {
+        // Expresiones regulares para extraer información del primer conjunto de datos
+        $patternNombres = '/NOMBRES: ([A-Z][A-ZA-ZA-Za-zÉéÍíÑñÓóÚúÁáÜü-]+(?: [A-Z][A-ZA-ZA-Za-zÉéÍíÑñÓóÚúÁáÜü-]+)?)\\s/';
+        $patternApellidos = '/APELLIDOS: ([A-Z][A-ZA-ZA-Za-zÉéÍíÑñÓóÚúÁáÜü-]+(?: [A-Z][A-ZA-ZA-Za-zÉéÍíÑñÓóÚúÁáÜü-]+)?)\\s/';
+        $patternRut = '/RUT: (\d{1,2}\.\d{3}\.\d{3}-[\dKk]|\d{7,8}-[\dKk])/'; // Modificamos la expresión regular del RUT
+        $patternTelefono = '/FONO: (\+569\d{8}|569\d{8}|\d{9}|\d{1} \d{8})/';
+        $patternEmail = '/MAIL: ([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})/';
 
-    preg_match($patternNombres, $this->textoPortapapeles, $matchesNombres);
-    preg_match($patternApellidos, $this->textoPortapapeles, $matchesApellidos);
-    preg_match($patternRut, $this->textoPortapapeles, $matchesRut);
-    preg_match($patternTelefono, $this->textoPortapapeles, $matchesTelefono);
-    preg_match($patternEmail, $this->textoPortapapeles, $matchesEmail);
+        preg_match($patternNombres, $this->textoPortapapeles, $matchesNombres);
+        preg_match($patternApellidos, $this->textoPortapapeles, $matchesApellidos);
+        preg_match($patternRut, $this->textoPortapapeles, $matchesRut);
+        preg_match($patternTelefono, $this->textoPortapapeles, $matchesTelefono);
+        preg_match($patternEmail, $this->textoPortapapeles, $matchesEmail);
 
-    $this->nombre = $matchesNombres[1]?? '';
-    $this->apellidos = $matchesApellidos[1] ?? '';
-    $this->rut = $matchesRut[1] ?? '';
-    $this->telefono = $matchesTelefono[1] ?? '';
-    $this->email = $matchesEmail[1] ?? '';
+        $this->nombre = $matchesNombres[1]?? '';
+        $this->apellidos = $matchesApellidos[1] ?? '';
+        $this->rut = $matchesRut[1] ?? '';
+        $this->telefono = $matchesTelefono[1] ?? '';
+        $this->email = $matchesEmail[1] ?? '';
 
-    
-        // Expresiones regulares para extraer información del segundo conjunto de datos
-        $patternNombre2 = '/([A-Z][a-z]+ [A-Z][a-z]+)/';
-        $patternApellidos2 = '/\n([A-Z][a-z]+ [A-Z][a-z ]+)\n/'; // Expresión regular para capturar apellidos de dos palabras
-        $patternRut2 = '/\b(\d{1,2}\.\d{3}\.\d{3}-[\dKk])\b/';
-        $patternTelefono2 = '/\b(\+?569\d{8}|9\d{8}|\d{8})\b/';   // Expresión regular para números de teléfono en ambas estructuras
-        $patternEmail2 = '/\b([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})\b/';
         
-        $matchesNombre2 = [];
-        $matchesApellidos2 = [];
-        $matchesTelefono2 = [];
-        
-        if (preg_match($patternNombre2, $this->textoPortapapeles, $matchesNombre2)) {
-            $nombreTemporal = $matchesNombre2[0];
-        } else {
-            $nombreTemporal = '';
-        }
-        
-        if (preg_match($patternApellidos2, $this->textoPortapapeles, $matchesApellidos2)) {
-            $apellidosTemporal = $matchesApellidos2[1];
-        } else {
-            $apellidosTemporal = '';
-        }
-        
-        if (preg_match($patternTelefono2, $this->textoPortapapeles, $matchesTelefono2)) {
-            $telefonoTemporal = $matchesTelefono2[0];
-        } else {
-            $telefonoTemporal = '';
-        }
+            // Expresiones regulares para extraer información del segundo conjunto de datos
+            $patternNombre2 = '/([A-Z][a-z]+ [A-Z][a-z]+)/';
+            $patternApellidos2 = '/\n([A-Z][a-z]+ [A-Z][a-z ]+)\n/'; // Expresión regular para capturar apellidos de dos palabras
+            $patternRut2 = '/\b(\d{1,2}\.\d{3}\.\d{3}-[\dKk]|\d{7,8}-[\dKk])\b/'; // Modificamos la expresión regular del RUT
+            $patternTelefono2 = '/\b(\+?569\d{8}|9\d{8}|\d{8})\b/';   // Expresión regular para números de teléfono en ambas estructuras
+            $patternEmail2 = '/\b([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})\b/';
+            
+            $matchesNombre2 = [];
+            $matchesApellidos2 = [];
+            $matchesTelefono2 = [];
+            
+            if (preg_match($patternNombre2, $this->textoPortapapeles, $matchesNombre2)) {
+                $nombreTemporal = $matchesNombre2[0];
+            } else {
+                $nombreTemporal = '';
+            }
+            
+            if (preg_match($patternApellidos2, $this->textoPortapapeles, $matchesApellidos2)) {
+                $apellidosTemporal = $matchesApellidos2[1];
+            } else {
+                $apellidosTemporal = '';
+            }
+            
+            if (preg_match($patternTelefono2, $this->textoPortapapeles, $matchesTelefono2)) {
+                $telefonoTemporal = $matchesTelefono2[0];
+            } else {
+                $telefonoTemporal = '';
+            }
 
-        $this->apellidos = empty($this->apellidos) ? $apellidosTemporal : $this->apellidos;
-        $this->nombre = empty($this->nombre) ? $nombreTemporal.' '.$this->apellidos : $this->nombre.' '.$this->apellidos;
-        $this->telefono = empty($this->telefono) ? $telefonoTemporal : $this->telefono;
-        
-        if (preg_match($patternRut2, $this->textoPortapapeles, $matchesRut2)) {
-            $this->rut = $matchesRut2[0];
-        }
-        
-        if (preg_match($patternEmail2, $this->textoPortapapeles, $matchesEmail2)) {
-            $this->email = $matchesEmail2[0];
-        }
-                
-        $this->search = $this->nombre;
-}
+            $this->apellidos = empty($this->apellidos) ? $apellidosTemporal : $this->apellidos;
+            $this->nombre = empty($this->nombre) ? $nombreTemporal.' '.$this->apellidos : $this->nombre.' '.$this->apellidos;
+            $this->telefono = empty($this->telefono) ? $telefonoTemporal : $this->telefono;
+            
+            if (preg_match($patternRut2, $this->textoPortapapeles, $matchesRut2)) {
+                $this->rut = $matchesRut2[0];
+            }
+            
+            if (preg_match($patternEmail2, $this->textoPortapapeles, $matchesEmail2)) {
+                $this->email = $matchesEmail2[0];
+            }
+            if ($this->nombre) {
+                 $this->search = $this->nombre;
+            }
+           
+    }
 
 
 

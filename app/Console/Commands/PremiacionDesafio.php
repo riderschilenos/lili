22690ad964df2780f32f26b30731f75fb7e3271c
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-11-06 15:01:34 -0300

    u

--- app/Console/Commands/PremiacionDesafio.php
+++ app/Console/Commands/PremiacionDesafio.php
@@ -40,115 +40,117 @@ class PremiacionDesafio extends Command
      * @return int
      */
     public function handle()
-    {   $tickets=Ticket::where('type','desafio')->where('status',3)->get();
+    {    $tickets=Ticket::where('status',3)->get();
         foreach ($tickets as $ticket) {
-            if ($ticket->user->activities){
-                $total=0;
-                foreach ($ticket->user->activities as $activitie){
-                    $date1=date($activitie->start_date_local);
-                    $date2=date($ticket->updated_at);
+            if ($ticket->evento->type=='desafio') {
+                if ($ticket->user->activities){
+                    $total=0;
+                    foreach ($ticket->user->activities as $activitie){
+                        $date1=date($activitie->start_date_local);
+                        $date2=date($ticket->updated_at);
 
-                    if ($date1>$date2){
-                         $total+=floatval($activitie->distance);
+                        if ($date1>$date2){
+                            $total+=floatval($activitie->distance);
+                        }
                     }
-                }
 
-                if ($ticket->inscripcions) {
-                    foreach ($ticket->inscripcions as $inscripcion) {
-                        if ($inscripcion->fecha->name=='Etapa 30 Km') {
-                            if ($total>30) {
-                                if($inscripcion->estado==2){
-                                    $inscripcion->estado=1;
-                                    $inscripcion->save();
-                        
-                                    foreach($ticket->inscripcions as $inscripcion){
-                                        if($inscripcion->estado==1){
-                                            $ticket->status=2;
-                                            $ticket->save();
-                                            $evento=Evento::find($ticket->evento_id);
-                                            if ($ticket->user) {
-                                                $evento->inscritos()->detach($ticket->user->id);
-                                            }
-                                          
-                        
-                                        }else{
-                                            $ticket->status=1;
-                                            $ticket->save();
-                                            $evento=Evento::find($ticket->evento_id);
-                                            $evento->inscritos()->attach($ticket->user->id);
-                                            break;
-                                        }
-                                    }
-                                }else{
-                                    $inscripcion->estado=4;
-                                    $inscripcion->save();
-                        
-                                    foreach($ticket->inscripcions as $inscripcion){
-                                        if($inscripcion->estado==4){
-                                            $ticket->status=4;
-                                            $ticket->save();
-                                            $evento=Evento::find($ticket->evento_id);
-                                            if ($ticket->user) {
-                                                $evento->inscritos()->detach($ticket->user->id);
-                                            }
-                        
-                                        }else{
-                                            $ticket->status=3;
-                                            $ticket->save();
-                        
-                                            $evento=Evento::find($ticket->evento_id);
-                                            $evento->inscritos()->attach($ticket->user->id);
-                                           
-                                            break;
-                                        }
-                                    }
-                                }
-
-                                $fono='569'.substr(str_replace(' ', '', $ticket->user->socio->fono), -8);
-                                //TOKEN QUE NOS DA FACEBOOK
-                        
-                                try {
-                                    $token = env('WS_TOKEN');
-                                    $phoneid= env('WS_PHONEID');
-                                    $version='v16.0';
-                                    $url="https://riderschilenos.cl/";
-                                    $payload=[
-                                        'messaging_product' => 'whatsapp',
-                                        "preview_url"=> false,
-                                        'to'=>$fono,
-                                        
-                                        'type'=>'template',
-                                            'template'=>[
-                                                'name'=>'desafio30_terminado',
-                                                'language'=>[
-                                                    'code'=>'es'],
+                    if ($ticket->inscripcions) {
+                        foreach ($ticket->inscripcions as $inscripcion) {
+                            if ($inscripcion->fecha->name=='Etapa 30 Km') {
+                                if ($total>30) {
+                                    if($inscripcion->estado==2){
+                                        $inscripcion->estado=1;
+                                        $inscripcion->save();
+                            
+                                        foreach($ticket->inscripcions as $inscripcion){
+                                            if($inscripcion->estado==1){
+                                                $ticket->status=2;
+                                                $ticket->save();
+                                                $evento=Evento::find($ticket->evento_id);
+                                                if ($ticket->user) {
+                                                    $evento->inscritos()->detach($ticket->user->id);
+                                                }
                                             
-                                            ]
-                                        
-                                    ];
-                                    
-                                    Http::withToken($token)->post('https://graph.facebook.com/'.$version.'/'.$phoneid.'/messages',$payload)->throw()->json();
-                                    
-                                    WhatsappMensaje::create(['numero'=> $fono,
-                                    'mensaje'=>"¡Felicidades!
-                                    Haz superado con éxito el desafio de 30Km ft. Strava",
-                                    'type'=>'enviado']);
-                        
-                        
-                                } catch (\Throwable $th) {
-                                    WhatsappMensaje::create(['numero'=> $fono,
-                                    'mensaje'=>"ERROR al enviar Mentaje => ¡Felicidades!
-                                    Haz superado con éxito el desafio de 30Km ft. Strava",
-                                    'type'=>'enviado']);
-                                }
+                            
+                                            }else{
+                                                $ticket->status=1;
+                                                $ticket->save();
+                                                $evento=Evento::find($ticket->evento_id);
+                                                $evento->inscritos()->attach($ticket->user->id);
+                                                break;
+                                            }
+                                        }
+                                    }else{
+                                        $inscripcion->estado=4;
+                                        $inscripcion->save();
+                            
+                                        foreach($ticket->inscripcions as $inscripcion){
+                                            if($inscripcion->estado==4){
+                                                $ticket->status=4;
+                                                $ticket->save();
+                                                $evento=Evento::find($ticket->evento_id);
+                                                if ($ticket->user) {
+                                                    $evento->inscritos()->detach($ticket->user->id);
+                                                }
+                            
+                                            }else{
+                                                $ticket->status=3;
+                                                $ticket->save();
+                            
+                                                $evento=Evento::find($ticket->evento_id);
+                                                $evento->inscritos()->attach($ticket->user->id);
+                                            
+                                                break;
+                                            }
+                                        }
+                                    }
 
+                                    $fono='569'.substr(str_replace(' ', '', $ticket->user->socio->fono), -8);
+                                    //TOKEN QUE NOS DA FACEBOOK
+                            
+                                    try {
+                                        $token = env('WS_TOKEN');
+                                        $phoneid= env('WS_PHONEID');
+                                        $version='v16.0';
+                                        $url="https://riderschilenos.cl/";
+                                        $payload=[
+                                            'messaging_product' => 'whatsapp',
+                                            "preview_url"=> false,
+                                            'to'=>$fono,
+                                            
+                                            'type'=>'template',
+                                                'template'=>[
+                                                    'name'=>'desafio30_terminado',
+                                                    'language'=>[
+                                                        'code'=>'es'],
+                                                
+                                                ]
+                                            
+                                        ];
+                                        
+                                        Http::withToken($token)->post('https://graph.facebook.com/'.$version.'/'.$phoneid.'/messages',$payload)->throw()->json();
+                                        
+                                        WhatsappMensaje::create(['numero'=> $fono,
+                                        'mensaje'=>"¡Felicidades!
+                                        Haz superado con éxito el desafio de 30Km ft. Strava",
+                                        'type'=>'enviado']);
+                            
+                            
+                                    } catch (\Throwable $th) {
+                                        WhatsappMensaje::create(['numero'=> $fono,
+                                        'mensaje'=>"ERROR al enviar Mentaje => ¡Felicidades!
+                                        Haz superado con éxito el desafio de 30Km ft. Strava",
+                                        'type'=>'enviado']);
+                                    }
+
+                                }
                             }
                         }
                     }
+
+
+
                 }
-
-
-
             }
         }
 

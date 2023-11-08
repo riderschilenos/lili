5f862bb7e12028b856679bd98726d514a168f149
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-11-08 20:33:19 -0300

    u

--- resources/views/livewire/socio/curriculum-deportivo.blade.php
+++ resources/views/livewire/socio/curriculum-deportivo.blade.php
@@ -5,7 +5,7 @@
         <ol class="border-l border-gray-200 dark:border-gray-700">
             @if ($resultados->count()>0)
                 @foreach ($resultados as $resultado)
-                        <li class="bg-gray-100 rounded-lg p-2 ml-2 mt-4 shadow-lg">
+                        <li class="bg-gray-100 rounded-lg p-2 mt-4 shadow-lg">
                             <div class="flex justify-between  rounded-lg p-1 items-center">
                                 <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{{$resultado->titulo}}</h3>
                                 <time class="my-auto text-sm font-normal leading-none text-gray-400 dark:text-gray-500 ml-2">Febrero 2023</time>
--- resources/views/socio/show.blade.php
+++ resources/views/socio/show.blade.php
@@ -425,24 +425,7 @@
 
                                     
                                             @endcan
-                                            <div class="grid grid-cols-4 gap-4 hidden">
-                                            
-                                                @if ($socio->user->serie_enrolled)
-                                                    
-                                                
-                                                    @foreach ($socio->user->serie_enrolled as $serie)
-                                                        <div class="text-center my-2">
-                                                            <a href="{{route('series.show', $serie)}}" class="text-main-color">
-                                                                <img class="h-16 w-20 mx-auto"
-                                                                src="{{Storage::url($serie->image->url)}}"
-                                                                alt="">
-                                                            </a>
-                                                        </div>
-                                                    @endforeach
-
-                                                @endif
-                                                                    
-                                            </div>
+                                          
                                         </div>
                                     </section>
                                     

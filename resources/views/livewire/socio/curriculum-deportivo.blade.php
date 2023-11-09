e97d0c4a245641dac8786fcd93bd0a83eb3f4026
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-11-08 22:49:06 -0300

    u

--- resources/views/livewire/socio/curriculum-deportivo.blade.php
+++ resources/views/livewire/socio/curriculum-deportivo.blade.php
@@ -132,14 +132,18 @@
           
            
         </ol>
-        <div class="flex justify-end mt-4"> 
-            <a href="#"
-            class=" inline-flex justify-end ml-auto items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-           <p class="whitespace-nowrap "> Ver todo </p>
+        @can('Super admin')
             
-           
-        </a>
-        </div>
+            <div class="flex justify-end mt-4"> 
+                <a href="#"
+                class=" inline-flex justify-end ml-auto items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+            <p class="whitespace-nowrap "> Ver todo </p>
+                
+            
+            </a>
+            </div>
+            
+        @endcan
       
     
     </div>

e8a5f32af4dcf783dc02336f67a51d8291137d31
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-11-10 16:13:00 -0300

    u

--- resources/views/livewire/socio/galeria-resultados.blade.php
+++ resources/views/livewire/socio/galeria-resultados.blade.php
@@ -32,7 +32,7 @@
                                                 @foreach ($resultado->image as $image)
                                                     <li class="shrink-0 snap-center w-full snap-mandatory">       
                                                         <img class="" src="{{Storage::url($image->url)}}" alt="" style="scroll-snap-align: center;">
-                                                        <p class="text-center">({{$n.'/'.$resultado->image->count()}})</p>
+                                                        <p class="text-center my-auto text-sm font-normal leading-none text-gray-400 dark:text-gray-500">({{$n.'/'.$resultado->image->count()}})</p>
                                                     </li>
                                                     @php
                                                         $n+=1;
@@ -49,8 +49,8 @@
                                     </div>
                                 
                                 @endif
-                                <div class="flex justify-start  rounded-lg p-1 items-center" wire:click="setresultado({{$resultado->id}})">
-                                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                                <div class="flex justify-start  rounded-lg p-1 items-center">
+                                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white cursor-pointer" wire:click="setresultado({{$resultado->id}})">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mb-1">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
                                           </svg>
@@ -58,16 +58,16 @@
                                           {{$resultado->titulo}}</h3>
                                  
                                 </div>
-                                <article class="grid grid-cols-6" wire:click="setresultado({{$resultado->id}})">
+                                <article class="grid grid-cols-6">
                                 
                                     <div class="col-span-2 items-center content-center my-auto">
                                      
                                           
                                                                 @if($resultado->image->first())
                                                                 
-                                                                    <img class="w-full h-32 object-contain content-center items-center " src=" {{Storage::url($resultado->image->first()->url)}}" alt="">
+                                                                    <img class="w-full h-32 object-contain content-center items-center " src=" {{Storage::url($resultado->image->first()->url)}}" alt="" wire:click="setresultado({{$resultado->id}})">
                                                                 @else
-                                                                    <img class="w-full h-32 object-contain content-center items-center " src="{{asset('img/copa.png')}}" alt="">
+                                                                    <img class="w-full h-32 object-contain content-center items-center " src="{{asset('img/copa.png')}}" alt="" wire:click="setresultado({{$resultado->id}})">
                                                                  
                                                                 @endif    
                                                                 
@@ -78,7 +78,9 @@
                                     <div class="px-2 py-2 col-span-4 ">
                                         <div class="flex justify-start ">
                                             <div class="items-center my-2">
-                                                <p class="text-gray-500 text-base font-bold">{{$resultado->descripcion}}</p>
+                                                <div wire:click="setresultado({{$resultado->id}})">
+                                                    <p class="text-gray-500 text-base font-bold cursor-pointer">{{$resultado->descripcion}}</p>
+                                                </div>
                                             </div>
                                         </div>
                                     
@@ -88,7 +90,7 @@
                                 </article>
                                 <article class="grid grid-cols-6">
                                 
-                                    <div class="col-span-2 items-center content-center my-auto">
+                                    <div class="col-span-2 items-center content-center my-auto text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                                      
                                           
                                                                 @if($resultado->image->first())

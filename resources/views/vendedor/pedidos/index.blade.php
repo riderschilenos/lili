873e3a79d990f831996117f725d365f17eb5438b
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-11-24 14:55:58 -0300

    u

--- resources/views/vendedor/pedidos/index.blade.php
+++ resources/views/vendedor/pedidos/index.blade.php
@@ -45,6 +45,49 @@
        
             </div>
             <div x-show="activeTab===1">
+                @can('Super admin')
+                    <div class="bg-gray-700 pt-4">
+                    
+                
+
+
+                        <div class="max-w-4xl mx-auto px-2 sm:px-6 mt-2 lg:px-8 pb-4" x-data="{whatsap: true}">
+                                
+                                @livewire('admin.money-info')
+
+                            <div class="flex justify-between">
+                                
+                                <button class="btn btn-success ml-2 text-center text-xl" x-on:click="whatsap=!whatsap">Whatsapp RCH</button>
+                                
+                                <div>
+                                    <a href="{{route('strava.sync')}}">
+                                        <button class="btn btn-danger ml-2 text-center text-xl">Strava Sync</button>
+                                    </a>
+                                    <a href="{{route('strava.check')}}">
+                                        <button class="btn btn-danger ml-2 text-center text-xl">Strava Check</button>
+                                    </a>
+                                </div>
+                                <a href="{{route('contabilidad')}}">
+                                    <button class="btn btn-danger ml-2 text-center text-xl">Gráficos y Estadisticas</button>
+                                </a>
+
+                                
+
+                            </div>
+
+                            <div x-show="!whatsap">
+
+                            @livewire('admin.whatsapp-sender-cliente')
+
+                            </div>
+                        </div>
+
+                        <div class="max-w-7xl mx-auto px-2 sm:px-6 mt-2 lg:px-8">
+                            @livewire('admin.pedidos-count')
+                        </div>
+
+                    </div>
+                @endcan  
                 @livewire('vendedor.pedidos-index')
             </div>
             

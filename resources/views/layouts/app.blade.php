6544a835daa638d04200ecb4a9b77bf332f3f063
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-03 19:09:30 -0300

    u

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,7 @@
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     
         
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         @yield('css')
         

07de20899714eece29259ba327f66ed5349ce487
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-04 11:12:47 -0300

    u

--- app/Http/Livewire/socio/StravaGeneralList.php
+++ app/Http/Livewire/socio/StravaGeneralList.php
@@ -11,6 +11,12 @@ class StravaGeneralList extends Component
 
     public function render()
     {   $atletas_stravas=AtletaStrava::where('id','>',0)->paginate(100);
+        $atletas_stravas->sortByDesc(function ($atleta) {
+            if ($atleta->activities) {
+                return $atleta->activities->sum('distance'); // Suponiendo que 'activities' es la relación de actividades en tu modelo de atleta
+            }
+          
+        });
         return view('livewire.socio.strava-general-list',compact('atletas_stravas'));
     }
 }

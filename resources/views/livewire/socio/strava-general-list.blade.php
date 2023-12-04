0092435ce0b3f38dcd3a7894b2c6613268e9f566
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-04 09:32:16 -0300

    u

--- resources/views/livewire/socio/strava-general-list.blade.php
+++ resources/views/livewire/socio/strava-general-list.blade.php
@@ -26,14 +26,14 @@
                             <img src="{{$atleta->user->profile_photo_url }}" class="w-[50px] h-[50px] inline-block shrink-0 rounded-2xl" alt="">
                             </div>
                             <div class="flex flex-col justify-start">
-                            <a href="javascript:void(0)" class="mb-1 font-semibold transition-colors duration-200 ease-in-out text-lg/normal text-secondary-inverse hover:text-primary">{{$atleta->user->name}}</a>
+                            <a href="javascript:void(0)" class="mb-1 font-semibold transition-colors duration-200 ease-in-out text-base  text-secondary-inverse hover:text-primary">{{$atleta->user->name}}</a>
                             </div>
                         </div>
                         </td>
                     
                         <td class="p-3 text-center">
                         <div class="flex justify-center">
-                            <span class="text-center px-4 py-3 mx-auto items-center font-semibold text-[.95rem] text-red-600 bg-red-100 rounded-lg"> {{$atleta->user->socio->disciplina->name}} </span>
+                            <span class="text-center px-4 py-3 mx-auto items-center font-semibold text-base text-red-600 bg-red-100 rounded-lg"> {{$atleta->user->socio->disciplina->name}} </span>
                         </div>
                         </td>
                         <td class="pr-0 text-center">
@@ -61,44 +61,7 @@
                     </tr>
                     
                 @endforeach
-            
-                <tr class="border-b border-dashed last:border-b-0">
-                  <td class="p-3 pl-0">
-                    <div class="flex items-center">
-                      <div class="relative inline-block shrink-0 rounded-2xl me-3">
-                        <img src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/riva-dashboard-tailwind/img/img-40-new.jpg" class="w-[50px] h-[50px] inline-block shrink-0 rounded-2xl" alt="">
-                      </div>
-                      <div class="flex flex-col justify-start">
-                        <a href="javascript:void(0)" class="mb-1 font-semibold transition-colors duration-200 ease-in-out text-lg/normal text-secondary-inverse hover:text-primary"> Geolocation App </a>
-                      </div>
-                    </div>
-                  </td>
-                
-                  <td class="p-3 text-center">
-                    <span class="text-center align-baseline inline-flex px-4 py-3 mr-auto items-center font-semibold text-[.95rem] leading-none text-light-inverse bg-light rounded-lg"> Under Review </span>
-                  </td>
-                  <td class="pr-0 text-center">
-                    <span class="font-semibold text-light-inverse text-md/normal">2024-04-11</span>
-                  </td>
-                  <td class="p-3 pr-0 text-center">
-                    <span class="text-center align-baseline inline-flex px-2 py-1 mr-auto items-center font-semibold text-base/none text-danger bg-danger-light rounded-lg">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6L9 12.75l4.286-4.286a11.948 11.948 0 014.306 6.43l.776 2.898m0 0l3.182-5.511m-3.182 5.51l-5.511-3.181" />
-                      </svg> 2.7% </span>
-                  </td>
-                
-               
-                  <td class="p-3 pr-0 text-center">
-                    <button class="mx-auto text-secondary-dark bg-light-dark hover:text-primary flex items-center h-[25px] w-[25px] text-base font-medium leading-normal text-center align-middle cursor-pointer rounded-2xl transition-colors duration-200 ease-in-out shadow-none border-0 justify-center">
-                      <span class="flex items-center justify-center p-0 m-0 leading-none shrink-0 ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                        </svg>
-                      </span>
-                    </button>
-                  </td>
-                </tr>
-                
+    
               
               </tbody>
             </table>

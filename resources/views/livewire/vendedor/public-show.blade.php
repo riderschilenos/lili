e293913cead09a8b7deec2a6db23037e464289a6
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-03 22:45:46 -0300

    u

--- resources/views/livewire/vendedor/public-show.blade.php
+++ resources/views/livewire/vendedor/public-show.blade.php
@@ -86,7 +86,7 @@
                         </div>
                       </div>
             
-                      <div class="flex -mx-2 mb-4">
+                      <div class="flex mx-2 mb-4">
                         <template x-for="i in 4">
                           <div class="flex-1 px-2">
                             <button x-on:click="image = i" :class="{ 'ring-2 ring-indigo-300 ring-inset': image === i }" class="focus:outline-none w-full rounded-lg h-24 md:h-32 bg-gray-100 flex items-center justify-center">

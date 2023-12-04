fce5ae0a5ec35277e8de179453079113abe036f5
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-04 01:01:48 -0300

    u

---
+++ app/Http/Livewire/socio/StravaCountTotal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Socio;
+
+use Livewire\Component;
+
+class StravaCountTotal extends Component
+{
+    public function render()
+    {
+        return view('livewire.socio.strava-count-total');
+    }
+}
--- resources/views/livewire/socio/socio-search.blade.php
+++ resources/views/livewire/socio/socio-search.blade.php
@@ -38,38 +38,7 @@
             
                 </div>
                 <div class="w-full lg:w-1/2 lg:pl-8">
-                    <div class="flex justify-center pb-2 items-center">
-                        <div class="bg-white rounded-lg profile-card w-96">
-                        
-                            <div class="text-center mb-4">
-                                <div class="grid grid-cols-3">
-                                    <div class="flex md:hidden">
-                                        <div class="w-full h-20  my-auto items-center m-auto rounded-xl text-white shadow-2xl" style="backface-visibility:hidden">
-                                            <img src="{{asset('img/strava/strava.jpg')}}" class="relative object-cover w-full h-full rounded-xl" />
-                                        
-                                        </div>
-                                    </div>
-                                    <div class="col-span-2 md:col-span-3 flex items-center">
-                                        <h2 class="text-xl font-semibold items-center my-auto">¿Cuanto Kilómetros Hemos Pedaleado?</h2>
-                                    </div>
-
-                                </div>
-                            
-                            
-                            </div>
-                            <div class="grid grid-cols-2 gap-4">
-                                <div class="text-center p-2 bg-gray-100 rounded-lg">
-                                
-                                    <p class="text-lg font-semibold mt-2">12.000 km</p>
-                                    <p class="text-sm text-gray-600">Total</p>
-                                </div>
-                                <div class="text-center p-2 bg-gray-100 rounded-lg">
-                                    <p class="text-lg font-semibold mt-2">1.654 km</p>
-                                    <p class="text-sm text-gray-600">Ultimos 7 Días</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                  @livewire('socio.strava-count-total')
                 </div>
             
             

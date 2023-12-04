32534012d7dff9f6ac9cf9028c65b8bc529ce0b1
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-03 21:51:49 -0300

    u

--- resources/views/livewire/vendedor/public-show.blade.php
+++ resources/views/livewire/vendedor/public-show.blade.php
@@ -125,7 +125,7 @@
                     <div class="flex justify-between py-4 space-x-4">
                       <div class="relative">
                         <div class="text-center left-0 pt-2 right-0 absolute block text-xs uppercase text-gray-400 tracking-wide font-semibold mb-2">Cantidad</div>
-                            <select class="cursor-pointer appearance-none rounded-xl border text-base border-gray-200 pl-4 pr-8 h-14 flex items-end pb-1 w-32 text-center">
+                            <select class="cursor-pointer appearance-none rounded-xl border mx-auto text-base border-gray-200 pl-4 pr-8 h-14 flex items-end pb-1 w-32 text-center">
                             <option>1</option>
                             <option>2</option>
                             <option>3</option>

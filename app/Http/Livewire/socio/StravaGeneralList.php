3fd22850de84ccf00678e3a6156866e2e52ef9ca
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-05 20:56:25 -0300

    u

--- app/Http/Livewire/socio/StravaGeneralList.php
+++ app/Http/Livewire/socio/StravaGeneralList.php
@@ -11,6 +11,7 @@ use Livewire\WithPagination;
 
 class StravaGeneralList extends Component
 {   use WithPagination;
+    public $orden=1;
 
     public function render()
     {   $atletas_stravas=User::select('users.id', 'users.name', 'users.profile_photo_path', DB::raw('COALESCE(SUM(activities.distance), 0) AS total_distance'))
@@ -19,8 +20,22 @@ class StravaGeneralList extends Component
         ->groupBy('users.id', 'users.name', 'users.profile_photo_path')
         ->orderByDesc('total_distance')
         ->paginate(100);
+
+        $fechaHace7Dias = Carbon::now()->subDays(7);
+
+        $atletas_stravas7dias = User::select('users.id', 'users.name', 'users.profile_photo_path', DB::raw('COALESCE(SUM(activities.distance), 0) AS total_distance'))
+            ->join('atleta_stravas', 'users.id', '=', 'atleta_stravas.user_id')
+            ->leftJoin('activities', 'users.id', '=', 'activities.user_id')
+            ->where('activities.start_date_local', '>=', $fechaHace7Dias) // Filtrar por actividades de los últimos 7 días
+            ->groupBy('users.id', 'users.name', 'users.profile_photo_path')
+            ->orderByDesc('total_distance')
+            ->paginate(100);
+
         $now=Carbon::now();
         
-        return view('livewire.socio.strava-general-list',compact('atletas_stravas','now'));
+        return view('livewire.socio.strava-general-list',compact('atletas_stravas','atletas_stravas7dias','now'));
+    }
+    public function set_orden($id){
+        $this->orden==$id;
     }
 }

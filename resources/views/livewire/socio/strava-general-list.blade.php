291fc9bba5fad3fe5a333c338b574f7045f302a7
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-05 20:41:56 -0300

    u

--- resources/views/livewire/socio/strava-general-list.blade.php
+++ resources/views/livewire/socio/strava-general-list.blade.php
@@ -113,7 +113,8 @@
         
                                         <td class="px-6 py-4 whitespace-nowrap">
                                             <div class="text-sm text-gray-900"> 
-                                                {{$week}}
+                                                <div class="text-sm text-gray-900 text-center font-bold mt-2">  {{$week}} Kms </div>
+                                               
                                                     <span class="hidden text-center align-baseline inline-flex px-2 py-1 mr-auto items-center font-semibold text-base/none text-danger bg-danger-light rounded-lg">
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6L9 12.75l4.286-4.286a11.948 11.948 0 014.306 6.43l.776 2.898m0 0l3.182-5.511m-3.182 5.51l-5.511-3.181" />

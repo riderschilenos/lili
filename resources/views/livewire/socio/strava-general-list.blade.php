2efa48b73fb3e7abe44d45af941b29f82b714054
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-05 11:47:28 -0300

    u

--- resources/views/livewire/socio/strava-general-list.blade.php
+++ resources/views/livewire/socio/strava-general-list.blade.php
@@ -55,11 +55,22 @@
                                                     <div class="hidden sm:flex  text-sm text-gray-900">
                                                         {{Str::limit($user->name,40)}}
                                                     </div>
-                                                    <div class="text-sm text-gray-500">
+                                                    <div class="text-sm text-gray-500 flex justify-start">
                                                         <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
                                                         {{$user->socio->disciplina->name}}
                                                         </span>
+                                                       
                                                     </div>
+                                                    @if(!is_null($user->socio->direccion))
+                                                        <div class="text-xs flex items-center mt-1">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-3 h-3 mx-1">
+                                                                <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
+                                                              </svg>
+                                                              
+                                                              
+                                                            {{$user->socio->direccion->comuna." ".Str::limit($user->socio->direccion->region,18)}} 
+                                                        </div>
+                                                    @endif
                                                 </div>
                                             </div>
                                             </a>

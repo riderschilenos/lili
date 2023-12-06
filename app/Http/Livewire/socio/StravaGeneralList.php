2bf082a8bbdf0c7341ef01e8854dfda742dbaf38
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-05 21:14:56 -0300

    u

--- app/Http/Livewire/socio/StravaGeneralList.php
+++ app/Http/Livewire/socio/StravaGeneralList.php
@@ -23,16 +23,20 @@ class StravaGeneralList extends Component
 
         $fechaHace7Dias = Carbon::now()->subDays(7);
 
-        $atletas_stravas7dias =  User::select('users.id', 'users.name', 'users.profile_photo_path', DB::raw('COALESCE(SUM(last_week_activities.distance), 0) AS total_distance'))
-                                ->join('atleta_stravas', 'users.id', '=', 'atleta_stravas.user_id')
-                                ->leftJoin(DB::raw('(SELECT user_id, SUM(distance) AS distance
-                                                FROM activities
-                                                WHERE start_date_local >= :fechaHace7Dias
-                                                GROUP BY user_id) AS last_week_activities'), 'users.id', '=', 'last_week_activities.user_id')
-                                ->setBindings(['fechaHace7Dias' => $fechaHace7Dias]) // Pasa la variable a la subconsulta
-                                ->groupBy('users.id', 'users.name', 'users.profile_photo_path')
-                                ->orderByDesc('total_distance')
-                                ->paginate(100);
+        $atletas_stravas7dias = User::select(
+                        'users.id',
+                        'users.name',
+                        'users.profile_photo_path',
+                        DB::raw('COALESCE(SUM(activities.distance), 0) AS total_distance'),
+                        DB::raw('COALESCE(SUM(CASE WHEN activities.start_date_local >= :fechaHace7Dias THEN activities.distance ELSE 0 END), 0) AS last_week_distance')
+                    )
+                    ->join('atleta_stravas', 'users.id', '=', 'atleta_stravas.user_id')
+                    ->leftJoin('activities', 'users.id', '=', 'activities.user_id')
+                    ->groupBy('users.id', 'users.name', 'users.profile_photo_path')
+                    ->orderByDesc('total_distance') // Ordenar por la suma total de distancia
+                    ->orderByDesc('last_week_distance') // Luego por la suma de la última semana
+                    ->setBindings(['fechaHace7Dias' => $fechaHace7Dias]) // Pasa la variable a la subconsulta
+                    ->paginate(100);
 
         $now=Carbon::now();
         

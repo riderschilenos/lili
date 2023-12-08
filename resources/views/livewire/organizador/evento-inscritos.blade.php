322d949f8cac172989db18c1a9b61b9ce9e8466e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-08 14:58:23 -0300

    u

--- resources/views/livewire/organizador/evento-inscritos.blade.php
+++ resources/views/livewire/organizador/evento-inscritos.blade.php
@@ -82,6 +82,7 @@
                                                             </a>
                                                             </div>
                                                             @can('Super admin')
+                                                                        @if ($ticket->status==3)
                                                                             <div class="ml-auto">
                                                                                     @php
                                                                                         if($ticket->metodo=='TRANSFERENCIA'){
@@ -97,6 +98,7 @@
                                                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
                                                                                     </svg></span>  
                                                                             </div>
+                                                                        @endif
                                                                 @endcan
                                                         </div>
                                                     </td>
@@ -273,20 +275,24 @@
                                                         </div>
                                                         <div class="ml-auto">
                                                             @can('Super admin')
-                                                                @php
-                                                                    if($tick->metodo=='TRANSFERENCIA'){
-                                                                        $ganancia=($tick->inscripcion*$tick->evento->comision/100);
-                                                                    }else{
-                                                                        $ganancia=($tick->inscripcion*$tick->evento->comision/100)-($tick->inscripcion*0.037961);
-                                                                    }
-                                                                    $gananciatotal+=$ganancia;
-                                                                @endphp
-                                                            <span class="text-center align-baseline inline-flex px-2 py-1 mr-auto items-center font-semibold text-base/none text-success bg-success-light rounded-lg">
-                                                                +${{number_format($ganancia)}} <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-1">
-                                                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
-                                                                </svg></span>  
+                                                                @if ($tick->status==3)
+                                                                    
                                                                 
-                                                            @endcan
+                                                                    @php
+                                                                        if($tick->metodo=='TRANSFERENCIA'){
+                                                                            $ganancia=($tick->inscripcion*$tick->evento->comision/100);
+                                                                        }else{
+                                                                            $ganancia=($tick->inscripcion*$tick->evento->comision/100)-($tick->inscripcion*0.037961);
+                                                                        }
+                                                                        $gananciatotal+=$ganancia;
+                                                                    @endphp
+                                                                <span class="text-center align-baseline inline-flex px-2 py-1 mr-auto items-center font-semibold text-base/none text-success bg-success-light rounded-lg">
+                                                                    +${{number_format($ganancia)}} <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-1">
+                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
+                                                                    </svg></span>  
+                                                                    
+                                                                @endcan
+                                                            @endif
                                                         </div>
                                                     </div>
                                                 </td>

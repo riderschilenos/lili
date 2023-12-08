4e95d439c677ea7eccec3113767b164bfeb131c3
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-07 22:50:48 -0300

    u

--- app/Http/Livewire/socio/StravaGeneralList.php
+++ app/Http/Livewire/socio/StravaGeneralList.php
@@ -36,9 +36,24 @@ class StravaGeneralList extends Component
                             ->setBindings(['fechaHace7Dias' => $fechaHace7Dias]) // Pasa la variable a la subconsulta
                             ->paginate(100);
 
+        $atletas_stravas7dias2 = User::select(
+                                'users.id',
+                                'users.name',
+                                'users.profile_photo_path',
+                                DB::raw('COALESCE(SUM(CASE WHEN activities.start_date_local >= :fechaHace7Dias THEN activities.distance ELSE 0 END), 0) AS last_week_distance'),
+                                DB::raw('COALESCE(SUM(activities.distance), 0) AS total_distance') // Suma total de la distancia
+                            )
+                                ->join('atleta_stravas', 'users.id', '=', 'atleta_stravas.user_id')
+                                ->leftJoin('activities', 'users.id', '=', 'activities.user_id')
+                                ->groupBy('users.id', 'users.name', 'users.profile_photo_path')
+                                ->havingRaw('last_week_distance = 0') // Filtra usuarios con last_week_distance igual a cero
+                                ->orderByDesc('total_distance') // Ordena por la suma total de distancia de mayor a menor
+                                ->setBindings(['fechaHace7Dias' => $fechaHace7Dias])
+                                ->paginate(100);
+
         $now=Carbon::now();
         
-        return view('livewire.socio.strava-general-list',compact('atletas_stravas','atletas_stravas7dias','now'));
+        return view('livewire.socio.strava-general-list',compact('atletas_stravas','atletas_stravas7dias','atletas_stravas7dias2','now'));
     }
     public function set_orden($id){
         $this->orden==$id;

e5f892c71b5702c42de895a50b7c89bbe9291b9e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-08 14:35:13 -0300

    u

--- resources/views/livewire/organizador/evento-inscritos.blade.php
+++ resources/views/livewire/organizador/evento-inscritos.blade.php
@@ -280,7 +280,7 @@
                                                                             @can('Super admin')
                                                                                 
                                                                             <span class="text-center align-baseline inline-flex px-2 py-1 mr-auto items-center font-semibold text-base/none text-success bg-success-light rounded-lg">
-                                                                                +6.5% <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
+                                                                                +$350 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-1">
                                                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
                                                                                 </svg></span>  
                                                                                 

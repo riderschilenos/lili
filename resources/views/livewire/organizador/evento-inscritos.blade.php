33341c38e8c86ef42d97c326214f7e4074fae814
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-08 18:25:04 -0300

    u

--- resources/views/livewire/organizador/evento-inscritos.blade.php
+++ resources/views/livewire/organizador/evento-inscritos.blade.php
@@ -551,7 +551,7 @@
                                         </tr>
                                   
                                 
-                            @else
+                            @elseif ($inscripcion->ticket->ticketable_type=='App\Models\Invitado')
                                     <tr>
                                     
                                         <td class="px-6 py-4 whitespace-nowrap">

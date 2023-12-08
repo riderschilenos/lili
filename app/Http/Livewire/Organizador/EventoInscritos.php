3fe33781b89f7f0926bb47449bbbc57311823e1e
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-07 23:57:34 -0300

    u

--- app/Http/Livewire/Organizador/EventoInscritos.php
+++ app/Http/Livewire/Organizador/EventoInscritos.php
@@ -34,15 +34,16 @@ class EventoInscritos extends Component
 
     public function render()
     {   
-        $tickets =   $this->evento->tickets()
-                        ->where('status', '>=', 1)
-                        ->join('inscripcions', 'tickets.id', '=', 'inscripcions.ticket_id')
-                        ->join('fecha_categorias', 'inscripcions.fecha_categoria_id', '=', 'fecha_categorias.id')
-                        ->join('categorias', 'fecha_categorias.categoria_id', '=', 'categorias.id')
-                        ->orderBy('categorias.name', 'asc') // Luego, ordenar por el nombre de la categoría en orden ascendente
-                        ->orderBy('tickets.id', 'desc') // Ordenar por ticketable_type en orden descendente
-                        ->select('tickets.*') // Seleccionar todas las columnas de la tabla 'tickets'
-                        ->get();
+        $tickets =  $this->evento->tickets()
+                    ->where('status', '>=', 1)
+                    ->join('inscripcions', 'tickets.id', '=', 'inscripcions.ticket_id')
+                    ->join('fecha_categorias', 'inscripcions.fecha_categoria_id', '=', 'fecha_categorias.id')
+                    ->join('categorias', 'fecha_categorias.categoria_id', '=', 'categorias.id')
+                    ->orderBy('categorias.name', 'asc')
+                    ->orderBy('tickets.id', 'desc')
+                    ->select('tickets.*')
+                    ->distinct() // Agregar DISTINCT para obtener resultados únicos de la tabla 'tickets'
+                    ->get();
                         
         $socios=Socio::all();
         $invitados=Invitado::all();

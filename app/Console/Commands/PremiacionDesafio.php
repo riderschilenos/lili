d294b78a3671951e904ed3c1281239dd95e8cb61
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-08 17:43:06 -0300

    u

--- app/Console/Commands/PremiacionDesafio.php
+++ app/Console/Commands/PremiacionDesafio.php
@@ -157,7 +157,7 @@ class PremiacionDesafio extends Command
                 if ($ticket->inscripcions) {
                     foreach ($ticket->inscripcions as $inscripcion) {
                         if ($inscripcion->fecha->name=='Etapa 15 km' && $inscripcion->estado<4) {
-                            if ($total>30) {
+                            if ($total>15) {
                                 if($inscripcion->estado==2){
                                     $inscripcion->estado=1;
                                     $inscripcion->save();

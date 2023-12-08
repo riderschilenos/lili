1e75188cba84a9d55790f9afce34e8e8c88793c5
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-07 22:38:33 -0300

    u

--- app/Http/Livewire/socio/StravaGeneralList.php
+++ app/Http/Livewire/socio/StravaGeneralList.php
@@ -28,13 +28,19 @@ class StravaGeneralList extends Component
             'users.name',
             'users.profile_photo_path',
             DB::raw('COALESCE(SUM(activities.distance), 0) AS total_distance'),
-            DB::raw('COALESCE(SUM(CASE WHEN activities.start_date_local >= :fechaHace7Dias THEN activities.distance ELSE 0 END), 0) AS last_week_distance')
+            DB::raw('COALESCE(SUM(CASE WHEN activities_last_week.total_distance_last_week IS NOT NULL THEN activities_last_week.total_distance_last_week ELSE 0 END), 0) AS last_week_distance')
         )
         ->join('atleta_stravas', 'users.id', '=', 'atleta_stravas.user_id')
         ->leftJoin('activities', 'users.id', '=', 'activities.user_id')
+        ->leftJoin(DB::raw('(SELECT user_id, SUM(distance) AS total_distance_last_week
+                         FROM activities
+                         WHERE start_date_local >= :fechaHace7Dias
+                         GROUP BY user_id) AS activities_last_week'), function($join) {
+                             $join->on('users.id', '=', 'activities_last_week.user_id');
+                         })
         ->groupBy('users.id', 'users.name', 'users.profile_photo_path')
-        ->orderByDesc('last_week_distance') // Ordenar por la suma de la última semana
-        ->setBindings(['fechaHace7Dias' => $fechaHace7Dias]) // Pasa la variable a la subconsulta
+        ->orderByDesc('last_week_distance')
+        ->setBindings(['fechaHace7Dias' => $fechaHace7Dias])
         ->paginate(100);
         
         $now=Carbon::now();

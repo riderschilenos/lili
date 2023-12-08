24fe3fd57a179e2de8e521a10a6e65b271aa0ba1
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-08 14:18:11 -0300

    u

--- resources/views/livewire/organizador/evento-inscritos.blade.php
+++ resources/views/livewire/organizador/evento-inscritos.blade.php
@@ -198,7 +198,7 @@
 
                                            
                                                         @can('Super admin')
-                                                            @if ($ticket->status<3;)
+                                                            @if ($ticket->status<3)
                                                                 <a wire:click="pagomanual({{$ticket->id}})" class="btn btn-success cursor-pointer h-10 my-auto">PAGO MANUAL</a>
                                                             @endif
                                                         @endcan   

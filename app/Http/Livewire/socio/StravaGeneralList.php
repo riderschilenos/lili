acdf6a0973f21143fef023918736925a6fd5c913
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-07 22:20:44 -0300

    u

--- app/Http/Livewire/socio/StravaGeneralList.php
+++ app/Http/Livewire/socio/StravaGeneralList.php
@@ -24,18 +24,18 @@ class StravaGeneralList extends Component
         $fechaHace7Dias = Carbon::now()->subDays(7);
 
         $atletas_stravas7dias = User::select(
-                                'users.id',
-                                'users.name',
-                                'users.profile_photo_path',
-                                DB::raw('COALESCE(SUM(activities.distance), 0) AS total_distance'),
-                                DB::raw('COALESCE(SUM(CASE WHEN activities.start_date_local >= :fechaHace7Dias THEN activities.distance ELSE 0 END), 0) AS last_week_distance')
-                            )
-                            ->join('atleta_stravas', 'users.id', '=', 'atleta_stravas.user_id')
-                            ->leftJoin('activities', 'users.id', '=', 'activities.user_id')
-                            ->groupBy('users.id', 'users.name', 'users.profile_photo_path')
-                            ->orderByDesc('last_week_distance') // Ordenar primero por la suma de la última semana
-                            ->setBindings(['fechaHace7Dias' => $fechaHace7Dias]) // Pasa la variable a la subconsulta
-                            ->paginate(100);
+            'users.id',
+            'users.name',
+            'users.profile_photo_path',
+            DB::raw('COALESCE(SUM(activities.distance), 0) AS total_distance'),
+            DB::raw('COALESCE(SUM(CASE WHEN activities.start_date_local >= :fechaHace7Dias THEN activities.distance ELSE 0 END), 0) AS last_week_distance')
+        )
+        ->join('atleta_stravas', 'users.id', '=', 'atleta_stravas.user_id')
+        ->leftJoin('activities', 'users.id', '=', 'activities.user_id')
+        ->groupBy('users.id', 'users.name', 'users.profile_photo_path')
+        ->orderByDesc('last_week_distance') // Ordenar por la suma de la última semana
+        ->setBindings(['fechaHace7Dias' => $fechaHace7Dias]) // Pasa la variable a la subconsulta
+        ->paginate(100);
 
         $now=Carbon::now();
         

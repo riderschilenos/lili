1d9f2164221052b52fd507b3a1a32adfdfa476fa
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-10 20:24:25 -0300

    u

--- app/Http/Controllers/StravaController.php
+++ app/Http/Controllers/StravaController.php
@@ -39,7 +39,7 @@ class StravaController extends Controller
         $atletaId = $data['athlete']['id'];
         $athleteName = $data['athlete']['username'];
         
-        AtletaStrava::create([
+        $atletaStrava=AtletaStrava::create([
             'user_id' => auth()->user()->id, // Reemplaza con el ID del atleta
             'atleta_id' => $atletaId, // Reemplaza con el ID del atleta
             'access_token' => $data['access_token'],
@@ -53,7 +53,8 @@ class StravaController extends Controller
         // $data['access_token'] contiene el token de acceso que puedes utilizar para acceder a la API de Strava
        
          //TOKEN QUE NOS DA FACEBOOK
-
+         
+         
         try {
             $token = env('WS_TOKEN');
             $phoneid= env('WS_PHONEID');
@@ -86,6 +87,88 @@ class StravaController extends Controller
             ];
             
             Http::withToken($token)->post('https://graph.facebook.com/'.$version.'/'.$phoneid.'/messages',$wsload)->throw()->json();
+            
+            $accessTokenExpiresAt = $atletaStrava->token_expires_at;
+         if (now() >= $accessTokenExpiresAt) {
+             // El token de acceso ha caducado, solicita un nuevo token usando el token de actualización
+         
+             // Realiza una solicitud POST para obtener un nuevo token de acceso usando el token de actualización
+             $client = new Client();
+             $response = $client->post('https://www.strava.com/oauth/token', [
+                 'form_params' => [
+                     'client_id' => env('ST_CLIENT_ID'),
+                     'client_secret' => env('ST_CLIENT_SECRET'),
+                     'refresh_token' => $atletaStrava->refresh_token,
+                     'grant_type' => 'refresh_token',
+                 ],
+             ]);
+         
+             $newData = json_decode($response->getBody(), true);
+         
+             // Actualiza el token de acceso en la base de datos con el nuevo token
+             $newAccessToken = $newData['access_token'];
+             $newAccessTokenExpiresAt = now()->addSeconds($newData['expires_in']); // Calcula el nuevo tiempo de expiración
+             $atletaStrava->update(['access_token'=>$newAccessToken,
+                         'token_expires_at'=>$newAccessTokenExpiresAt]);
+
+             // Actualiza el token de acceso y su tiempo de expiración en la base de datos
+         } else {
+             // El token de acceso aún es válido, úsalo para hacer solicitudes a la API de Strava
+         }
+         // ID del atleta para el que deseas obtener las actividades
+         $athleteId = $atletaStrava->atleta_id;
+         $accessToken = $atletaStrava->access_token;
+         // URL de la API de Strava
+         $url = "https://www.strava.com/api/v3/athletes/{$athleteId}/activities";
+
+         // Configuración de la solicitud cURL
+         $ch = curl_init($url);
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         curl_setopt($ch, CURLOPT_HTTPHEADER, [
+             'Authorization: Bearer ' . $accessToken,
+         ]);
+
+         // Realizar la solicitud cURL
+         $response = curl_exec($ch);
+
+         $activities = json_decode($response, true);
+
+        
+
+         foreach($activities as $item){
+        
+             $activ=Activitie::where('strava_id',$item['id'])->first();
+
+             if ($activ) {
+                //
+             } else {
+                 Activitie::create([
+                     'user_id'=>$atletaStrava->user_id,
+                     'name'=>$item['name'],
+                     'type'=>$item['type'],
+                     'strava_id'=>$item['id'],
+                     'start_date_local'=>$item['start_date_local'],
+                     'moving_time'=> $item['moving_time'],
+                  'distance'=>number_format(($item['distance']/1000), 2, '.', '.'),
+                  'total_elevation_gain'=>null,
+                  'average_speed'=>number_format($item['average_speed'], 2),
+                  'max_speed'=>number_format($item['max_speed'], 2),
+                    'commute'=>$item['commute'] ? 'Yes' : 'No' ,
+                    'private'=>$item['private'] ? 'Yes' : 'No' ,
+                    'achievement_count'=>$item['achievement_count']
+                 ]);
+                 $user=User::find($atletaStrava->user_id);
+                 if ($user) {
+                     $user->ForceFill([
+                         'updated_at'=> Carbon::now()
+                     ])->save();
+                 }
+                }
+                
+                
+       
+            }
+            
             return redirect()->route('home');
 
          
@@ -147,12 +230,10 @@ class StravaController extends Controller
 
             $activities = json_decode($response, true);
 
-            var_dump($activities); // O utiliza print_r($activities); para una visualización más legible
 
 
             foreach($activities as $item){
-                var_dump($item); // O utiliza print_r($activities); para una visualización más legible
-
+               
                 $activ=Activitie::where('strava_id',$item['id'])->first();
 
                 if ($activ) {
@@ -396,7 +477,7 @@ class StravaController extends Controller
                                                 'pedidoable_type'=> 'App\Models\Socio']);
                                             $pedido->status=5;
                                             $pedido->save();
-                                            
+
                                             $orden= Orden::create([
                                                     'producto_id'=> 55,
                                                     'name'=>'Etapa 30 km',

48c6cf8b297fc24fe950d8dd72f90d7013702a58
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-10 13:13:45 -0300

    u

--- resources/views/Evento/show.blade.php
+++ resources/views/Evento/show.blade.php
@@ -230,11 +230,11 @@
                                                                                     <a href="{{route('socio.show', $item->user->socio)}}">
                                                                             
                                                                                         {{ Str::limit($item->user->name, 18) }}<br>
-                                                                                        <span class="px-2 mt-1 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-100 text-gray-800">
+                                                                                        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-100 text-gray-800">
                                                                                            Ver Perfil Rider
                                                                                         </span>
                                                                                         @if(!is_null($item->user->socio->direccion))
-                                                                                            <div class="text-xs flex items-center mt-1">
+                                                                                            <div class="text-xs flex items-center">
                                                                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-3 h-3 mx-1">
                                                                                                     <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
                                                                                                 </svg>

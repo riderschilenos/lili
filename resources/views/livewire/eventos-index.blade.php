a9527ddc1989814d6b74eab05e91bb40ccfe58af
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-14 15:03:11 -0300

    u

--- resources/views/livewire/eventos-index.blade.php
+++ resources/views/livewire/eventos-index.blade.php
@@ -241,7 +241,7 @@
                                                                 </div>
                                                             </a>
                     
-                                                            <ul class="grid grid-cols-1 lg:grid-cols-1 gap-x-4 gap-y-2 mt-my-auto">
+                                                            <ul class="grid grid-cols-1 lg:grid-cols-1 gap-x-4 gap-y-2 my-auto">
                                                                 @php
                                                                     $n=0;
                                                                 @endphp

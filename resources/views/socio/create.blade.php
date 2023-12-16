7d06423b0fa29487ec06ad4c0df40eba43b870b8
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-16 00:01:49 -0300

    u

--- resources/views/socio/create.blade.php
+++ resources/views/socio/create.blade.php
@@ -37,8 +37,8 @@
             <div class="card pb-8">
                 <div class="card-body">
                     
-                    <div class="bg-white font-sans flex items-center justify-center">
-                        <div class="px-2 sm:px-8 py-8">
+                    <div class="bg-white font-sans flex items-center justify-center mb-4">
+                        <div class="">
                             <div class="max-w-lg mx-auto">
                                 <div class="transition-all duration-300 bg-white p-4 rounded-lg shadow-md border-l-4 border-blue-600">
                                     <h2 class="text-2xl font-semibold mb-2 text-blue-600">Hola {{Auth()->user()->name}}</h2>

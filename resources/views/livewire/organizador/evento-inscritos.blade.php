d7ff032d12fad39edf311eaad9dcba0089691d55
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-18 19:15:39 -0300

    i

--- resources/views/livewire/organizador/evento-inscritos.blade.php
+++ resources/views/livewire/organizador/evento-inscritos.blade.php
@@ -136,7 +136,7 @@
                                                                             <a href="{{route('ticket.view',$ticket)}}" class="btn btn-danger h-10 my-auto">Nro: {{$ticket->id}} COBRADO</a>
 
                                                                             @if ($ticket->pedido)
-                                                                                <a href="{{route('vendedor.pedido.editing',$ticket->pedido)}}" class="btn btn-success h-10 my-auto mt-2">Pedido Nro: {{$ticket->pedido->id}}</a>
+                                                                                <a href="{{route('vendedor.pedidos.edit',$ticket->pedido)}}" class="btn btn-success h-10 my-auto mt-2">Pedido Nro: {{$ticket->pedido->id}}</a>
                                                                             @else
                                                                                 <a wire:click="realizarpedido({{$ticket->id}})" class="btn bg-gray-200 hover:bg-gray-100 h-10 my-auto cursor-pointer">Realizar Pedido</a>
                                                                             @endif

a19ff727c2993195c0e6c17bc99791c0145800ed
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-19 18:15:21 -0300

    u

--- resources/views/admin/socios/show.blade.php
+++ resources/views/admin/socios/show.blade.php
@@ -32,7 +32,7 @@
                 </span><span> </span>
                 @isset($socio->foto)
 
-                    <img id="picture" class="h-56 w-100 object-contain object-center mt-8"src="{{Storage::url($socio->foto)}}" alt="">
+                    <img id="picture" class="h-44  object-contain object-center mt-8"src="{{Storage::url($socio->foto)}}" alt="">
                 @else
                     <img id="picture" class="h-56 w-100 object-contain object-center"src="https://st4.depositphotos.com/5575514/23597/v/600/depositphotos_235978748-stock-illustration-neutral-profile-picture.jpg" alt="">
                     {!! Form::open(['route'=>['socio.fotos',$socio],'files'=>true , 'autocomplete'=>'off', 'method'=> 'POST' ]) !!}

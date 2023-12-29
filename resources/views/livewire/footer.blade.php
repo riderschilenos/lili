8d65233af2e3128bff2a36b6a3c39c118c9adc8b
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2023-12-29 13:48:57 -0300

    u

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -82,7 +82,7 @@
             </p>
             <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
             
-              <a class="ml-3 text-gray-500">
+              <a class="ml-3 text-white">
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>

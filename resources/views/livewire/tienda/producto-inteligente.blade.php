346813df662b3a3fc1edf4d956ec9ed40c0766d4
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-01-03 20:19:12 -0300

    u

--- resources/views/livewire/tienda/producto-inteligente.blade.php
+++ resources/views/livewire/tienda/producto-inteligente.blade.php
@@ -119,7 +119,7 @@
                                                                     <span aria-hidden
                                                                         class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
                                                     <span class="relative">SI</span>
-                                                @else
+                                                @elseif ($producto->personalizable=='no')
                                                         <span
                                                                     class="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
                                                                     <span aria-hidden

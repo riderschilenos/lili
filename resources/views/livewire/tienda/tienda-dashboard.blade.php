9d28e767986c5803f192f79a88e1a21c945859fb
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-01-04 16:25:34 -0300

    u

--- resources/views/livewire/tienda/tienda-dashboard.blade.php
+++ resources/views/livewire/tienda/tienda-dashboard.blade.php
@@ -208,31 +208,34 @@
         
         
             <div class="mt-4 w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-               
-                <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
-                    <div class="flex items-center">
-                        <div class="flex-shrink-0">
-                            <span class="text-2xl sm:text-3xl leading-none font-bold text-gray-900">{{$pedidos->count()}}</span>
-                            <h3 class="text-base font-normal text-gray-500">Pedidos del mes</h3>
-                        </div>
-                        <div class="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold">
-                            {{number_format(($pedidos->count())*100/$pedidostotal->count(),2)}}
-                            %
-                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
-                            </svg>
+                <a href="{{route('tiendas.pedidos',$tienda)}}">
+                    <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
+                        <div class="flex items-center">
+                            <div class="flex-shrink-0">
+                                <span class="text-2xl sm:text-3xl leading-none font-bold text-gray-900">{{$pedidos->count()}}</span>
+                                <h3 class="text-base font-normal text-gray-500">Pedidos del mes</h3>
+                            </div>
+                            <div class="ml-5 w-0 flex items-center justify-end flex-1 text-green-500 text-base font-bold">
+                                {{number_format(($pedidos->count())*100/$pedidostotal->count(),2)}}
+                                %
+                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                                </svg>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
-                    <div class="flex items-center">
-                        <div class="flex-shrink-0">
-                            <span class="text-2xl sm:text-3xl leading-none font-bold text-gray-900">{{$pedidostotal->count()}}</span>
-                            <h3 class="text-base font-normal text-gray-500">Pedidos en total</h3>
+                </a>
+                <a href="{{route('tiendas.pedidos',$tienda)}}">
+                    <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
+                        <div class="flex items-center">
+                            <div class="flex-shrink-0">
+                                <span class="text-2xl sm:text-3xl leading-none font-bold text-gray-900">{{$pedidostotal->count()}}</span>
+                                <h3 class="text-base font-normal text-gray-500">Pedidos en total</h3>
+                            </div>
+                            
                         </div>
-                        
                     </div>
-                </div>
+                </a>
                 <a href="{{route('tiendas.productos',$tienda)}}">
                     <div class="bg-white hover:bg-gray-100 shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
                         <div class="flex items-center">

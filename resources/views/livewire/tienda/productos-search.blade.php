e70f94e3cea7528fba8257725ec75ff59004afc3
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-01-04 16:56:36 -0300

    u

--- resources/views/livewire/tienda/productos-search.blade.php
+++ resources/views/livewire/tienda/productos-search.blade.php
@@ -30,11 +30,11 @@
                              <tr>
                                 <th
                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                   Name
+                                   Nombre
                                 </th>
                                 <th
                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                   products
+                                   productos
                                 </th>
                                 <th
                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">

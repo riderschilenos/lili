81f1800233aac484d6867939d884a785c28946fe
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-01-04 14:21:15 -0300

    u

--- resources/views/tiendas/cargamanual.blade.php
+++ resources/views/tiendas/cargamanual.blade.php
@@ -4,6 +4,51 @@
             <div class="grid grid-cols-3 w-full gap-x-2 m-6">
                                 
                <div class="mx-auto w-full bg-white col-span-2 py-4 px-6">
+                @if (session('sku'))
+                  <div x-show="alert" class="font-regular relative block w-full max-w-screen-md rounded-lg bg-red-500 px-4 py-4 text-base text-white mb-4" data-dismissible="alert">
+                      <div class="absolute top-4 left-4">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          aria-hidden="true"
+                          class="mt-px h-6 w-6"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                            clip-rule="evenodd"
+                          ></path>
+                        </svg>
+                      </div>
+                      <div class="ml-8 mr-12">
+                        <h5 class="block font-sans text-xl font-semibold leading-snug tracking-normal text-white antialiased">
+                       Producto no encontrado en la base de datos
+                        </h5>
+                      
+                      </div>
+                      <div data-dismissible-target="alert" x-on:click="alert=false"
+                        data-ripple-dark="true"
+                        class="absolute top-3 right-3 w-max rounded-lg transition-all hover:bg-white hover:bg-opacity-20" >
+                        <div role="button" class="w-max rounded-lg p-1">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            stroke-width="2"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M6 18L18 6M6 6l12 12"
+                            ></path>
+                          </svg>
+                        </div>
+                      </div>
+                  </div>
+                @endif
                 <h1 class="font-bold text-2xl mb-4">Crear Nuevo Producto</h1>
 
                   {!! Form::open(['route'=>'admin.products.store', 'autocomplete'=>'off', 'files'=> true ]) !!}

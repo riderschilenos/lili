63c72c0c52677fa0dd80070a7fd4416be6021f83
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-01-04 15:10:13 -0300

    u

--- app/Http/Livewire/Tienda/ProductosSearch.php
+++ app/Http/Livewire/Tienda/ProductosSearch.php
@@ -6,24 +6,30 @@ use App\Models\Invitado;
 use App\Models\Pago;
 use App\Models\Pedido;
 use App\Models\Socio;
+use App\Models\Tienda;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class ProductosSearch extends Component
 {   use WithPagination;
+    public $tienda;
+
+    public function mount($tienda){
+        $this->tienda=Tienda::find($tienda);
+    }
 
     public function render()
     {   $invitados= Invitado::all();
         $socios= Socio::all();
         $pedidos = Pedido::whereHas('ordens', function ($query) {
                 $query->whereHas('producto', function ($queryProducto) {
-                    $queryProducto->where('tienda_id', 4);
+                    $queryProducto->where('tienda_id', $this->tienda->id);
                 });
             })->where('status', '>=', 4)->paginate(50);
 
         $pagos = Pago::whereHas('pedidos', function ($query) {
                 $query->whereHas('ordens.producto', function ($queryProducto) {
-                    $queryProducto->where('tienda_id', 4);
+                    $queryProducto->where('tienda_id', $this->tienda->id);
                 })->where('status', '>=', 4);
             })
             ->where('created_at', '>=', now()->subDays(365))
--- resources/views/tiendas/pedidos.blade.php
+++ resources/views/tiendas/pedidos.blade.php
@@ -1,5 +1,5 @@
 <x-tienda-layout :tienda="$tienda">
          <main>
-            @livewire('tienda.productos-search')
+            @livewire('tienda.productos-search',['tienda'=>$tienda->id])
          </main>
 </x-tienda-layout>

56c5ad37d0b32b79bc9224b48c6ae9ae74a4a9df
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-01-04 21:55:42 -0300

    u

--- resources/views/livewire/admin/pedidos-produccion.blade.php
+++ resources/views/livewire/admin/pedidos-produccion.blade.php
@@ -136,11 +136,13 @@
                                                     <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Eliminar</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
                                                 </span>
                                                 <img class="h-11 w-11 object-cover object-center rounded-full" src="{{Storage::url($pedido->image->url)}}" alt="">
+                                                id:{{$pedido->id}}
                                               
                                             @else
                                                 <img class="h-11 w-11 object-cover object-center rounded-full" src="{{asset('img/compras.jpg')}}" alt="">
+                                                id:{{$pedido->id}}
                                             @endisset
-                                            
+                                           
                                         </div>
                                         <a href="{{route('vendedor.pedidos.edit',$pedido)}}" target="_blank" >
                                         <div class="ml-4">

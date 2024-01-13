daf629c22659e920f4ba4f60a22aa416513f6898
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-01-13 20:54:14 -0300

    u

--- resources/views/livewire/organizador/evento-inscritos.blade.php
+++ resources/views/livewire/organizador/evento-inscritos.blade.php
@@ -135,7 +135,7 @@
                                                                         @else
                                                                      
                                                                             <a href="{{route('ticket.view',$ticket)}}" class="btn btn-danger h-10 my-auto">Nro: {{$ticket->id}} COBRADO</a>
-                                                                            <button wire:click="strava_wtsp({{$ticket->user->id}})" class="btn bg-green-200 my-auto text-xs">REENVIAR WHATSAP</button>
+                                                                            <button wire:click="strava_wtsp({{$ticket->id}})" class="btn bg-green-200 my-auto text-xs">REENVIAR WHATSAP</button>
                                                                       
                                                                             @if ($ticket->pedido)
                                                                                 <a href="{{route('vendedor.pedidos.edit',$ticket->pedido)}}" target="_blank" class="btn btn-success h-10 my-auto mt-2">Pedido Nro: {{$ticket->pedido->id}}</a>

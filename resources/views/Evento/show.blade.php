76308a56e108198b9a4331e193d1b69aa5b3cb75
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-02-01 10:01:48 -0300

    u

--- resources/views/Evento/show.blade.php
+++ resources/views/Evento/show.blade.php
@@ -1164,7 +1164,7 @@
                     
                 <script>
                     function updateCountdownClock2() {
-                        var startSellTime2 = new Date( <?php echo json_encode($evento->fechas->where('end_sell', '!=', null)->first()->end_sell) ?>);
+                        var startSellTime2 = new Date( <?php echo json_encode(new DateTime($evento->fechas->where('end_sell', '!=', null)->first()->end_sell)) ?>);
                         var currentTime = new Date();
 
                         var difference = startSellTime2 - currentTime;

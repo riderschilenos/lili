299854c147b2849d4d7af2587970afd4077c048b
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-02-01 09:55:03 -0300

    u

--- resources/views/Evento/show.blade.php
+++ resources/views/Evento/show.blade.php
@@ -1125,7 +1125,7 @@
         
     </x-fast-view>
     @if($fechas->where('start_sell','!=',null)->count()>0)
-        @if ($fechas->where('start_sell','!=',null)->first()->start_sell>now())
+        @if (strtotime($fechas->where('start_sell','!=',null)->first()->start_sell)>now())
             <script>
                 function updateCountdownClock() {
                     var startSellTime = new Date("{{ $evento->fechas->where('start_sell', '!=', null)->first()->start_sell }}");
@@ -1151,7 +1151,7 @@
                             "s para comenzar";
                     } else {
                         document.getElementById("countdownClock").innerHTML = "¡La venta ya ha comenzado!";
-                        window.location.href = "{{ route('checkout.evento', $evento) }}";
+                        //window.location.href = "{{ route('checkout.evento', $evento) }}";
                     }
 
                     setTimeout(updateCountdownClock, 1000);

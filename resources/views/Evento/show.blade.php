947943325e2ab3a72da86fdac05d65c17134e0ba
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-02-01 09:50:05 -0300

    u

--- resources/views/Evento/show.blade.php
+++ resources/views/Evento/show.blade.php
@@ -1186,7 +1186,7 @@
                                 seconds +
                                 "s para finalizar";
                         } else {
-                            document.getElementById("countdownClock2").innerHTML = startSellTime2;
+                            document.getElementById("countdownClock2").innerHTML = "¡La venta ya ha comenzado!";
                         //       window.location.href = "{{ route('checkout.evento', $evento) }}";
                         }
 

0d0a52a790f5edecf49895c506d464660c5ecd26
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-02-02 14:25:21 -0300

    u

--- resources/views/Evento/show.blade.php
+++ resources/views/Evento/show.blade.php
@@ -117,7 +117,7 @@
                     <p class="mb-2"><i class="fas fa-user"></i> Organizador: {{$evento->user->name}}</p>
 
                         <div>
-                        <div class="inline-block mb-2 ms-[calc(25%-1.25rem)] py-0.5 px-1.5 bg-blue-50 border border-blue-200 text-xs font-medium text-blue-600 rounded-lg dark:bg-blue-800/30 dark:border-blue-800 dark:text-blue-500">{{number_format($inscritos*100/$evento->limite,1)}}%</div>
+                        <div class="inline-block mb-2 ms-[calc({{$inscritos*100/$evento->limite}}%-1.25rem)] py-0.5 px-1.5 bg-blue-50 border border-blue-200 text-xs font-medium text-blue-600 rounded-lg dark:bg-blue-800/30 dark:border-blue-800 dark:text-blue-500">{{number_format($inscritos*100/$evento->limite,1)}}%</div>
                         <div class="flex w-full h-2 bg-gray-200 rounded-full overflow-hidden dark:bg-gray-700" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
                           <div class="flex flex-col justify-center rounded-full overflow-hidden bg-blue-600 text-xs text-white text-center whitespace-nowrap transition duration-500 dark:bg-blue-500" style="width: {{$inscritos*100/$evento->limite}}%"></div>
                         </div>

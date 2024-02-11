35e14bea4ae8b01a38c1aec321ef63d7d7f3c24c
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-02-11 16:34:16 -0300

    u

--- app/Http/Controllers/Ticket/EventoController.php
+++ app/Http/Controllers/Ticket/EventoController.php
@@ -217,6 +217,94 @@ class EventoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+    public function show(Evento $evento)
+    {   if(Cache::has('autos')){
+            $autos = Cache::get('autos');
+        }else{
+            $autos = Vehiculo::where('status',4)
+                            ->orwhere('status',5)
+                            ->orwhere('status',7)
+                            ->latest('id')->get()->take(3);
+            Cache::put('autos',$autos);
+        }
+
+        if(Cache::has('series')){
+            $series = Cache::get('series');
+        }else{
+            $series = Serie::where('status',3)->where('content','serie')->latest('id')->get()->take(4);
+            Cache::put('series',$series);
+        }
+
+
+        if(Cache::has('riders')){
+            $riders = Cache::get('riders');
+        }else{
+        $riders = Socio::join('users','socios.user_id','=','users.id')
+                ->select('socios.*','users.name','users.email','users.updated_at')->where('status',1)
+                ->orwhere('status',2)
+                ->orderByRaw("CASE WHEN users.profile_photo_path IS NOT NULL THEN 0 ELSE 1 END, 
+                CASE WHEN socios.created_at >= CURDATE() THEN 0 ELSE 1 END, 
+                CASE WHEN socios.updated_at >= CURDATE() THEN 0 ELSE 1 END, 
+                id DESC")->get()->take(4);
+            Cache::put('riders',$riders);
+        }
+
+        if(auth()->user())
+        {
+        if(auth()->user()->socio)
+        {
+        $socio2 = Socio::where('user_id',auth()->user()->id)->first();
+        }else{
+        $socio2=null;
+        }
+
+        }
+        else{
+        $socio2=null;
+        }
+
+        $disciplinas= Disciplina::pluck('name','id');
+        
+        $fechas= Fecha::where('evento_id',$evento->id)->get();
+        
+        $similares = Evento::where('disciplina_id',$evento->disciplina_id)
+                            ->where('id','!=',$evento->id)
+                            ->where('status',1)
+                            ->latest('id')
+                            ->take(5)
+                            ->get();
+
+        if(auth()->user())
+        {   
+            if(Ticket::where('evento_id',$evento->id)->where('user_id',auth()->user()->id)){    
+                    $ticket = Ticket::where('evento_id',$evento->id)->where('status',1)->where('user_id',auth()->user()->id)->first();
+                }else{
+                    $ticket =null;
+                }             
+        }
+        else{
+            $ticket =null;
+        }        
+          
+        $fech = Fecha::where('evento_id',$evento->id)->first();
+
+        $tickets =   $evento->tickets()
+                        ->select('tickets.*', 'categorias.name as categoria_name') // Agrega la columna necesaria para el ORDER BY
+                        ->where('status', '>=', 3)
+                        ->join('inscripcions', 'tickets.id', '=', 'inscripcions.ticket_id')
+                        ->join('fecha_categorias', 'inscripcions.fecha_categoria_id', '=', 'fecha_categorias.id')
+                        ->join('categorias', 'fecha_categorias.categoria_id', '=', 'categorias.id')
+                        ->orderBy('tickets.ticketable_type', 'desc')
+                        ->orderBy('tickets.id', 'desc')
+                        ->orderBy('categoria_name', 'asc') // Ordena por la columna agregada en SELECT
+                        ->distinct()
+                        ->paginate(250);
+
+        $invitados=Invitado::all();
+
+        return view('Evento.show',compact('coach','invitados','tickets','evento','fechas','similares','ticket','fech','series','riders','autos','socio2','disciplinas'));
+    }
+
     public function showsocio(Evento $evento,Socio $socio)
     {   if(Cache::has('autos')){
             $autos = Cache::get('autos');

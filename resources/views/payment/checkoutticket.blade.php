3f8a5b4fc3f55659cb2bcd9aaecb9caa63eb7c9c
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-02-19 05:01:04 -0300

    j

--- resources/views/payment/checkoutticket.blade.php
+++ resources/views/payment/checkoutticket.blade.php
@@ -635,41 +635,42 @@
                             <p class="text-2xl font-bold leading-normal text-right text-gray-800 ">${{number_format($ticket->inscripcion)}}</p>
                             </div>
                         </div>
-                        <div class="max-w-4xl bg-white rounded-xl shadow-md pt-6 pb-4 px-2 sm:px-8 ">
-                            <div class="max-w-4xl flex justify-between items-center">
+                        @if ($ticket->inscripcion==0)
+                            <form action="{{route('ticket.semipago',$ticket)}}" method="POST">
+                                @csrf
                             
-                                <div class="items-center">
-                                    <img class="h-full w-40  object-contain" src="{{asset('img/mercadopago.png')}}" alt="">
+                                <button class="btn btn-danger mt-4" >Inscribirme ahora!</button>
                                 
-                                </div>
-                                <div class="hidden sm:flex items-center">
+                            </form>
+                        @else
+                            <div class="max-w-4xl bg-white rounded-xl shadow-md pt-6 pb-4 px-2 sm:px-8 ">
+                                <div class="max-w-4xl flex justify-between items-center">
                                 
-                                    <p class="text-lg my-auto text-center">Paga Utilizando tarjeta de Crédito o Débito</p>
-                                </div>
-                                
-                                @if ($ticket->inscripcion==0)
-                                    <form action="{{route('ticket.semipago',$ticket)}}" method="POST">
-                                        @csrf
+                                    <div class="items-center">
+                                        <img class="h-full w-40  object-contain" src="{{asset('img/mercadopago.png')}}" alt="">
                                     
-                                        <button class="btn btn-danger mt-4" >Inscribirme ahora!</button>
-                                        
-                                    </form>
-                                @else
-                                    <div class="cho-container mt-2 mb-4">
-                                        <!-- Esto es <a href="" class="font-bold py-2 px-4 rounded bg-blue-500 text-white">Pagar</a> un comentario -->
                                     </div>
-                                @endif
-                                   
+                                    <div class="hidden sm:flex items-center">
+                                    
+                                        <p class="text-lg my-auto text-center">Paga Utilizando tarjeta de Crédito o Débito</p>
+                                    </div>
+                                    
+                                
+                                        <div class="cho-container mt-2 mb-4">
+                                            <!-- Esto es <a href="" class="font-bold py-2 px-4 rounded bg-blue-500 text-white">Pagar</a> un comentario -->
+                                        </div>
+                                    
 
-                            </div>
-                
+                                </div>
                     
-                        <div class="flex justify-center sm:hidden items-center">
                         
-                            <p class="text-base my-auto text-center">Paga Utilizando tarjeta de Crédito o Débito</p>
-                        </div>
-                    
-                    </div>
+                                <div class="flex justify-center sm:hidden items-center">
+                                
+                                    <p class="text-base my-auto text-center">Paga Utilizando tarjeta de Crédito o Débito</p>
+                                </div>
+                            
+                            </div>
+                        @endif
                         
 
                         @can('Super admin')

2c2b4232bbcdeb8723efddfa776e587c211c8a26
riderschilenos <94719272+riderschilenos@users.noreply.github.com>
2024-03-17 17:26:27 -0300

    u

--- resources/views/livewire/vendedor/pedidos-create.blade.php
+++ resources/views/livewire/vendedor/pedidos-create.blade.php
@@ -353,90 +353,91 @@
                 </div>
                 {!! Form::close() !!}
             @else
-            <x-table-responsive>
-            
+            @if ($type=='pedido')
+                <x-table-responsive>
+                
 
-                @if ($guess->count())
+                    @if ($guess->count())
 
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                        <tr>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                            Invitados
-                            </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Email
-                            </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Rut
-                            </th>
-                        
-                            <th scope="col" class="relative px-6 py-3">
-                            <span class="sr-only">Edit</span>
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-
-                            @foreach ($guess as $invitado)
-            
-                                    
-                                    <tr wire:click="updateinvitado_id({{$invitado->id}})">
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            <div class="flex items-center">
-                                                <div class="flex-shrink-0 h-10 w-10">
-                                                        
-                                                        <img class="h-11 w-11 object-cover object-center rounded-full" src="{{asset('img/compras.jpg')}}" alt=""  />
-                                                    
-                                                        
-                                                    
-                                                </div>
-                                                <div class="ml-4">
-                                                    <div class="text-sm text-gray-900">
-                                                        {{$invitado->name}}
-                                                    </div>
-                                                    
-                                                </div>
-                                            </div>
-                                        </td>
-
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            <div class="text-sm text-gray-900">{{$invitado->email}}</div>
-                                            
-                                        </td>
-
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            <div class="text-sm text-gray-900">{{$invitado->rut}}</div>
-                                            
-                                        </td>
-
-                                        
-                                        
-
-                                        
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <a wire:click="updateinvitado_id({{$invitado->id}})" class="text-indigo-600 hover:text-indigo-900 cursor-pointer">Seleccionar</a>
-                                        
-                                        </td>
-                                    </tr>
-
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Invitados
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Email
+                                </th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Rut
+                                </th>
                             
-                            @endforeach
-                            <!-- More people... -->
-                        </tbody>
-                    </table>
-                    
-                @else
+                                <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+
+                                @foreach ($guess as $invitado)
+                
+                                        
+                                        <tr wire:click="updateinvitado_id({{$invitado->id}})">
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="flex items-center">
+                                                    <div class="flex-shrink-0 h-10 w-10">
+                                                            
+                                                            <img class="h-11 w-11 object-cover object-center rounded-full" src="{{asset('img/compras.jpg')}}" alt=""  />
+                                                        
+                                                            
+                                                        
+                                                    </div>
+                                                    <div class="ml-4">
+                                                        <div class="text-sm text-gray-900">
+                                                            {{$invitado->name}}
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </div>
+                                            </td>
+
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="text-sm text-gray-900">{{$invitado->email}}</div>
+                                                
+                                            </td>
+
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <div class="text-sm text-gray-900">{{$invitado->rut}}</div>
+                                                
+                                            </td>
+
+                                            
+                                            
+
+                                            
+                                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <a wire:click="updateinvitado_id({{$invitado->id}})" class="text-indigo-600 hover:text-indigo-900 cursor-pointer">Seleccionar</a>
+                                            
+                                            </td>
+                                        </tr>
+
+                                
+                                @endforeach
+                                <!-- More people... -->
+                            </tbody>
+                        </table>
+                        
+                    @else
+                        <div class="px-6 py-4">
+                            No hay ningun registro
+                        </div>
+                    @endif 
+
                     <div class="px-6 py-4">
-                        No hay ningun registro
+                        {{$guess->links()}}
                     </div>
-                @endif 
-
-                <div class="px-6 py-4">
-                    {{$guess->links()}}
-                </div>
-            </x-table-responsive>
-
+                </x-table-responsive>
+            @endif
                 <h1 class="text-center my-4"> Crea un nuevo invitado a continuacion</h1>
                 <div>
                     {!! Form::open(['route' => 'vendedor.pedidos.store', 'method'=> 'POST']) !!}
